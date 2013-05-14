92bf1899ff2d38fae4f2686f1b1e1927c3e379be
bitsteak <anonymous@bitsteak.com>
2013-05-14 15:44:02 -0400

    more stringent filename validation

--- store.py
+++ store.py
@@ -1,9 +1,36 @@
+# -*- coding: utf-8 -*-
 import os
+import re
 import web
 import config
 
+# taken from store.urls mapping
+VALIDATE_FILENAME = re.compile("^[0-9]+\.[0-9]+(?:_msg|_doc|)\.gpg$").match
+
+def verify(p):
+    if os.path.commonprefix([config.STORE_DIR, p]) != config.STORE_DIR:
+        raise Exception("Invalid directory %s" % (p, ))
+
+    filename = os.path.basename(p)
+    ext = os.path.splitext(filename)[-1]
+
+    if not ext:
+        # if there's no extension, we're at a subdirectory
+        return
+
+    elif ext != '.gpg':
+        # if there's an extension, verify it's a GPG
+        raise Exception("Invalid file extension %s" % (ext, ))
+
+    if not VALIDATE_FILENAME(filename):
+        raise Exception("Invalid filename %s" % (filename, ))
+    return
+
 def path(*s):
-    return os.path.join(config.STORE_DIR, *s)
+    joined = os.path.join(os.path.abspath(config.STORE_DIR), *s)
+    absolute = os.path.abspath(joined)
+    verify(absolute)
+    return absolute
 
 def log(msg):
     file(path('NOTES'), 'a').write(msg)
