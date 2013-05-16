5c9b3010b3399dc3dc05f4e5531dc35a262081f6
Bobby Powers <bobbypowers@gmail.com>
2013-05-15 23:05:46 -0400

    doc: basic HACKING file describing dev environment setup
    This addresses some of #8.

---
+++ example_config.py
@@ -0,0 +1,10 @@
+# data directories - should be on secure media
+STORE_DIR='data/store'
+GPG_KEY_DIR='data/keys'
+
+SOURCE_TEMPLATES_DIR='./source_templates'
+JOURNALIST_TEMPLATES_DIR='./journalist_templates'
+WORD_LIST='./wordlist'
+
+HMAC_SECRET='c75f391d54717cbfbd89d3a5597dacad0eab42242661512d18d8087a3c842128' # $ date | sha256sum
+JOURNALIST_KEY=''
