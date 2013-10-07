238a70f48ff9454772078bb116ca836ffc53f0f6
Yan <yan@mit.edu>
2013-10-07 12:34:23 -0700

    Clean up, add comments

--- svs_interface.py
+++ svs_interface.py
@@ -6,11 +6,11 @@ A simple GPG2 interface to batch encrypt/decrypt files.
 """
 
 # TODO:
-# * Get the user passphrase in a prompt instead of making them type it in the
-# terminal
+# * Show interactive GPG prompts in popup windows instead of making user type in the
+# terminal. Might involve some subprocess pipe trickery.
 # * Make output messages more helpful
 # * Functionality for one-click secure wipes
-
+# * Make the UI less ugly
 
 import subprocess
 import Tkinter
@@ -19,14 +19,18 @@ import tkSimpleDialog
 import os
 import datetime
 
+
 GPG = 'gpg2'
-SERVER_KEY = ''  # replace with gpg key ID of server key
+SERVER_KEY = ''  # replace with gpg key ID of server key, eventually
 DECRYPTED_PREFIX = 'decrypted'
 
+
 class GpgApp(object):
+
     def __init__(self, master):
         frame = Tkinter.Frame(master)
         frame.pack()
+        # create the main notification text box with a scrollbar
         scrollbar = Tkinter.Scrollbar(master)
         scrollbar.pack(side=Tkinter.RIGHT, fill=Tkinter.Y)
         self.text = Tkinter.Text(master, yscrollcommand=scrollbar.set)
@@ -35,29 +39,27 @@ class GpgApp(object):
         self.sane_insert('Welcome to the decryption and encryption interface!\n')
         self.sane_insert('To encrypt and decrypt files, go to the File menu.')
         self.sane_insert('Please remember to check the Terminal window for passphrase prompts.\n')
+        # add a file menu
         menu = Tkinter.Menu(master)
         root.config(menu=menu)
-
         filemenu = Tkinter.Menu(menu, tearoff=0)
         menu.add_cascade(label="File", menu=filemenu)
         filemenu.add_command(label="Decrypt files", command=self.batch_decrypt)
         filemenu.add_command(label="Encrypt files", command=self.batch_encrypt)
         filemenu.add_separator()
         filemenu.add_command(label="Exit", command=self.do_exit)
+
     def sane_insert(self, text):
         """Insert text sanely into the UI notification box"""
         self.text.config(state=Tkinter.NORMAL)
         self.text.insert(Tkinter.END, text+'\n')
         self.text.config(state=Tkinter.DISABLED)
-    def filename_open(self):
-        fin = tkFileDialog.askopenfilenames()
-        if fin:
-            self.sane_insert(fin)
-            return fin
+
     def get_recipient(self):
         prompt = "Enter the email address to encrypt files to: "
         recipient = tkSimpleDialog.askstring("Encrypt files", prompt)
         return recipient
+
     def batch_decrypt(self):
         fin = tkFileDialog.askopenfilenames()
         if not fin:
@@ -66,6 +68,7 @@ class GpgApp(object):
         for f in fin:
             self.decrypt_file(f, dirname+os.path.basename(f)+'_decrypted')
         self.sane_insert('Wrote decrypted files to '+dirname)
+
     def batch_encrypt(self, recipient=None):
         recipient = self.get_recipient()
         if recipient is None:
@@ -78,24 +81,31 @@ class GpgApp(object):
         for f in fin:
             self.encrypt_file(f, dirname+os.path.basename(f)+'_encrypted', recipient)
         self.sane_insert('Wrote encrypted files to '+dirname)
+
     def encrypt_file(self, input_file, output_file, recipient):
         args = [GPG, '--output', output_file, '--recipient', recipient, '-sea', input_file]
         self.do_subprocess(args)
+
     def decrypt_file(self, input_file, output_file):
         args = [GPG, '--output', output_file, '--decrypt', input_file]
         self.do_subprocess(args)
+
     def get_timestamped_folder(self, prefix):
+        """Create a timestamped folder for encrypted/decrypted files"""
         timestring = datetime.datetime.now().strftime("%Y%m%d_%H%M")
         dirname = prefix+'_'+timestring+'/'
         if not os.path.isdir(dirname):
             os.mkdir(dirname)
         return dirname
+
     def do_subprocess(self, cmd):
         proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                          stdin=subprocess.PIPE)
         out, err = proc.communicate()
         if err: self.sane_insert(err)
+
     def do_exit(self):
+        """Close the window and quit"""
         root.destroy()
 
 
