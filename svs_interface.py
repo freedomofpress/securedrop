2c37dd895b2e836625df5a495e3848a1758d10a8
Yan <yan@mit.edu>
2013-10-07 11:12:58 -0700

    improvements to text

--- svs_interface.py
+++ svs_interface.py
@@ -17,20 +17,26 @@ class GpgApp(object):
         frame.pack()
         self.text = Tkinter.Text()
         self.text.pack()
+        self.sane_insert('Welcome to the decryption and encryption interface!\n')
+        self.sane_insert('To encrypt and decrypt files, go to the File menu.')
+        self.sane_insert('Please remember to check the Terminal window for passphrase prompts.')
         menu = Tkinter.Menu(master)
         root.config(menu=menu)
 
         filemenu = Tkinter.Menu(menu, tearoff=0)
         menu.add_cascade(label="File", menu=filemenu)
-        filemenu.add_command(label="Open", command=self.filename_open)
         filemenu.add_command(label="Decrypt files", command=self.batch_decrypt)
         filemenu.add_command(label="Encrypt files", command=self.batch_encrypt)
         filemenu.add_separator()
         filemenu.add_command(label="Exit", command=self.do_exit)
+    def sane_insert(self, text):
+        self.text.config(state=Tkinter.NORMAL)
+        self.text.insert(Tkinter.END, text+'\n')
+        self.text.config(state=Tkinter.DISABLED)
     def filename_open(self):
         fin = tkFileDialog.askopenfilenames()
         if fin:
-            self.text.insert(Tkinter.END,fin)
+            self.sane_insert(fin)
             return fin
     def get_recipient(self):
         prompt = "Enter the email address to encrypt files to: "
@@ -42,17 +48,16 @@ class GpgApp(object):
         if not os.path.isdir(dirname):
             os.mkdir(dirname)
         fin = tkFileDialog.askopenfilenames()
-        print fin
         for f in fin:
             try:
                 self.decrypt_file(f, dirname+os.path.basename(f)+'_decrypted')
             except:
                 print "Error decrypting: "+f
-        self.text.insert(Tkinter.END, 'Wrote decrypted files to '+dirname+'\n')
+        self.sane_insert('Wrote decrypted files to '+dirname)
     def batch_encrypt(self, recipient=None):
         recipient = self.get_recipient()
         if recipient is None:
-            self.text.insert(Tkinter.END, "You must specify an email address for encryption\n")
+            self.sane_insert("You must specify an email address for encryption")
             return
         timestring = datetime.datetime.now().strftime("%Y%m%d_%H%M")
         dirname = 'encrypted_'+timestring+'/'
@@ -64,7 +69,7 @@ class GpgApp(object):
                 self.encrypt_file(f, dirname+os.path.basename(f)+'_encrypted', recipient)
             except:
                 print "Error encrypting: "+f
-        self.text.insert(Tkinter.END, 'Wrote encrypted files to '+dirname+'\n')
+        self.sane_insert('Wrote encrypted files to '+dirname)
     def encrypt_file(self, input_file, output_file, recipient):
         args = [GPG, '--output', output_file, '--recipient', recipient, '-sea', input_file]
         subprocess.call(args)
