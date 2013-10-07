bf38247c57e46bc595659da2d4fa7692badbfe3e
William Budington <bill@eff.org>
2013-10-06 18:38:37 -0700

    regenerate code link

--- modules/deaddrop/files/deaddrop/static/js/ui.js
+++ modules/deaddrop/files/deaddrop/static/js/ui.js
@@ -1,3 +1,23 @@
+var secure_drop = {
+  init: function(){
+    this.regenerate_ui();
+  },
+  regenerate_ui: function(){
+    $('#regenerate-link').click(function(){
+      $.ajax({
+        url: '/gen_ajax/?words=10'
+      }).done(function(resp){
+        if(resp.result == "success"){
+          $("#code-name").text(resp.id);
+        } else {
+          alert('Regenerate timed out.  Please try again.');
+        }
+      });
+      return false;
+    });
+  }
+};
+
 $(function(){
-  
+  secure_drop.init();
 });
