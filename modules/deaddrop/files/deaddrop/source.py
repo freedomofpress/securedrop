a607e9f4c68a3253e486f056a0a7490858e3ee84
William Budington <bill@eff.org>
2013-10-12 18:28:28 -0700

    /generate/ is now a GET endpoint, which POST refers to when provided with # of words.  added endpoint /create/ to create docstore. added form to regenerate codes

--- modules/deaddrop/files/deaddrop/source.py
+++ modules/deaddrop/files/deaddrop/source.py
@@ -7,6 +7,7 @@ urls = (
   '/', 'index',
   '/generate/', 'generate',
   '/gen_ajax/', 'gen_ajax',
+  '/create/', 'create',
   '/lookup/', 'lookup',
 )
 
@@ -21,17 +22,13 @@ class index:
     return render.index()
 
 class generate:
-  def GET(self):
-    raise web.seeother('/')
-  
   def POST(self):
-    iid = crypto.genrandomid()
-    if os.path.exists(store.path(crypto.shash(iid))):
-      # if this happens, we're not using very secure crypto
-      store.log('Got a duplicate ID.')
-    else:
-      os.mkdir(store.path(crypto.shash(iid)))
-      
+    i = web.input()
+    return self.GET(int(i['number-words']))
+
+  def GET(self, number_of_words=8):
+    iid = crypto.genrandomid(number_of_words)
+     
     web.header('Cache-Control', 'no-cache, no-store, must-revalidate')
     web.header('Pragma', 'no-cache')
     web.header('Expires', '-1')
@@ -48,6 +45,61 @@ class gen_ajax:
     else:
       return json.dumps({'result': 'fail'})
 
+def store_endpoint(i):
+  sid = crypto.shash(i.id)
+  loc = store.path(sid)
+  if not os.path.exists(loc): raise web.notfound()
+  
+  received = False
+  
+  if i.action == 'upload':
+    if i.msg:
+      loc1 = store.path(sid, '%.2f_msg.gpg' % (uuid.uuid4().int, ))
+      crypto.encrypt(config.JOURNALIST_KEY, i.msg, loc1)
+      received = 2
+      
+    if i.fh.value:
+      # we put two zeroes here so that we don't save a file 
+      # with the same name as the message
+      loc2 = store.path(sid, '%.2f_doc.gpg' % (uuid.uuid4().int, ))
+      crypto.encrypt(config.JOURNALIST_KEY, i.fh.file, loc2, fn=i.fh.filename)
+      received = i.fh.filename or '[unnamed]'
+
+    if not crypto.getkey(sid):
+      background.execute(lambda: crypto.genkeypair(sid, i.id))
+  
+  elif i.action == 'delete':
+    potential_files = os.listdir(loc)
+    if i.mid not in potential_files: raise web.notfound()
+    assert '/' not in i.mid
+    crypto.secureunlink(store.path(sid, i.mid))
+  
+  msgs = []
+  for fn in os.listdir(loc):
+    if fn.startswith('reply-'):
+      msgs.append(web.storage(
+        id=fn,
+        date=str(datetime.datetime.fromtimestamp(os.stat(store.path(sid, fn)).st_mtime)),
+        msg=crypto.decrypt(sid, i.id, file(store.path(sid, fn)).read())
+      ))
+
+  web.header('Cache-Control', 'no-cache, no-store, must-revalidate')
+  web.header('Pragma', 'no-cache')
+  web.header('Expires', '-1')
+  return render.lookup(i.id, msgs, received=received)
+
+class create:
+  def POST(self):
+    i = web.input('id', fh={}, msg=None, mid=None, action=None)
+    sid = crypto.shash(i.id)
+
+    if os.path.exists(store.path(sid)):
+      # if this happens, we're not using very secure crypto
+      store.log('Got a duplicate ID.')
+    else:
+      os.mkdir(store.path(sid))
+    return store_endpoint(i)
+ 
 class lookup:
   def GET(self):
     web.header('Cache-Control', 'no-cache, no-store, must-revalidate')
@@ -57,47 +109,7 @@ class lookup:
   
   def POST(self):
     i = web.input('id', fh={}, msg=None, mid=None, action=None)
-    sid = crypto.shash(i.id)
-    loc = store.path(sid)
-    if not os.path.exists(loc): raise web.notfound()
-    
-    received = False
-    
-    if i.action == 'upload':
-      if i.msg:
-        loc1 = store.path(sid, '%.2f_msg.gpg' % (uuid.uuid4().int, ))
-        crypto.encrypt(config.JOURNALIST_KEY, i.msg, loc1)
-        received = 2
-        
-      if i.fh.value:
-        # we put two zeroes here so that we don't save a file 
-        # with the same name as the message
-        loc2 = store.path(sid, '%.2f_doc.gpg' % (uuid.uuid4().int, ))
-        crypto.encrypt(config.JOURNALIST_KEY, i.fh.file, loc2, fn=i.fh.filename)
-        received = i.fh.filename or '[unnamed]'
-
-      if not crypto.getkey(sid):
-        background.execute(lambda: crypto.genkeypair(sid, i.id))
-    
-    elif i.action == 'delete':
-      potential_files = os.listdir(loc)
-      if i.mid not in potential_files: raise web.notfound()
-      assert '/' not in i.mid
-      crypto.secureunlink(store.path(sid, i.mid))
-    
-    msgs = []
-    for fn in os.listdir(loc):
-      if fn.startswith('reply-'):
-        msgs.append(web.storage(
-          id=fn,
-          date=str(datetime.datetime.fromtimestamp(os.stat(store.path(sid, fn)).st_mtime)),
-          msg=crypto.decrypt(sid, i.id, file(store.path(sid, fn)).read())
-        ))
-
-    web.header('Cache-Control', 'no-cache, no-store, must-revalidate')
-    web.header('Pragma', 'no-cache')
-    web.header('Expires', '-1')
-    return render.lookup(i.id, msgs, received=received)
+    return store_endpoint(i)
 
 def notfound():
   web.header('Cache-Control', 'no-cache, no-store, must-revalidate')
