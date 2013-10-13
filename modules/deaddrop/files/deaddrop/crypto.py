1a8bef6cfb152aa1cb9c39fa3870b519945412cb
William Budington <bill@eff.org>
2013-10-13 16:22:23 -0700

    remove debug statement

--- modules/deaddrop/files/deaddrop/crypto.py
+++ modules/deaddrop/files/deaddrop/crypto.py
@@ -95,7 +95,6 @@ def encrypt(fingerprint, s, output=None, fn=None):
     else:
         if fn:
             with _gpghacklock:
-                print "filename: "+fn
                 out = gpg.encrypt(s, fingerprint, output=_shquote(fn), always_trust=True)
         else:
             out = gpg.encrypt(s, fingerprint, output=_shquote(fn), always_trust=True)
