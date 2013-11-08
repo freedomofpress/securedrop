c5e04c204869164ed017d7f526ea2b5764db2bc7
Garrett Robinson <garrettr@riseup.net>
2013-11-08 09:45:15 -0800

    tests, and accompanying revisions

--- modules/deaddrop/files/deaddrop/source.py
+++ modules/deaddrop/files/deaddrop/source.py
@@ -2,20 +2,37 @@
 import os
 from datetime import datetime
 import uuid
+from functools import wraps
 
-from flask import Flask, request, render_template, session, redirect, url_for, \
-    flash
-from itsdangerous import Signer, BadSignature
-from werkzeug import secure_filename
+from flask import (Flask, request, render_template, session, redirect, url_for,
+    flash, abort, g)
 from flask_wtf.csrf import CsrfProtect
 
 import config, version, crypto, store, background
 
 app = Flask(__name__, template_folder=config.SOURCE_TEMPLATES_DIR)
 app.secret_key = config.SECRET_KEY
-CsrfProtect(app)
 
 app.jinja_env.globals['version'] = version.__version__
+
+def logged_in():
+  if 'logged_in' in session: return True
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not logged_in():
+            return redirect(url_for('lookup'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+@app.before_request
+def setup_g():
+  """Store commonly used values in Flask's special g object"""
+  if logged_in():
+    g.codename = session['codename']
+    g.sid = crypto.shash(g.codename)
+    g.loc = store.path(g.sid)
 
 @app.after_request
 def no_cache(response):
@@ -30,10 +47,6 @@ def no_cache(response):
     response.headers.add(header, header_value)
   return response
 
-@app.template_filter()
-def sign(s):
-  return Signer(config.SECRET_KEY).sign(s)
-
 @app.route('/')
 def index():
   return render_template('index.html')
@@ -43,94 +56,92 @@ def generate():
   number_words = 8
   if request.method == 'POST':
     number_words = int(request.form['number-words'])
-    if number_words not in range(4, 11): abort(500)
-  return render_template('generate.html',
-      codename=crypto.genrandomid(number_words))
+    if number_words not in range(4, 11): abort(403)
+  session['codename'] = crypto.genrandomid(number_words)
+  return render_template('generate.html', codename=session['codename'])
 
 @app.route('/create', methods=['POST'])
 def create():
-  # TODO: need to unescape?
-  codename = Signer(config.SECRET_KEY).unsign(request.form['codename'])
-  sid = crypto.shash(codename)
+  sid = crypto.shash(session['codename'])
   if os.path.exists(store.path(sid)):
     # if this happens, we're not using very secure crypto
-    store.log('Got a duplicate ID %s' % sid)
+    store.log("Got a duplicate ID '%s'" % sid)
   else:
     os.mkdir(store.path(sid))
-  session['codename'] = codename
+  session['logged_in'] = True
   return redirect(url_for('lookup'))
 
-def get_codename_ids(codename):
-  sid = crypto.shash(codename)
-  loc = store.path(sid)
-  return sid, loc
-
-def valid_codename(codename):
-  return os.path.exists(get_codename_ids(codename)[1])
-
-def get_msgs(codename):
-  sid, loc = get_codename_ids(codename)
+@app.route('/lookup', methods=('GET',))
+@login_required
+def lookup():
   msgs = []
-  for fn in os.listdir(loc):
+  for fn in os.listdir(g.loc):
     if fn.startswith('reply-'):
       msgs.append(dict(
         id=fn,
-        date=str(datetime.fromtimestamp(os.stat(store.path(sid, fn)).st_mtime)),
-        msg=crypto.decrypt(sid, codename, file(store.path(sid, fn)).read())
+        date=str(datetime.fromtimestamp(os.stat(store.path(g.sid, fn)).st_mtime)),
+        msg=crypto.decrypt(g.sid, g.codename, file(store.path(g.sid, fn)).read())
       ))
-  return msgs
+  return render_template('lookup.html', codename=g.codename, msgs=msgs)
 
 @app.route('/submit', methods=('POST',))
+@login_required
 def submit():
-  if 'codename' in session and valid_codename(session['codename']):
-    codename = session['codename']
-    sid, loc = get_codename_ids(codename)
+  msg = request.form['msg']
+  fh = request.files['fh']
+  if msg:
+    msg_loc = store.path(g.sid, '%s_msg.gpg' % uuid.uuid4())
+    crypto.encrypt(config.JOURNALIST_KEY, msg, msg_loc)
+    flash("Thanks! We received your message.", "notification")
+  if fh:
+    file_loc = store.path(g.sid, "%s_doc.gpg" % uuid.uuid4())
+    # TODO - retain original filename
+    crypto.encrypt(config.JOURNALIST_KEY, fh, file_loc)
+    flash("Thanks! We received your document '%s'."
+        % fh.filename or '[unnamed]', "notification")
 
-    msg = request.form['msg']
-    fh = request.files['fh']
-    if msg:
-      msg_loc = store.path(sid, '%s_msg.gpg' % uuid.uuid4())
-      crypto.encrypt(config.JOURNALIST_KEY, msg, msg_loc)
-      flash("Thanks! We received your message.", "notification")
-    if fh:
-      file_loc = store.path(sid, "%s_doc.gpg" % uuid.uuid4())
-      # TODO - retain original filename
-      crypto.encrypt(config.JOURNALIST_KEY, fh, file_loc)
-      flash("Thanks! We received your document '%s'."
-          % fh.filename or '[unnamed]', "notification")
-
-    if not crypto.getkey(sid):
+  # helper function to generate a keypair asynchronously
+  def async_genkey(sid, codename):
+    with app.app_context():
       background.execute(lambda: crypto.genkeypair(sid, codename))
+
+  # Generate a keypair to encrypt replies from the journalist
+  if not crypto.getkey(g.sid):
+    async_genkey(g.sid, g.codename)
 
   return redirect(url_for('lookup'))
 
 @app.route('/delete', methods=('POST',))
+@login_required
 def delete():
-  if 'codename' in session and valid_codename(session['codename']):
-    codename = session['codename']
-    sid, loc = get_codename_ids(codename)
-
-    msgid = request.form['msgid']
-    potential_files = os.listdir(loc)
-    if msgid not in potential_files: abort(404) # TODO are the checks necessary?
-    assert '/' not in msgid
-    crypto.secureunlink(store.path(sid, msgid))
-    flash("Reply successfully deleted.", "notification")
+  msgid = request.form['msgid']
+  assert '/' not in msgid
+  potential_files = os.listdir(g.loc)
+  if msgid not in potential_files: abort(404) # TODO are the checks necessary?
+  crypto.secureunlink(store.path(g.sid, msgid))
+  flash("Reply deleted.", "notification")
 
   return redirect(url_for('lookup'))
 
-@app.route('/lookup', methods=('GET',))
-def lookup():
-  if 'codename' in session and valid_codename(session['codename']):
-    codename = session['codename']
-    return render_template('lookup.html', codename=codename,
-        msgs=get_msgs(codename))
+def valid_codename(codename):
+  return os.path.exists(store.path(crypto.shash(codename)))
 
-  return render_template('lookup_get.html')
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+  if request.method == 'POST':
+    codename = request.form['codename']
+    if valid_codename(codename):
+      session.update(codename=codename, logged_in=True)
+      return redirect(url_for('lookup'))
+    else:
+      flash("Sorry, that is not a recognized codename.", "error")
+  return render_template('login.html')
 
 @app.errorhandler(404)
 def page_not_found(error):
   return render_template('notfound.html'), 404
 
 if __name__ == "__main__":
+  # TODO: make sure this gets run by the web server
+  CsrfProtect(app)
   app.run(debug=True, port=8080)
