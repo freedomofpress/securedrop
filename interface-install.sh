58de372a95e4d877022530b69b1543515caf6503
James <dolanjs@gmail.com>
2013-11-09 15:06:41 -0800

    added second chroot jail

--- interface-install.sh
+++ interface-install.sh
@@ -1,17 +1,19 @@
-
-
 apt-get install dchroot debootstrap
 
 mkdir -p /opt/chroot/{source,journalist}
 
 cat << EOF > /etc/schroot/schroot.conf
-[precise]
+[source]
 description=Ubuntu Precise
 location=/var/chroot/source
-priority=3
 users=source
-groups=sbuild
-root-groups=root
+groups=source
+
+[journalist]
+description=Ubuntu Precise
+location=/var/chroot/journalist
+users=journalist
+groups=journalist
 EOF
 
 debootstrap --variant=buildd --arch amd64 precise /var/chroot/source http://us.archive.ubuntu.com/ubuntu
