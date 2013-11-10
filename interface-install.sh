feee21c001569e647cabf9e102d07e70674469a2
James <dolanjs@gmail.com>
2013-11-09 23:40:21 -0800

    put it in a for loop

--- interface-install.sh
+++ interface-install.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
- 
+JAILS="source_pub journalist" 
+apt-get update -y
+apt-get upgrade -y
 apt-get -y install dchroot debootstrap
-mkdir -p /opt/chroot/{source,journalist}
-
-cat << EOF > /etc/schroot/schroot.conf
-[source]
-description=Ubuntu Precise
-directory=/var/chroot/source
-users=source
-groups=source
-[journalist]
-description=Ubuntu Precise
-directory=/var/chroot/journalist
-users=journalist
-groups=journalist
-EOF
-
-debootstrap --variant=buildd --arch amd64 precise /var/chroot/source http://us.archive.ubuntu.com/ubuntu
-debootstrap --variant=buildd --arch amd64 precise /var/chroot/journalist http://us.archive.ubuntu.com/ubuntu
-
-TOR_REPO="deb     http://deb.torproject.org/torproject.org $( lsb_release -c | cut -f 2) main "
-TOR_KEY_ID="886DDD89"
-TOR_KEY_FINGERPRINT="A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89"
 
 #Check that user is root
 if [[ $EUID -ne 0 ]]; then
@@ -29,23 +10,42 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-useradd source 
+TOR_REPO="deb     http://deb.torproject.org/torproject.org $( lsb_release -c | cut -f 2) main "
+TOR_KEY_ID="886DDD89"
+TOR_KEY_FINGERPRINT="A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89"
 
-mkdir /var/chroot/source/{proc,etc}
-mkdir -p /var/chroot/source/etc/apt
-mount -o bind /proc /var/chroot/source/proc
-cp /etc/resolv.conf /var/chroot/source/etc/resolv.conf
-cp /etc/apt/sources.list /var/chroot/source/etc/apt
+DEPENDENCIES='secure-delete gnupg2 haveged libpam-google-authenticator apparmor-profiles apparmor-utils python-software-properties apache2-mpm-worker libapache2-mod-wsgi python-pip python-dev'
 
+PIPDEPENDENCIES='Flask python-gnupg python-bcrypt pycrypto'
 
-schroot -c source -u root<<FOE
+for JAIL in $JAILS; do
+echo "FOE for $JAIL"
+
+  cat << EOF > /etc/schroot/chroot.d/$JAIL.conf
+[$JAIL]
+description=Ubuntu Precise
+directory=/var/chroot/$JAIL
+users=source
+groups=source
+EOF
+
+  debootstrap --variant=buildd --arch amd64 precise /var/chroot/$JAIL http://us.archive.ubuntu.com/ubuntu
+  useradd $JAIL
+  mkdir -p /opt/chroot/$JAIL
+  mkdir /var/chroot/$JAIL/{proc,etc}
+  mkdir -p /var/chroot/$JAIL/etc/apt
+  mount -o bind /proc /var/chroot/$JAIL/proc
+  cp /etc/resolv.conf /var/chroot/$JAIL/etc/resolv.conf
+  cp /etc/apt/sources.list /var/chroot/$JAIL/etc/apt
+
+  schroot -c $JAIL -u root<<FOE
 
 echo "Updating chroot system..."
 
 catch_error() {
   if [ !$1 = "0" ]; then
-   echo "ERROR encountered $2"
-   exit 1
+    echo "ERROR encountered $2"
+    exit 1
   fi
 }
 
@@ -53,16 +53,9 @@ apt-get -y update
 apt-get -y upgrade
 apt-get -y install ubuntu-minimal
 
-TOR_REPO="deb     http://deb.torproject.org/torproject.org $( lsb_release -c | cut -f 2) main "
-TOR_KEY_ID="886DDD89"
-TOR_KEY_FINGERPRINT="A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89"
-
-DEPENDENCIES='secure-delete gnupg2 haveged libpam-google-authenticator apparmor-profiles apparmor-utils python-software-properties apache2-mpm-worker libapache2-mod-wsgi python-pip python-dev'
-PIPDEPENDENCIES='Flask python-gnupg python-bcrypt pycrypto'
-
-
 #Install dependencies
 echo ""
+echo $DEPENDENCIES
 echo "Installing dependencies..."
 
 echo ""
@@ -91,7 +84,6 @@ echo "Tor installed"
 cat << EOF > /etc/tor/torrc
 RunAsDaemon 1
 HiddenServiceDir /var/lib/tor/hidden_service/
-HiddenServicePort 80 127.0.0.1:80
 SafeLogging 1
 SocksPort 0
 EOF
@@ -103,10 +95,9 @@ catch_error $? "restarting tor"
 
 echo "inside chroot"
 FOE
-
+echo "FOE done"
+done
 
 echo "outside chroot"
 
-#chroot /opt/chroot/journals
- 
-# ...
+exit 0
