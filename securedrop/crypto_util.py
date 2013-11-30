8059ca9e34e1f0b23d1afaf7d5104b3a68dc9b18
Garrett Robinson <garrettr@riseup.net>
2013-11-30 18:45:31 -0500

    remove ref to deaddrop, and fake email from uid
    Since the reference to deaddrop was part of the fake email address, it made
    sense to remove them both since the fake email was not actually needed.
    I modified getkey so it should still work, even if used with reply keypairs
    generated with previous versions of securedrop (they'll still have the
    hashed codename in their uid).

--- securedrop/crypto_util.py
+++ securedrop/crypto_util.py
@@ -96,14 +96,14 @@ def genkeypair(name, secret):
     return gpg.gen_key(gpg.gen_key_input(
         key_type=GPG_KEY_TYPE, key_length=GPG_KEY_LENGTH,
         passphrase=secret,
-        name_email="%s@deaddrop.example.com" % name
+        name_email=name
     ))
 
 
 def getkey(name):
     for key in gpg.list_keys():
         for uid in key['uids']:
-            if ' <%s@' % name in uid:
+            if name in uid:
                 return key['fingerprint']
     return None
 
