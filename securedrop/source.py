254ddf07dffda90775af37624540eed7c139301c
Garrett Robinson <garrettr@riseup.net>
2013-11-29 22:20:35 -0500

    add download link for journalist pubkey, started info page

--- securedrop/source.py
+++ securedrop/source.py
@@ -5,7 +5,7 @@ import uuid
 from functools import wraps
 
 from flask import (Flask, request, render_template, session, redirect, url_for,
-                   flash, abort, g)
+                   flash, abort, g, send_file)
 from flask_wtf.csrf import CsrfProtect
 
 import config
@@ -216,6 +216,20 @@ def howto_disable_js():
 @app.route('/tor2web-warning')
 def tor2web_warning():
     return render_template("tor2web-warning.html")
+
+
+@app.route('/journalist-key')
+def download_journalist_pubkey():
+    journalist_pubkey = crypto_util.gpg.export_keys(config.JOURNALIST_KEY)
+    return send_file(StringIO(journalist_pubkey),
+                     mimetype="application/pgp-keys",
+                     attachment_filename=config.JOURNALIST_KEY + ".asc",
+                     as_attachment=True)
+
+
+@app.route('/why-journalist-key')
+def why_download_journalist_pubkey():
+    return render_template("why-journalist-key.html")
 
 
 @app.errorhandler(404)
