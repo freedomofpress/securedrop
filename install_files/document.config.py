8106ad2c34576ae9c974718e6bd893f88f55baec
James <dolanjs@gmail.com>
2013-11-30 17:16:43 -0800

    defined the nouns.txt and adjectives.txt in the config.py file for the document interface and added the mysql portion to the document.config.py file

--- install_files/document.config.py
+++ install_files/document.config.py
@@ -26,7 +26,8 @@ JOURNALIST_KEY='APP_GPG_KEY_FINGERPRINT'
 SOURCE_TEMPLATES_DIR='/var/www/securedrop/source_templates'
 JOURNALIST_TEMPLATES_DIR='/var/www/securedrop/journalist_templates'
 WORD_LIST='/var/www/securedrop/wordlist'
-
+NOUNS='/var/www/securedrop/dictionaries/nouns.txt'
+ADJECTIVES='/var/www/securedrop/dictionaries/adjectives.txt'
 BCRYPT_SALT='BCRYPT_SALT_VALUE'
 
 # Default to the production configuration
@@ -39,3 +40,19 @@ if os.environ.get('SECUREDROP_ENV') == 'test':
     GPG_KEY_DIR=os.path.join(TEST_DIR, 'keys')
     # test_journalist_key.pub
     JOURNALIST_KEY='65A1B5FF195B56353CC63DFFCC40EF1228271441'
+
+tabase Configuration
+
+# Default to using a sqlite database file for development
+#DATABASE_ENGINE = 'sqlite'
+#DATABASE_FILE=os.path.join(SECUREDROP_ROOT, 'db.sqlite')
+
+# Uncomment to use mysql (or any other databaes backend supported by
+# SQLAlchemy). Make sure you have the necessary dependencies installed, and run
+# `python -c "import db; db.create_tables()"` to initialize the database
+
+DATABASE_ENGINE = 'mysql'
+DATABASE_HOST = 'localhost'
+DATABASE_NAME = 'securedrop'
+DATABASE_USERNAME = 'securedrop'
+DATABASE_PASSWORD = ''
