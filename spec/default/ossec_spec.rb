918ed3e41ccb4f26a47d5d2ff9db967ad8ed87d7
Chris Ng <chaoselixir@gmail.com>
2014-01-26 11:56:55 -0800

    Initial config for install specs using serverspec

---
+++ spec/default/ossec_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+['postfix', 'procmail'].each do |pkg|
+  describe package(pkg) do
+    it { should be_installed }
+  end
+end
+
+describe file('/etc/postfix/main.cf') do
+  it { should be_file }
+  its(:content) { should match /^mailbox_command = \/usr\/bin\/procmail$/ }
+end
+
+describe file("/var/ossec/.gnupg") do
+  it { should be_directory }
+  it { should be_owned_by "ossec" }
+  it { should be_mode '700' }
+end
+
+describe file("/var/ossec/.procmailrc") do
+  its(:content) { should match "/var/ossec/send_encrypted_alarm.sh" }
+end
+  
+describe file("/var/ossec/send_encrypted_alarm.sh") do
+  it { should be_mode '0755' }
+end
+
+describe file("/var/log/procmail.log") do
+  it { should be_owned_by "ossec" }
+end
+  
