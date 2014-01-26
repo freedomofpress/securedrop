47a74b7841be7b174d8e34a1c98a883666a200a0
Jack Singleton <git@jacksingleton.com>
2014-01-25 18:27:35 -0800

    made tests verbose

--- securedrop/test.sh
+++ securedrop/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # -f makes unittest fail fast, so we can use && to avoid burying test failures
-python -m unittest -f tests.unit_tests && python -m unittest -f tests.functional_tests
+python -m unittest -fv tests.unit_tests && python -m unittest -fv tests.functional_tests
