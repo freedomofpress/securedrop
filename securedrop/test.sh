39f6165bfcf6e7fab9bd5c5ae8223084331bb44a
Jack Singleton <git@jacksingleton.com>
2014-01-29 12:41:04 -0800

    updated vagrant warning in test.sh

--- securedrop/test.sh
+++ securedrop/test.sh
@@ -2,7 +2,7 @@
 
 if [ $(which vagrant) ] ; then
     echo ""
-    echo "*** You probably want to run tests from vagrant. Run 'vagrant ssh', then 'cd /vagrant/securedrop' ***"
+    echo "*** You probably want to run tests from vagrant. Run 'vagrant ssh', then 'cd /vagrant/securedrop' and re-run this script***"
     echo ""
 fi
 
