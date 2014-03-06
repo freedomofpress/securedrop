04197f113c29bbb4f7cc890c8bc13dc6c34327bf
Pete Hodgson <git@thepete.net>
2014-03-05 22:14:02 -0800

    download more more more

--- travis/generate-poor-but-copious-randomosity.sh
+++ travis/generate-poor-but-copious-randomosity.sh
@@ -8,6 +8,14 @@ cat /proc/sys/kernel/random/entropy_avail
 echo 'here comes some entropy!!!!'
 
 wget --quiet -O /dev/null http://softwarestudies.com/projects/manga.viz/manga.first_10_titles.Xstdev.Yentropy.10000w.jpeg_medium.jpg
+wget --quiet -O /dev/null http://softwarestudies.com/projects/manga.viz/manga.first_10_titles.Xstdev.Yentropy.10000w.jpeg_medium.jpg
+wget --quiet -O /dev/null http://softwarestudies.com/projects/manga.viz/manga.first_10_titles.Xstdev.Yentropy.10000w.jpeg_medium.jpg
+wget --quiet -O /dev/null http://softwarestudies.com/projects/manga.viz/manga.first_10_titles.Xstdev.Yentropy.10000w.jpeg_medium.jpg
+wget --quiet -O /dev/null http://softwarestudies.com/projects/manga.viz/manga.first_10_titles.Xstdev.Yentropy.10000w.jpeg_medium.jpg
+wget --quiet -O /dev/null http://softwarestudies.com/projects/manga.viz/manga.first_10_titles.Xstdev.Yentropy.10000w.jpeg_medium.jpg
+wget --quiet -O /dev/null http://softwarestudies.com/projects/manga.viz/manga.first_10_titles.Xstdev.Yentropy.10000w.jpeg_medium.jpg
+wget --quiet -O /dev/null http://softwarestudies.com/projects/manga.viz/manga.first_10_titles.Xstdev.Yentropy.10000w.jpeg_medium.jpg
+wget --quiet -O /dev/null http://softwarestudies.com/projects/manga.viz/manga.first_10_titles.Xstdev.Yentropy.10000w.jpeg_medium.jpg
 
 echo 'did you see all that entropy right there?!'
 cat /proc/sys/kernel/random/entropy_avail
