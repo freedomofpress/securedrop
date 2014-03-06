cbcc082c8f8ade22d6c641776cddc2202875a014
Pete Hodgson <git@thepete.net>
2014-03-05 21:30:42 -0800

    maybe downloading a big file will generate entropy?

--- travis/generate-poor-but-copious-randomosity.sh
+++ travis/generate-poor-but-copious-randomosity.sh
@@ -6,6 +6,9 @@ ls -l /dev*
 # generate a bunch of disk-based entropy?
 cat /proc/sys/kernel/random/entropy_avail
 echo 'here comes some entropy!!!!'
-cat /dev/sda1 | tail -n 10000 > /dev/null
+
+wget --quiet -O /dev/null http://softwarestudies.com/projects/manga.viz/ma
+nga.first_10_titles.Xstdev.Yentropy.10000w.jpeg_medium.jpg
+
 echo 'did you see all that entropy right there?!'
 cat /proc/sys/kernel/random/entropy_avail
