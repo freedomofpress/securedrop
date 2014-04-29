1f549d2aae823655b9598cc915892b7c14f579c6
Micah Lee <micah@micahflee.com>
2014-04-29 14:35:54 -0400

    merged chroot scripts into a single bash script with commands. bundle ubuntu signing key to verify ubuntu packages in chroot jail.

--- chroot/restart_servers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-SOURCE_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
-CHROOT_DIR=$SOURCE_ROOT/.chroot
-
-sudo schroot -c securedrop -d / -- killall python
-sudo schroot -c securedrop -d /securedrop/securedrop -u dev -- python source.py &
-sudo schroot -c securedrop -d /securedrop/securedrop -u dev -- python journalist.py &
-
