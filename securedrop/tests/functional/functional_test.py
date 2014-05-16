a58b0deb2ce3b47a299cdc08045b1c9e77daaec7
Jack Singleton <git@jacksingleton.com>
2014-05-15 19:53:04 -0700

    adding signal handler functional tests to dump traceback. should help debug when they freeze

--- securedrop/tests/functional/functional_test.py
+++ securedrop/tests/functional/functional_test.py
@@ -15,6 +15,9 @@ import journalist
 import test_setup
 import urllib2
 
+import signal
+import traceback
+
 class FunctionalTest():
 
     def _unused_port(self):
@@ -25,6 +28,8 @@ class FunctionalTest():
         return port
 
     def setUp(self):
+        signal.signal(signal.SIGUSR1, lambda _, s: traceback.print_stack(s))
+
         test_setup.create_directories()
         self.gpg = test_setup.init_gpg()
         test_setup.init_db()
