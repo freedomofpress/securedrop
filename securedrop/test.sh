4cabcf9c10ce6a3afa6a3f208ba23da5e4908aaf
Charlie Austin <charlie@loyal3.com>
2014-05-31 14:51:25 -0700

    Trying to create unit tests.

--- securedrop/test.sh
+++ securedrop/test.sh
@@ -9,6 +9,6 @@ fi
 export PYTHONPATH=./tests
 
 # -f makes unittest fail fast, so we can use && to avoid burying test failures
-python -m unittest -fv tests.test_unit && python -m unittest -fv tests.test_journalist  && python -m unittest -fv tests.functional.submit_and_retrieve_message && python -m unittest -fv tests.functional.submit_and_retrieve_file
+python -m unittest discover ./tests/unit_tests/ -fv -p 'test_*.py' && python -m unittest -fv tests.test_integration && python -m unittest -fv tests.functional.submit_and_retrieve_message && python -m unittest -fv tests.functional.submit_and_retrieve_file
 
 
