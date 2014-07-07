527638d55752c3a036b8a94d235fd9bb97815125
dolanjs <dolanjs@gmail.com>
2014-07-07 15:00:55 -0700

    removes basic auth and apache module for 2fa due to sec bug in apache module

--- install_files/scripts/add-journalists.sh
+++ install_files/scripts/add-journalists.sh
@@ -15,22 +15,6 @@ if [[ ! "$(grep "$journalist_user" $basedir/etc/tor/torrc)" ]]; then
     sed -i "/HiddenServiceAuthorizeClient/s/$/,$journalist_user/" $basedir/etc/tor/torrc
 fi
 
-# Create pw and 2fa code for journalist to access document interface
-gauth_dir=/var/chroot/document/var/www/securedrop/gauth
-gen_2fa_code() {
-    touch $gauth_dir/$journalist_user
-    gauthcode=$(cat /dev/urandom | tr -dc 'A-Z' | head -c 16) 
-    gauthpw=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 10)
-    cat > $gauth_dir/$journalist_user << EOF
-$gauthcode
-"PASSWORD=$gauthpw
-EOF
-}
-
-    if [ ! -f $gauth_dir/$journalist_user ]; then
-        gen_2fa_code
-    fi
-
 schroot -c document -u root --directory / -- service tor reload
 echo "sleep 10 sec to wait for cert to be created"
 sleep 10
@@ -45,9 +29,5 @@ if [ -f $basedir/var/lib/tor/hidden_service/hostname ]; then
     echo "$red $journalist_user$blue's ssh ATHS address is$red $ONION_ADDRESS $normalcolor"
     echo "$red $journalist_user$blue's torrc config line is:"
     echo "$red HidServAuth $ONION_ADDRESS $AUTH_VALUE # $journalist_user$normalcolor"
-    twofactor_secret="$(sed -n 1p $gauth_dir/$journalist_user)"
-    doc_int_pw="$(awk -F'[/=]' '/'\"PASSWORD'/ {print $2}' $gauth_dir/$journalist_user)"
-    echo "$red $journalist_user$blue's 2 factor authentication secret code is $red$twofactor_secret$normalcolor"
-    echo "$red $journalist_user$blue's password for the document interface is $red$doc_int_pw
     echo "$bold$blue################################################################################$normalcolor"
 fi
