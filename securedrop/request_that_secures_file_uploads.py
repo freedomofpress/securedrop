a789baa8f86d2b6f179fd4a6882e949690332c5a
Garrett Robinson <garrett@pressfreedomfoundation.org>
2014-07-11 20:15:23 -0700

    subclass BytesIO and extend .close to zerofill the underlying buffer

--- securedrop/request_that_secures_file_uploads.py
+++ securedrop/request_that_secures_file_uploads.py
@@ -1,5 +1,34 @@
 from flask import wrappers
-from io import BytesIO
+import io
+
+class ZeroFillStream(io.BytesIO):
+
+    def close(self):
+        """Zero fill the underlying buffer before closing.
+
+        Note that you cannot rely on the garbage collector to get rid of this
+        in a timely manner. For best results, call .close() explicitly when you
+        are done, or use a context manager."""
+        #
+        # XXX this might not be sufficient. As data is repeatedly .written to
+        # BytesIO, it is a periodically re-allocated with an underlying call to
+        # PyMem_Realloc. That means that fragments of plaintext will escape our
+        # grasp and become impossible to zero-fill.
+        # 
+        # TODO test with the poc from the audit
+        #
+        # The only potential fix here is to create our own secure stream data
+        # type in C, which is essentially a BytesIO, but zerofills the
+        # underlying buffer on realloc and free.
+        #
+        self.seek(io.SEEK_END)
+        data_len = self.tell()
+        self.seek(io.SEEK_SET)
+        self.write('\0' * data_len)
+        super(ZeroFillStream, self).close()
+
+def create_secure_file_stream():
+    return BytesIO()
 
 class RequestThatSecuresFileUploads(wrappers.Request):
 
@@ -15,6 +44,3 @@ class RequestThatSecuresFileUploads(wrappers.Request):
                                            self.max_content_length,
                                            self.parameter_storage_class)
 
-
-def create_secure_file_stream():
-    return BytesIO()
