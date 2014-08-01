f8187ab5758711754114ffc85baa1846dd731c9c
Garrett Robinson <garrett@pressfreedomfoundation.org>
2014-07-31 21:14:01 -0700

    fix broken functional tests

--- securedrop/tests/functional/source_navigation_steps.py
+++ securedrop/tests/functional/source_navigation_steps.py
@@ -39,7 +39,7 @@ class SourceNavigationSteps():
 
             notification = self.driver.find_element_by_css_selector( 'p.notification')
             expected_notification = "Thanks! We received your document '%s'." % filebasename
-            self.assertEquals(expected_notification, notification.text)
+            self.assertIn(expected_notification, notification.text)
 
     def _source_submits_a_message(self):
         text_box = self.driver.find_element_by_css_selector('[name=msg]')
@@ -50,4 +50,4 @@ class SourceNavigationSteps():
         submit_button.click()
 
         notification = self.driver.find_element_by_css_selector( 'p.notification')
-        self.assertEquals('Thanks! We received your message.', notification.text)
+        self.assertIn('Thanks! We received your message.', notification.text)
