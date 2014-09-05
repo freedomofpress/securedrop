5a75bda70515dcddad6a774b25adbc45925fabb5
Garrett Robinson <garrett@pressfreedomfoundation.org>
2014-09-04 20:15:52 -0500

    Remove MAT-specific code from pip_update.sh

--- pip_update.sh
+++ pip_update.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Run in the virtual environment periodically to keep packages up-to-date
+# Run periodically to keep packages up-to-date
 
 set -e
 cd "`dirname $0`"
 
 cd securedrop/
-
-# Latest MAT version needs to be manually checked at https://mat.boum.org/files/
-LATEST_MAT=https://mat.boum.org/files/mat-0.4.2.tar.gz
 
 # Auto-install updated packages
 sudo pip-review --auto
@@ -18,8 +15,5 @@ touch requirements.txt
 
 # Dump updated version numbers to *requirements.txt
 pip-dump
-
-# pip-dump removes URLs for some reason. Put them back.
-echo $LATEST_MAT | tee -a "source-requirements.txt" "document-requirements.txt" >/dev/null
 
 rm requirements.txt
