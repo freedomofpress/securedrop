c99c5b7b282ec1414cc6083a456b41f86608d8fe
David Stainton <dstainton415@gmail.com>
2014-09-24 00:36:13 +0000

    Improve tor spect test slightly

--- spec-tests/spec/localhost/tor_spec.rb
+++ spec-tests/spec/localhost/tor_spec.rb
@@ -6,5 +6,10 @@ end
 
 describe file('/etc/tor/torrc') do
   it { should be_file }
-  its(:content) { should match /SocksPort 0/ }
+  its(:content) { should contain 'ReachableAddresses *:80,*:8080,*:443,*:8443,*:9001,*:9030' }
 end
+
+describe command('service tor status') do
+  it { should return_exit_status 0 }
+end
+
