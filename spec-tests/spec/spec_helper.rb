6b877386e99d7e69ee16dad7f66734f6170204aa
David Stainton <dstainton415@gmail.com>
2014-09-24 21:51:23 +0000

    Teach serverspec to parse ansible YAML config file
    Here we demonstrate how to parse the ansible external yaml configuration
    and use it to supply needed info for our test. In this case we check
    the torrc for the presence of authenticated tor hidden service user names
    for document and ssh users.

--- spec-tests/spec/spec_helper.rb
+++ spec-tests/spec/spec_helper.rb
@@ -2,12 +2,11 @@ require 'serverspec'
 
 include SpecInfra::Helper::Exec
 include SpecInfra::Helper::DetectOS
+include Serverspec::Helper::Properties
+require 'yaml'
+
+properties = YAML.load_file('../install_files/ansible-base/secureDropConf.yml')
 
 RSpec.configure do |c|
-  if ENV['ASK_SUDO_PASSWORD']
-    require 'highline/import'
-    c.sudo_password = ask("Enter sudo password: ") { |q| q.echo = false }
-  else
-    c.sudo_password = ENV['SUDO_PASSWORD']
-  end
+  set_property properties
 end
