6e34229347dc7d2532f379d504c7353587e1453a
maxamg <maxamg@gmail.com>
2014-10-02 19:01:42 -0700

    fixing up initial spec set

--- spec-tests/spec/spec_helper.rb
+++ spec-tests/spec/spec_helper.rb
@@ -1,12 +1,1 @@
-require 'serverspec'
-
-include SpecInfra::Helper::Exec
-include SpecInfra::Helper::DetectOS
-include Serverspec::Helper::Properties
-require 'yaml'
-
-properties = YAML.load_file('../install_files/ansible-base/secureDropConf.yml')
-
-RSpec.configure do |c|
-  set_property properties
-end
+spec_helper.rb.vagrant
