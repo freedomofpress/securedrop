ff13a38a7d72280e78a7c079c245ef83c1af17f2
James <james@freedom.press>
2014-10-14 14:10:23 -0700

    removed the old ansible-securedrop role

--- install_files/ansible-base/roles/ansible-securedrop/templates/send_encrypted_alarm.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-/usr/bin/formail -I "" | /usr/bin/gpg --homedir /var/ossec/.gnupg --trust-model always -ear "{{ ossec_gpg_fpr }}" | /usr/bin/mail -s "\$SUBJECT" {{ ossec_alert_email }}
-
