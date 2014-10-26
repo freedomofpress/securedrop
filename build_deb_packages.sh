29ed7657ca718d49a540f677542434603a3a6e4d
James <james@freedom.press>
2014-10-25 20:21:01 -0700

    Added a control file required to build the securedrop-grsec meta package.
    removed the ossec and securedrop app portions from the build script since they are different now.
    
    ```
    vagrant up mon-build --no-provision
    sudo su
    cd /vagrant/
    ./build_deb_packages.sh
    ```
    
    dpkg does not resolve dependencies so testing is a 3 part process:
    
    ```
    dpkg -i /tmp/build/securedrop-grsec-0.3-amd64.deb
    apt-get -fy install
    dpkg -i /tmp/build/securedrop-grsec-0.3-amd64.deb
    ```
    
    There are other tools that you can install and use that do handle package dependencies for local deb packages.

--- build_deb_packages.sh
+++ build_deb_packages.sh
@@ -3,66 +3,37 @@
 
 set -e
 set -x
-MY_PATH="$(dirname $0)"
-MY_PATH=$(cd $MY_PATH && pwd)
-BUILD_PATH="$MY_PATH/build"
+BUILD_PATH="/tmp/build"
 SD_VERSION=${1:-0.3}
 SD_ARCH=${2:-amd64}
 umask 022
 
-apt-get install libssl-dev python-pip python-dev  -y
-pip install wheel
-
-if [ ! -d "$BUILD_PATH" ]; then
+if [ ! -d $BUILD_PATH ]; then
     mkdir $BUILD_PATH
 fi
 
-build_app_deb() {
+apt-key add /vagrant/install_files/ansible-base/roles/common/files/fpf-signing-key.pub
+
+if [ ! -f /etc/apt/sources.list.d/fpf.list ]; then
+    echo "deb [arch=amd64] https://apt.pressfreedomfoundation.org/ trusty main" > /etc/apt/sources.list.d/fpf.list
+    apt-get update
+fi
+
+build_meta() {
     PACKAGE_NAME="$1"
     PACKAGE_DIR="$BUILD_PATH/$PACKAGE_NAME-$SD_VERSION-$SD_ARCH"
     if [ -d $PACKAGE_DIR ]; then
         rm -R $PACKAGE_DIR
+        mkdir -p $PACKAGE_DIR
     fi
 
-    # move the deb package to build dir
-    cp -R $MY_PATH/install_files/securedrop-app-code/ $PACKAGE_DIR
-
-    # move the app code to correct directory
-    mkdir -p $PACKAGE_DIR/var/www
-    cp -R $MY_PATH/securedrop/ $PACKAGE_DIR/var/www/
-
-    # move pip wheel dependencies to correct location
-    pip wheel -r securedrop/requirements/securedrop-requirements.txt
-    mkdir -p $PACKAGE_DIR/var/securedrop
-    mv wheelhouse $PACKAGE_DIR/var/securedrop/
-
-    # move the apparmor profiles to the correct location
-    mkdir -p $PACKAGE_DIR/etc/apparmor.d
-    cp $MY_PATH/install_files/ansible-base/roles/ansible-secureDrop-AppHardening/files/usr.* $PACKAGE_DIR/etc/apparmor.d/
-    gzip -9 $PACKAGE_DIR/usr/share/doc/$PACKAGE_NAME/changelog.Debian
+    cp -r /vagrant/install_files/securedrop-grsec/DEBIAN $PACKAGE_DIR/DEBIAN
 
     # Create the deb package
     dpkg-deb --build $PACKAGE_DIR
     cp $BUILD_PATH/$PACKAGE_NAME-$SD_VERSION-$SD_ARCH.deb $MY_PATH/
 }
 
-build_ossec_agent() {
-    PACKAGE_NAME="$1"
-    PACKAGE_DIR="$BUILD_PATH/$PACKAGE_NAME-$SD_VERSION-$SD_ARCH"
-    if [ -d $PACKAGE_DIR ]; then
-        rm -R $PACKAGE_DIR
-    fi
 
-    # move the deb package to build dir
-    cp -R $MY_PATH/install_files/$PACKAGE_NAME/ $PACKAGE_DIR
-
-    gzip -9 $PACKAGE_DIR/usr/share/doc/$PACKAGE_NAME/changelog.Debian
-
-    # Create the deb package
-    dpkg-deb --build $PACKAGE_DIR
-    cp $BUILD_PATH/$PACKAGE_NAME-$SD_VERSION-$SD_ARCH.deb $MY_PATH/
-}
-
-build_app_deb securedrop-app-code
-build_ossec_agent ossec-agent
+build_meta securedrop-grsec
 exit 0
