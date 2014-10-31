490b5b72e758eab32860c1be4d562debf1f3bd90
Garrett Robinson <garrett.f.robinson@gmail.com>
2014-10-31 12:07:35 -0400

    Load files from absolute paths so this can be run from anywhere

--- migration_scripts/0.3/crypto_util.py
+++ migration_scripts/0.3/crypto_util.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 # Minimal set of functions and variables from 0.2.1's crypto_util.py needed to
 # regenerate journalist designations from soure's filesystem id's.
+import os
 import random as badrandom
 
-nouns = file("nouns.txt").read().split('\n')
-adjectives = file("adjectives.txt").read().split('\n')
+# Find the absolute path relative to this file so this script can be run anywhere
+SRC_DIR = os.path.dirname(os.path.realpath(__file__))
+
+nouns = file(os.path.join(SRC_DIR, "nouns.txt")).read().split('\n')
+adjectives = file(os.path.join(SRC_DIR, "adjectives.txt")).read().split('\n')
 
 def displayid(n):
     badrandom_value = badrandom.WichmannHill()
