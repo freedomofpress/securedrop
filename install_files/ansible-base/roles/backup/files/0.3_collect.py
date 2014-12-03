716125d77925a689c9da4e4cdbd1d891266542ca
James <james@freedom.press>
2014-12-03 09:39:34 -0800

    made the recommended changes from @garrettr

--- install_files/ansible-base/roles/backup/files/0.3_collect.py
+++ install_files/ansible-base/roles/backup/files/0.3_collect.py
@@ -14,31 +14,31 @@ import zipfile
 from datetime import datetime
 import functools
 
-securedrop_code = "/var/www/securedrop"
-securedrop_data = "/var/lib/securedrop"
-tor_services    = "/var/lib/tor/services"
-tor_config      = "/etc/tor/torrc"
+SECUREDROP_CODE = "/var/www/securedrop"
+SECUREDROP_DATA = "/var/lib/securedrop"
+TOR_SERVICES    = "/var/lib/tor/services"
+TOR_CONFIG      = "/etc/tor/torrc"
 
 def collect_config_file(zf):
-    config_file_path = os.path.join(securedrop_code, "config.py")
+    config_file_path = os.path.join(SECUREDROP_CODE, "config.py")
     zf.write(config_file_path)
 
-def collect_securedrop_data(zf):
+def collect_SECUREDROP_DATA(zf):
     # The store and key dirs are shared between both interfaces
-    for root, dirs, files in os.walk(securedrop_data):
+    for root, dirs, files in os.walk(SECUREDROP_DATA):
         for name in files:
             zf.write(os.path.join(root, name))
 
 def collect_custom_header_image(zf):
     # The custom header image is copied over the deafult `static/i/logo.png`.
-    zf.write(os.path.join(securedrop_code, "static/i/logo.png"))
+    zf.write(os.path.join(SECUREDROP_CODE, "static/i/logo.png"))
 
 def collect_tor_files(zf):
     # All of the tor hidden service private keys are stored in the THS specific
     # subdirectory `/var/lib/tor/services` backing up this directory will back
     # up all of the THS and ATHS required keys needed to restore all the hidden
     # services on that system.
-    for root, dirs, files in os.walk(tor_services):
+    for root, dirs, files in os.walk(TOR_SERVICES):
         for name in files:
             zf.write(os.path.join(root, name))
 
@@ -46,15 +46,15 @@ def collect_tor_files(zf):
     # the ATHS info. These names are also in the the specific client_key file
     # but backing up this file makes it easier than parsing the files during a
     # restore.
-    zf.write(tor_config)
+    zf.write(TOR_CONFIG)
 
 def main():
     # name append a timestamp to the sd-backup zip filename
     dt = str(datetime.utcnow().strftime("%Y-%m-%d--%H-%M-%S"))
-    zf_fn = 'sd-backup-'+dt+'.zip'
+    zf_fn = 'sd-backup-{}.zip'.format(dt)
     with zipfile.ZipFile(zf_fn, 'w') as zf:
         collect_config_file(zf)
-        collect_securedrop_data(zf)
+        collect_SECUREDROP_DATA(zf)
         collect_custom_header_image(zf)
         collect_tor_files(zf)
     print zf_fn
