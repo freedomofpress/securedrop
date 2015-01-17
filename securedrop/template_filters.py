4991d282b56e1e3eafde68249abdd40d2adc209f
Ali Clark <ali@clark.gb.net>
2015-01-17 19:14:50 +0000

    imprecise source age strings, to nearest [1, 1-1.5, 2, 3, 4, ...] days
    The first smallest range is "less than a day", so we can truncate HH:MM
    information and this description will still make sense.
    
    "a day ago" only applies to the period of 1 to 1.5 days, because after that we
    use rounded descriptions, ie. "2 days" is actually the range 1.5 to 2.5 days.

--- securedrop/template_filters.py
+++ securedrop/template_filters.py
@@ -3,33 +3,41 @@ from datetime import datetime
 from jinja2 import Markup, escape
 
 
-def datetimeformat(dt, fmt=None, relative=False):
+def datetimeformat(dt, fmt=None, relative=False, imprecise=False):
     """Template filter for readable formatting of datetime.datetime"""
-    fmt = fmt or '%b %d, %Y %I:%M %p'
+    if fmt and imprecise:
+        raise ValueError("'imprecise' cannot be used with 'fmt'")
+    fmt = fmt or ('%b %d, %Y' if imprecise else '%b %d, %Y %I:%M %p')
     if relative:
-        time_difference = _relative_timestamp(dt)
+        time_difference = _relative_timestamp(dt, imprecise)
         if time_difference:
             return '{} ago'.format(time_difference)
     return dt.strftime(fmt)
 
 
-def _relative_timestamp(dt):
+def _relative_timestamp(dt, imprecise=False):
     """"
     Format a human readable relative time for timestamps up to 30 days old
     """
     delta = datetime.utcnow() - dt
     diff = (delta.microseconds + (delta.seconds + delta.days * 24 * 3600) * 1e6) / 1e6
-    if diff < 45:
-        return '{} second{}'.format(int(diff), '' if int(diff) == 1 else 's')
-    elif diff < 90:
-        return 'a minute'
-    elif diff < 2700:
-        return '{} minutes'.format(int(max(diff / 60, 2)))
-    elif diff < 5400:
-        return 'an hour'
-    elif diff < 79200:
-        return '{} hours'.format(int(max(diff / 3600, 2)))
-    elif diff < 129600:
+
+    if imprecise:
+        if diff < 86400:
+            return 'less than a day'
+    else:
+        if diff < 45:
+            return '{} second{}'.format(int(diff), '' if int(diff) == 1 else 's')
+        elif diff < 90:
+            return 'a minute'
+        elif diff < 2700:
+            return '{} minutes'.format(int(max(diff / 60, 2)))
+        elif diff < 5400:
+            return 'an hour'
+        elif diff < 79200:
+            return '{} hours'.format(int(max(diff / 3600, 2)))
+
+    if diff < 129600:
         return 'a day'
     elif diff < 2592000:
         return '{} days'.format(int(max(diff / 86400, 2)))
