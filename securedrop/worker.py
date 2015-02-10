7cdbf0c989109c089c758d28b68f5f1925ebf388
Garrett Robinson <garrett@freedom.press>
2015-02-10 10:36:23 -0800

    Increase job timeout for securely deleting files

--- securedrop/worker.py
+++ securedrop/worker.py
@@ -5,7 +5,8 @@ from rq import Queue
 
 queue_name = 'test' if os.environ.get('SECUREDROP_ENV') == 'test' else 'default'
 
-q = Queue(name=queue_name, connection=Redis())
+# `srm` can take a long time on large files, so allow it run for up to an hour
+q = Queue(name=queue_name, connection=Redis(), default_timeout=3600)
 
 def enqueue(*args, **kwargs):
     q.enqueue(*args, **kwargs)
