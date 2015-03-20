a6aa41aa0aa2d3abc16e54f889916a0c620d8216
Kevin Gallagher <kevin@freedom.press>
2015-03-20 06:07:20 +0000

    Persistent SecureDrop config

--- tails_files/install.sh
+++ tails_files/install.sh
@@ -11,6 +11,10 @@ INSTALL_DIR=$PERSISTENT/.securedrop
 ADDITIONS=$INSTALL_DIR/torrc_additions
 SCRIPT_PY=$INSTALL_DIR/securedrop_init.py
 SCRIPT_BIN=$INSTALL_DIR/securedrop_init
+DOTFILES=/live/persistence/TailsData_unlocked/dotfiles
+DESKTOP=/home/amnesia/Desktop
+DOCUMENT=`cat $PERSISTENT/securedrop/install_files/ansible-base/app-document-aths | cut -d ' ' -f 1`
+SOURCE=`cat $PERSISTENT/securedrop/install_files/ansible-base/app-source-ths`
 
 mkdir -p $INSTALL_DIR
 
@@ -19,9 +23,11 @@ apt-get update
 apt-get install -y build-essential
 gcc -o $SCRIPT_BIN securedrop_init.c
 
-# copy launcher and icon
+# copy icon and launchers
 cp securedrop_icon.png $INSTALL_DIR
-cp SecureDrop\ Init.desktop $PERSISTENT
+cp document.desktop $INSTALL_DIR
+cp source.desktop $INSTALL_DIR
+cp .xsessionrc $INSTALL_DIR
 
 # copy securedrop_init.py script
 cp securedrop_init.py $SCRIPT_PY
@@ -41,15 +47,24 @@ chown root:root $ADDITIONS
 chmod 400 $ADDITIONS
 
 chown amnesia:amnesia $INSTALL_DIR/securedrop_icon.png
-chmod 600 $INSTALL_DIR/securedrop_icon.png
-chown amnesia:amnesia $PERSISTENT/SecureDrop\ Init.desktop
-chmod 700 $PERSISTENT/SecureDrop\ Init.desktop
-sudo -u amnesia mkdir -p /live/persistence/TailsData_unlocked/dotfiles/Desktop
-cp -p $PERSISTENT/SecureDrop\ Init.desktop /live/persistence/TailsData_unlocked/dotfiles/Desktop/
-cp -p $PERSISTENT/SecureDrop\ Init.desktop /home/amnesia/Desktop/
+chmod 600 $INSTALL_DIR/securedrop_icon.png 
+
+chown amnesia:amnesia $INSTALL_DIR/document.desktop $INSTALL_DIR/source.desktop $INSTALL_DIR/.xsessionrc
+chmod 700 $INSTALL_DIR/document.desktop $INSTALL_DIR/source.desktop $INSTALL_DIR/.xsessionrc
+
+echo "Exec=/usr/local/bin/tor-browser $DOCUMENT" >> $INSTALL_DIR/document.desktop
+echo "Exec=/usr/local/bin/tor-browser $SOURCE" >> $INSTALL_DIR/source.desktop
+
+cp -p $INSTALL_DIR/document.desktop $DESKTOP
+cp -p $INSTALL_DIR/source.desktop $DESKTOP
+sudo -u amnesia mkdir -p $DOTFILES/Desktop
+cp -p $DESKTOP/source.desktop $DOTFILES/Desktop
+cp -p $DESKTOP/document.desktop $DOTFILES/Desktop
+cp -p $INSTALL_DIR/.xsessionrc $DOTFILES
+
+$INSTALL_DIR/securedrop_init
 
 echo ""
-echo "Successfully configured the auto-launcher for the document interface!"
-echo "In the future, automatically set up Tor to access the document interface by double-clicking the \"SecureDrop Init\" icon in your Persistent folder."
-echo "You will see a notification appear in the top right corner of your screen when it completes."
+echo "Successfully configured Tor and set up desktop bookmarks for SecureDrop!"
+echo "You will see a notification appear in the top-right corner of your screen."
 echo ""
