72137280b2496c647ac27c6f03c5e16543ac52c7
Kevin Gallagher <kevin@freedom.press>
2015-03-26 14:40:03 -0700

    Persistent automatic SecureDrop Tails setup

--- tails_files/install.sh
+++ tails_files/install.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
+# SecureDrop persistent setup script for Tails 
 
 # check for root
 if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root" 1>&2
-    exit 1
+	echo "This script must be run as root" 1>&2
+	exit 1
 fi
 
-PERSISTENT=/home/amnesia/Persistent
+# set paths and variables
+HOMEDIR=/home/amnesia
+PERSISTENT=$HOMEDIR/Persistent
 INSTALL_DIR=$PERSISTENT/.securedrop
 ADDITIONS=$INSTALL_DIR/torrc_additions
 SCRIPT_PY=$INSTALL_DIR/securedrop_init.py
 SCRIPT_BIN=$INSTALL_DIR/securedrop_init
 DOTFILES=/live/persistence/TailsData_unlocked/dotfiles
+TAILSCFG=/live/persistence/TailsData_unlocked
+DESKTOP=$HOMEDIR/Desktop
+ANSIBLE=$PERSISTENT/securedrop/install_files/ansible-base
+SSH_ALIASES=false
+
+# check for persistence
+if [ ! -d "$TAILSCFG" ]
+	echo "This script must be run on Tails with a persistent volume." 1>&2
+	exit 1
+fi
+
+# detect whether admin or journalist
+if [ -f $ANSIBLE/app-document-aths ]; then
+	ADMIN=true
+else
+	ADMIN=false
+fi
 
 mkdir -p $INSTALL_DIR
 
@@ -20,15 +40,44 @@ apt-get update
 apt-get install -y build-essential
 gcc -o $SCRIPT_BIN securedrop_init.c
 
-# copy init scripts
-cp securedrop_init.py $SCRIPT_PY
-cp .xsessionrc $INSTALL_DIR
-
+# copy icon, launchers and scripts
 cp securedrop_icon.png $INSTALL_DIR
+cp document.desktop $INSTALL_DIR
+cp source.desktop $INSTALL_DIR
+cp .xsessionrc $INSTALL_DIR
+cp securedrop_init.py $SCRIPT_PY
 
-# prepare torrc_additions
-cp torrc_additions $ADDITIONS
-gedit $ADDITIONS > /dev/null 2>&1
+if $ADMIN; then
+  DOCUMENT=`cat $ANSIBLE/app-document-aths | cut -d ' ' -f 2`
+  SOURCE=`cat $ANSIBLE/app-source-ths`
+  APPSSH=`cat $ANSIBLE/app-ssh-aths | cut -d ' ' -f 2`
+  MONSSH=`cat $ANSIBLE/mon-ssh-aths | cut -d ' ' -f 2`
+  cat $ANSIBLE/app-ssh-aths $ANSIBLE/mon-ssh-aths $ANSIBLE/app-document-aths >> $ADDITIONS
+  if [[ -d "$HOMEDIR/.ssh" && ! -f "$HOMEDIR/.ssh/config" ]]; then
+    # create SSH host aliases and install them
+    SSHUSER=$(zenity --entry --title="Admin SSH user" --window-icon=$INSTALL_DIR/securedrop_icon.png --text="Enter your username on the App and Monitor server:")
+    cat > $INSTALL_DIR/ssh_config <<EOL
+Host app
+  Hostname $APPSSH
+  User $SSHUSER
+Host mon
+  Hostname $MONSSH
+  User $SSHUSER
+EOL
+    chown amnesia:amnesia $INSTALL_DIR/ssh_config
+    chmod 600 $INSTALL_DIR/ssh_config
+    cp -p $INSTALL_DIR/ssh_config $HOMEDIR/.ssh/config
+    SSH_ALIASES=true
+  fi
+  # set ansible to auto-install
+  if ! grep -q 'ansible' "$TAILSCFG/live-additional-software.conf"; then
+    echo "ansible" >> $TAILSCFG/live-additional-software.conf
+  fi
+else
+# prepare torrc_additions (journalist)
+  cp torrc_additions $ADDITIONS
+  gedit $ADDITIONS > /dev/null 2>&1
+fi
 
 # set permissions
 chmod 755 $INSTALL_DIR
@@ -39,19 +88,56 @@ chown root:root $SCRIPT_PY
 chmod 700 $SCRIPT_PY
 chown root:root $ADDITIONS
 chmod 400 $ADDITIONS
-chown amnesia:amnesia $INSTALL_DIR/securedrop_icon.png
-chmod 600 $INSTALL_DIR/securedrop_icon.png
-chown amnesia:amnesia $INSTALL_DIR/.xsessionrc
-chmod 700 $INSTALL_DIR/.xsessionrc
 
-# make the init script persistent
+chown amnesia:amnesia $INSTALL_DIR/securedrop_icon.png
+chmod 600 $INSTALL_DIR/securedrop_icon.png 
+chown amnesia:amnesia $INSTALL_DIR/document.desktop $INSTALL_DIR/source.desktop $INSTALL_DIR/.xsessionrc
+chmod 700 $INSTALL_DIR/document.desktop $INSTALL_DIR/source.desktop $INSTALL_DIR/.xsessionrc
+
+# get addresses (journalist)
+if ! $ADMIN; then
+	INTERFACES=$(zenity --forms --title="Desktop shortcut setup" --window-icon=$INSTALL_DIR/securedrop_icon.png --text="Enter each interface's .onion address." \
+	--separator="," --width=500 --add-entry="Document Interface:" --add-entry="Source Interface:")
+	DOC=$(awk -F, '{print $1}' <<<$INTERFACES)
+	SRC=$(awk -F, '{print $2}' <<<$INTERFACES)
+	DOCUMENT="${DOC#http://}"
+	SOURCE="${SRC#http://}"
+fi
+
+# make the shortcuts
+echo "Exec=/usr/local/bin/tor-browser $DOCUMENT" >> $INSTALL_DIR/document.desktop
+echo "Exec=/usr/local/bin/tor-browser $SOURCE" >> $INSTALL_DIR/source.desktop
+
+# copy launchers to desktop
+cp -p $INSTALL_DIR/document.desktop $DESKTOP
+cp -p $INSTALL_DIR/source.desktop $DESKTOP
+
+# make it all persistent
+sudo -u amnesia mkdir -p $DOTFILES/Desktop
+cp -p $DESKTOP/source.desktop $DOTFILES/Desktop
+cp -p $DESKTOP/document.desktop $DOTFILES/Desktop
 cp -p $INSTALL_DIR/.xsessionrc $DOTFILES
 
-# run the torrc update
+# set torrc and reload Tor
 $INSTALL_DIR/securedrop_init
 
+# finished
 echo ""
-echo "Successfully configured the persistent initialization script for SecureDrop's Tor configuration!"
-echo "You will see a notification appear in the top-right corner of your screen when it runs."
+echo "Successfully configured Tor and set up desktop bookmarks for SecureDrop!"
+echo "You will see a notification appear in the top-right corner of your screen."
+echo ""
+echo "The Document Interface's Tor onion URL is: http://$DOCUMENT"
+echo "The Source Interfaces's Tor onion URL is: http://$SOURCE"
+if $ADMIN; then
+	echo ""
+	echo "The App Server's SSH hidden service address is:"
+	echo $APPSSH
+	echo "The Monitor Server's SSH hidden service address is:"
+	echo $MONSSH
+	if $SSH_ALIASES; then
+		echo ""
+		echo "SSH aliases are set up. You can use them with 'ssh app' and 'ssh mon'"
+	fi
+fi
 echo ""
 exit 0
