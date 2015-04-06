5ddf24bdacc47df8e1ca91251d24e112379babd9
James <james@freedom.press>
2015-04-06 11:57:13 -0700

    snap_ci pinned.sh comments why we use older version of vagrant_digitalocean plugin

--- snap_ci/pinned.sh
+++ snap_ci/pinned.sh
@@ -7,6 +7,9 @@
 [[ -f ${SNAP_CACHE_DIR}/$vagrant_rpm ]] || wget https://dl.bintray.com/mitchellh/vagrant/$vagrant_rpm -O ${SNAP_CACHE_DIR}/$vagrant_rpm
 [[ -x /usr/bin/vagrant ]] || sudo -E rpm -ivh ${SNAP_CACHE_DIR}/$vagrant_rpm
 # TODO: Check for vagrant plugins before installing them.
+# An older version of digital_ocean plugin is used because of an issue with the
+# current version that doesn't support using snapshots.
+# https://github.com/smdahlen/vagrant-digitalocean/issues/187
 vagrant plugin install vagrant-digitalocean --plugin-version '0.7.0'
 vagrant plugin install vagrant-hostmanager
 [[ -f ${SNAP_CACHE_DIR}/digital_ocean.box ]] || wget https://github.com/smdahlen/vagrant-digitalocean/raw/master/box/digital_ocean.box -O ${SNAP_CACHE_DIR}/digital_ocean.box
