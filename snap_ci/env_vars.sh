6c497aa244689616c4e214cb119013aceb3a53db
Conor Schaefer <conor@freedom.press>
2015-04-07 10:56:30 -0700

    adds DO_IMAGE_NAME to env vars

--- snap_ci/env_vars.sh
+++ snap_ci/env_vars.sh
@@ -5,3 +5,7 @@ export vagrant_rpm="vagrant_1.7.2_x86_64.rpm"
 export DO_SSH_KEYFILE_NAME="Vagrant"
 export DO_SIZE="1gb"
 export DO_REGION="sfo1"
+export DO_IMAGE_NAME="ubuntu-14.04-2015-04-06"
+export APP_IMAGE_NAME="$DO_IMAGE_NAME"
+export MON_IMAGE_NAME="$DO_IMAGE_NAME"
+
