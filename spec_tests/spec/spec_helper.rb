8bc1d395b782fe01d8d8a71aea5091bdbb9fe381
Conor Schaefer <conor@freedom.press>
2015-04-07 13:38:33 -0700

    renames metadata[:example_group_block] -> metadata[:block]
    resolves deprecation warning:
    
    Deprecation Warnings:
    
    `metadata[:example_group_block]` is deprecated. Use `metadata[:block]` instead.
    Called from ./spec_tests/spec/spec_helper.rb:18:in `yield'.

--- spec_tests/spec/spec_helper.rb
+++ spec_tests/spec/spec_helper.rb
@@ -12,7 +12,7 @@ RSpec.configure do |c|
     c.sudo_password = ENV['SUDO_PASSWORD']
   end
   c.before :all do
-    block = self.class.metadata[:example_group_block]
+    block = self.class.metadata[:block]
     if RUBY_VERSION.start_with?('1.8')
       file = block.to_s.match(/.*@(.*):[0-9]+>/)[1]
     else
