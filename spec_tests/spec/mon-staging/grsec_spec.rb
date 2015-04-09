feb69f86ac389aa1c97259617b8d245040d79ce9
Conor Schaefer <conor@freedom.press>
2015-04-09 16:29:50 -0700

    adds (copies) grsec spectests to mon-staging

---
+++ spec_tests/spec/mon-staging/grsec_spec.rb
@@ -0,0 +1,36 @@
+#require 'spec_helper'
+
+['securedrop-grsec'].each do |pkg|
+  describe package(pkg) do
+    it { should be_installed }
+  end
+end
+
+# Check that the system is booted in grsec
+describe command("uname -r") do
+  its(:stdout) { should match /grsec$/ }
+end
+
+# Check that the grsec sysctl settings are correct
+describe 'Grsecurity kernel parameters' do
+  context linux_kernel_parameter('kernel.grsecurity.grsec_lock') do
+    its(:value) { should eq 1 }
+  end
+
+  context linux_kernel_parameter('kernel.grsecurity.rwxmap_logging') do
+    its(:value) { should eq 0 }
+  end
+end
+
+# Check that paxtest does not report anything vulnerable
+# Requires the package paxtest to be installed
+# The paxtest package is currently being installed in the app-test role
+describe command("paxtest blackhat") do
+  its(:stdout) { should_not match /vulnerable/ }
+end
+
+# Check pax flags for apache tor
+# paxctl -v /usr/sbin/apache2
+# paxctl -v /usr/sbin/tor
+# paxctl -v /usr/sbin/ntp
+# paxctl -v /usr/sbin/apt
