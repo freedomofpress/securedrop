1253a3af35bd235f4b2b02206051b918cc433cb8
Conor Schaefer <conor@freedom.press>
2015-04-09 17:48:11 -0700

    ports tor tests to v2 format

--- spec_tests/spec/app-staging/tor_spec.rb
+++ spec_tests/spec/app-staging/tor_spec.rb
@@ -6,10 +6,18 @@ end
 
 describe file('/etc/tor/torrc') do
   it { should be_file }
+  it { should be_mode '644' }
   its(:content) { should match "HiddenServiceAuthorizeClient stealth journalist" }
   its(:content) { should match "HiddenServiceAuthorizeClient stealth admin" }
 end
 
-describe command('sudo service tor status') do
-  it { should return_exit_status 0 }
+describe service('tor') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+# Likely overkill
+describe command('service tor status') do
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should match /tor is running/ }
 end
