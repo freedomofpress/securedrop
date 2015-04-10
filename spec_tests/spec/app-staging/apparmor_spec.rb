e1bd7549fed68aadab88a65cb6f3e5c0e53f01d4
Conor Schaefer <conor@freedom.press>
2015-04-09 17:32:05 -0700

    tweak apparmor spec tests for app-staging

--- spec_tests/spec/app-staging/apparmor_spec.rb
+++ spec_tests/spec/app-staging/apparmor_spec.rb
@@ -7,35 +7,38 @@
   end
 end
 
-# Required apparmor profiles configs
-#
-# SecureDrop apache apparmor profile
-describe file('/etc/apparmor.d/usr.sbin.apache2') do
-  it { should be_file }
-  it { should be_owned_by 'root' }
-  it { should be_mode '644' }
-  # The following line should not contain `complain`
-  # And should have the correct path for apache2
-  its(:content) { should match "/usr/sbin/apache2 {" }
-end
-
 # Check that apparmor is enabled.
 # The command returns error code if AppArmor not enabled
 describe command("aa-status --enabled") do
   its(:exit_status) { should eq 0 }
 end
 
-# Check that the expected profiles are present in the aa-status command.
-[ 'apache', 'tor', 'ntp'].each do |enforced_profile|
-  describe command("aa-status") do
-    it { should return_stdout /#{enforced_profile}/ }
+# SecureDrop apache apparmor profile
+# Staging role has two profiles in complain mode:
+# tor and apache2. Make sure the config file includes 
+# that flag, since restarting apparmor will load 
+# whatever's on disk
+['tor', 'apache2'].each do |complaining_process|
+  describe file("/etc/apparmor.d/usr.sbin.#{complaining_process}") do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_mode '644' }
+    its(:content) { should match /^\/usr\/sbin\/#{complaining_process} flags=\(complain\) \{/ }
   end
 end
 
-# Ensure that there are no processes in complain mode
+
+# Make sure that ONLY two profiles are in complain mode.
 describe command("aa-status --complaining") do
-  its(:stdout) { should eq 0 }
+  its(:stdout) { should eq "2\n" }
 end
+
+# Check that the expected profiles are present in the aa-status command.
+#[ 'apache', 'tor', 'ntp'].each do |enforced_profile|
+#  describe command("aa-status") do
+#    it { should return_stdout /#{enforced_profile}/ }
+#  end
+#end
 
 # Ensure that there are no processes that are unconfined but have a profile
 describe command("aa-status") do
