cc8982d1896a2344785bdf5ac964edfb80698b5d
Conor Schaefer <conor@freedom.press>
2015-04-10 11:03:37 -0700

    starts tests for validating apt config

---
+++ spec_tests/spec/app-staging/apt_packages_spec.rb
@@ -0,0 +1,26 @@
+#require 'spec_helper'
+
+['cron-apt', 'ntp', 'paxctl'].each do |pkg|
+  describe package(pkg) do
+    it { should be_installed }
+  end
+end
+
+# Check for cron-apt auto-update config
+describe file('/etc/cron-apt/action.d/0-update') do
+  it { should be_file }
+  it { should be_owned_by  'root' }
+  it { should be_mode '644' }
+  its(:content) { should match 'update -o quiet=2 -o Dir::Etc::SourceList=/etc/apt/security.list -o Dir::Etc::SourceParts=""' }
+end
+
+# Check for cron-apt security.list config
+describe file('/etc/cron-apt/action.d/5-security') do
+  it { should be_file }
+  it { should be_owned_by  'root' }
+  it { should be_mode '644' }
+  its(:content) { should match /^autoclean -y$/ }
+  its(:content) { should match "dist-upgrade -y -o APT::Get::Show-Upgraded=true -o Dir::Etc::SourceList=/etc/apt/security.list -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold"}
+  its(:content) { should match /^autoremove -y$/ }
+end
+
