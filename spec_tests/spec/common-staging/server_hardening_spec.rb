d280572e7a703f935cf2bc006b7ab3a9c2f2cd0a
Conor Schaefer <conor@freedom.press>
2015-04-14 12:53:19 -0700

    adds spec tests for sysctl flags to common

--- spec_tests/spec/common-staging/server_hardening_spec.rb
+++ spec_tests/spec/common-staging/server_hardening_spec.rb
@@ -1,3 +1,31 @@
+# declare desired sysctl flags
+sysctl_flags = {
+  'net.ipv4.tcp_max_syn_backlog' => 4096,
+  'net.ipv4.tcp_syncookies' => 1,
+  'net.ipv4.conf.all.rp_filter' => 1,
+  'net.ipv4.conf.all.accept_source_route' => 0,
+  'net.ipv4.conf.all.accept_redirects' => 0,
+  'net.ipv4.conf.all.secure_redirects' => 0,
+  'net.ipv4.conf.default.rp_filter' => 1,
+  'net.ipv4.conf.default.accept_source_route' => 0,
+  'net.ipv4.conf.default.accept_redirects' => 0,
+  'net.ipv4.conf.default.secure_redirects' => 0,
+  'net.ipv4.icmp_echo_ignore_broadcasts' => 1,
+  'net.ipv4.ip_forward' => 0,
+  'net.ipv4.conf.all.send_redirects' => 0,
+  'net.ipv4.conf.default.send_redirects' => 0,
+  'net.ipv6.conf.all.disable_ipv6' => 1,
+  'net.ipv6.conf.default.disable_ipv6' => 1,
+  'net.ipv6.conf.lo.disable_ipv6' => 1,
+}
+# ensure sysctl flags are set correctly
+describe command('sysctl --all') do
+  sysctl_flags.each do |sysctl_flag, value|
+    sysctl_flag_regex = Regexp.quote("#{sysctl_flag} = #{value}")
+    its(:stdout) { should match /^#{sysctl_flag_regex}$/ }
+  end
+end
+
 # ensure DNS server is named
 # TODO: nameserver var is hard-coded below. consider 
 # dynamically populated this var via spec_helper.
@@ -5,7 +33,6 @@ describe file('/etc/resolvconf/resolv.conf.d/base') do |resolvconf|
   it { should be_mode '644' }
   its(:content) { should match /^nameserver 8\.8\.8\.8$/ }
 end
-
 
 disabled_kernel_modules = [
   'bluetooth',
