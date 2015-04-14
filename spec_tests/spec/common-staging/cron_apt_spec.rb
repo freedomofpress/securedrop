f289aea63572afd63b5cf3fa5966fc0b37ddf47d
Conor Schaefer <conor@freedom.press>
2015-04-14 11:55:19 -0700

    adds a bunch of apt-related tests to common

--- spec_tests/spec/common-staging/cron_apt_spec.rb
+++ spec_tests/spec/common-staging/cron_apt_spec.rb
@@ -7,21 +7,52 @@
   end
 end
 
-# Check for cron-apt auto-update config
+# ensure custom cron-apt config file is present
+describe file('/etc/cron-apt/config') do
+  it { should be_file }
+  it { should be_owned_by  'root' }
+  it { should be_mode '644' }
+  its(:content) { should match /^SYSLOGON="always"$/ }
+  its(:content) { should match /^EXITON=error$/ }
+end
+
+# list desired security repositories
+security_repositories = [
+  'deb http://security.ubuntu.com/ubuntu trusty-security main',
+  'deb-src http://security.ubuntu.com/ubuntu trusty-security main',
+  'deb http://security.ubuntu.com/ubuntu trusty-security universe',
+  'deb-src http://security.ubuntu.com/ubuntu trusty-security universe',
+  'deb [arch=amd64] https://apt.freedom.press trusty main',
+  'deb http://deb.torproject.org/torproject.org trusty main',
+]
+# ensure custom security.list file is present
+describe file('/etc/apt/security.list') do
+  it { should be_file }
+  it { should be_owned_by  'root' }
+  it { should be_mode '644' }
+  security_repositories.each do |repo|
+    repo_regex = Regexp.quote(repo)
+    its(:content) { should match /^#{repo_regex}$/ }
+  end
+end
+
+# ensure cron-apt updates the security.list packages
 describe file('/etc/cron-apt/action.d/0-update') do
   it { should be_file }
   it { should be_owned_by  'root' }
   it { should be_mode '644' }
-  its(:content) { should match 'update -o quiet=2 -o Dir::Etc::SourceList=/etc/apt/security.list -o Dir::Etc::SourceParts=""' }
+  repo_regex = Regexp.quote('update -o quiet=2 -o Dir::Etc::SourceList=/etc/apt/security.list -o Dir::Etc::SourceParts=""')
+  its(:content) { should match /^#{repo_regex}$/ }
 end
 
-# Check for cron-apt security.list config
+# ensure cron-apt upgrades the security.list packages
 describe file('/etc/cron-apt/action.d/5-security') do
   it { should be_file }
   it { should be_owned_by  'root' }
   it { should be_mode '644' }
   its(:content) { should match /^autoclean -y$/ }
-  its(:content) { should match "dist-upgrade -y -o APT::Get::Show-Upgraded=true -o Dir::Etc::SourceList=/etc/apt/security.list -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold"}
+  config_regex = Regexp.quote('dist-upgrade -y -o APT::Get::Show-Upgraded=true -o Dir::Etc::SourceList=/etc/apt/security.list -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold')
+  its(:content) { should match /^#{config_regex}$/ }
   its(:content) { should match /^autoremove -y$/ }
 end
 
