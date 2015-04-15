267c68d1a734bc829096c7fbc660f3c560e4e43a
Conor Schaefer <conor@freedom.press>
2015-04-15 15:42:46 -0700

    adds tests for application dir mode and perms

--- spec_tests/spec/app-staging/securedrop_app_spec.rb
+++ spec_tests/spec/app-staging/securedrop_app_spec.rb
@@ -1,9 +1,25 @@
 #require 'spec_helper'
 
-['/var/www/securedrop'].each do |myDir|
-  describe file(myDir) do
+# ensure default apache html directory is absent
+describe command('/bin/bash -c "[[ ! -e /var/www/html  ]]"') do
+  its(:exit_status) { should eq 0 }
+end
+
+# declare securedrop app directories
+securedrop_app_directories = [
+  '/var/www/securedrop',
+  '/var/lib/securedrop',
+  '/var/lib/securedrop/store',
+  '/var/lib/securedrop/keys',
+  '/var/lib/securedrop/tmp',
+]
+
+# ensure securedrop app directories exist with correct permissions
+securedrop_app_directories.each do |securedrop_app_directory|
+  describe file(securedrop_app_directory) do
     it { should be_directory }
     it { should be_owned_by  'www-data' }
+    it { should be_grouped_into  'www-data' }
     it { should be_mode '700' }
   end
 end
