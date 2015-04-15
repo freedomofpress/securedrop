d96da4cf3c7d876878ed09afdfc735d9698d0679
Conor Schaefer <conor@freedom.press>
2015-04-15 12:00:45 -0700

    adds tests for ossec-agent packages and keyfile

--- spec_tests/spec/app-staging/ossec_agent_spec.rb
+++ spec_tests/spec/app-staging/ossec_agent_spec.rb
@@ -6,3 +6,18 @@ describe file('/etc/hosts') do
   # using the vagrant-hostmanager plugin. it may no longer be necessary
   its(:content) { should match /^10\.0\.1\.3  mon-staging securedrop-monitor-server-alias$/ }
 end
+
+# ensure custom ossec-agent package is installed
+describe package('securedrop-ossec-agent') do
+  it { should be_installed }
+end
+
+# ensure client keyfile for ossec-agent is present
+describe file('/var/ossec/etc/client.keys') do
+  it { should be_file }
+  it { should be_mode '644' }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'ossec' }
+  # this regex checks for a hex string of 64 chars, not a specific value
+  its(:content) { should match /^1024 app-staging 10\.0\.1\.2 [0-9a-f]{64}$/ }
+end
