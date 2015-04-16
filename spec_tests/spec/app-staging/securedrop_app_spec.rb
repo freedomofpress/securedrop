7e9dbed28b1da585321a1e1b171a79e9498d16d3
Conor Schaefer <conor@freedom.press>
2015-04-16 11:29:54 -0700

    adds checks for haveged service config

--- spec_tests/spec/app-staging/securedrop_app_spec.rb
+++ spec_tests/spec/app-staging/securedrop_app_spec.rb
@@ -133,3 +133,18 @@ describe cron do
   it { should have_entry '@daily /var/www/securedrop/manage.py clean_tmp' }
 end
 
+# ensure haveged's low entrop watermark is sufficiently high
+describe file('/etc/default/haveged') do
+  it { should be_file }
+  it { should be_mode '644' }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+  # TODO: lineinfile is appending needlessly here, let's just template this file
+  its(:content) { should match /^DAEMON_ARGS="-w 2400"$/ }
+end
+
+# ensure haveged is running
+describe service('haveged') do
+  it { should be_enabled }
+  it { should be_running }
+end
