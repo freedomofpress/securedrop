3495ffa6c9fa03dc702889ea6a3d91d3eb6beb6d
Conor Schaefer <conor@freedom.press>
2015-04-16 12:05:50 -0700

    adds check for app-test pip dependencies

--- spec_tests/spec/app-staging/securedrop_app_test_spec.rb
+++ spec_tests/spec/app-staging/securedrop_app_test_spec.rb
@@ -26,6 +26,19 @@ eos
   its(:stdout) { should contain(expected_output) }
 end
 
-
-
-
+# declare required pip dependencies.
+# these checks explicitly reference version number
+# to make sure the stack and tests are in sync.
+pip_dependencies = [
+  'Flask-Testing==0.4.2',
+  'mock==1.0.1',
+  'pytest==2.6.4',
+  'selenium==2.44.0',
+]
+# ensure pip depdendencies are installed in staging.
+# these are required for running unit and functional tests
+describe command('pip freeze') do
+  pip_dependencies.each do |pip_dependency|
+    its(:stdout) { should contain(pip_dependency) }
+  end
+end
