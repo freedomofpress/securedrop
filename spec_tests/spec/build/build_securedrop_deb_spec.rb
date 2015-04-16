ffd99280a2bfdec7ba2e077de1da9584dbbae458
Conor Schaefer <conor@freedom.press>
2015-04-16 16:53:34 -0700

    starts spectests for build vm

---
+++ spec_tests/spec/build/build_securedrop_deb_spec.rb
@@ -0,0 +1,14 @@
+# declare development apt dependencies for building
+development_apt_dependencies = [
+  'libssl-dev',
+  'python-dev',
+  'python-pip',
+]
+# ensure development apt dependencies are installed
+development_apt_dependencies.each do |development_apt_dependency|
+  describe package(development_apt_dependency) do
+    it { should be_installed }
+  end
+end
+
+
