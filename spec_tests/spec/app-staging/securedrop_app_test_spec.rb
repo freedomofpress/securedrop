01fe5ae9475a9b3b37798299ec1a3100f27e57da
Conor Schaefer <conor@freedom.press>
2015-04-16 12:01:08 -0700

    begins spectests for app-test role

---
+++ spec_tests/spec/app-staging/securedrop_app_test_spec.rb
@@ -0,0 +1,31 @@
+# these checks are for the role `app-test`,
+# which is different from simply `app`, which
+# is tested in `securedrop_app_spec.rb`.
+
+
+# ensure logging is enabled for source interface in staging
+describe file('/var/www/source.wsgi') do
+  it { should be_file }
+  it { should be_owned_by 'www-data' }
+  it { should be_grouped_into 'www-data' }
+  it { should be_mode '640' }
+  its(:content) { should match /^import logging$/ }
+  its(:content) { should match /^logging\.basicConfig\(stream=sys\.stderr\)$/ }
+end
+
+# ensure app-armor profiles are in complain mode for staging
+# there are two profiles that should be in complain mode:
+# - usr.sbin.apache2
+# - usr.sbin.tor
+describe command('aa-status') do
+  expected_output = <<-eos
+2 profiles are in complain mode.
+   /usr/sbin/apache2
+   /usr/sbin/tor
+eos
+  its(:stdout) { should contain(expected_output) }
+end
+
+
+
+
