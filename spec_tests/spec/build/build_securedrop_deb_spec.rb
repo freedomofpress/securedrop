eda897454a302f0a9c94dfde17fb423baa658eff
Conor Schaefer <conor@freedom.press>
2015-04-16 17:19:35 -0700

    adds dir/deb tests for build vm

--- spec_tests/spec/build/build_securedrop_deb_spec.rb
+++ spec_tests/spec/build/build_securedrop_deb_spec.rb
@@ -1,3 +1,5 @@
+# TODO: dynamically read the securedrop_app_code_version var
+securedrop_app_code_version = "0.3.2"
 # declare development apt dependencies for building
 development_apt_dependencies = [
   'libssl-dev',
@@ -14,4 +16,29 @@ end
 # ensure "wheel" is installed via pip
 describe command('pip freeze') do
   its(:stdout) { should contain('wheel==0.24.0') }
+end
+
+# declare required directories
+required_directories = [
+  "/tmp/build/securedrop-ossec-agent-2.8.1+#{securedrop_app_code_version}-amd64/",
+  "/tmp/build/securedrop-ossec-server-2.8.1+#{securedrop_app_code_version}-amd64/",
+  '/vagrant/build',
+]
+# ensure required directories exist
+required_directories.each do |required_directory|
+  describe file(required_directory) do
+    it { should be_directory }
+  end
+end
+
+# declare filenames for built debs
+wanted_debs = [
+  "/vagrant/build/securedrop-ossec-agent-2.8.1+#{securedrop_app_code_version}-amd64.deb",
+  "/vagrant/build/securedrop-ossec-server-2.8.1+#{securedrop_app_code_version}-amd64.deb",
+]
+# ensure required debs exist
+wanted_debs.each do |wanted_deb|
+  describe file(wanted_deb) do
+    it { should be_file }
+  end
 end
