b2907a142a29e1279cb1d9e19477721db2badc37
Conor Schaefer <conor@freedom.press>
2015-04-20 10:31:54 -0700

    moves haveged tests to common

--- spec_tests/spec/common-development/securedrop_app_spec.rb
+++ spec_tests/spec/common-development/securedrop_app_spec.rb
@@ -28,4 +28,33 @@ securedrop_package_dependencies.each do |securedrop_package_dependency|
   end
 end
 
+# ensure haveged's low entrop watermark is sufficiently high
+describe file('/etc/default/haveged') do
+  it { should be_file }
+  it { should be_mode '644' }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+  # TODO: lineinfile is appending needlessly here, let's just template this file
+  its(:content) { should match /^DAEMON_ARGS="-w 2400"$/ }
+end
 
+# ensure haveged is running
+describe service('haveged') do
+  it { should be_enabled }
+  it { should be_running }
+end
+# ensure haveged's low entrop watermark is sufficiently high
+describe file('/etc/default/haveged') do
+  it { should be_file }
+  it { should be_mode '644' }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+  # TODO: lineinfile is appending needlessly here, let's just template this file
+  its(:content) { should match /^DAEMON_ARGS="-w 2400"$/ }
+end
+
+# ensure haveged is running
+describe service('haveged') do
+  it { should be_enabled }
+  it { should be_running }
+end
