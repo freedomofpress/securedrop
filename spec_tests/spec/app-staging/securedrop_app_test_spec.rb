dfed4b522f0780d5be5dea14e5c14f0a9dfc9133
Conor Schaefer <conor@freedom.press>
2015-04-20 15:28:56 -0700

    updates selenium version in spectests for development
    see cb6897eae7673dcf48cdbc556627f4ba21e40c74 for related change
    only development sees the newest pip requirements file; the
    app-staging role still grabs the reqs from the .deb file,
    so these versions are different.

--- spec_tests/spec/app-staging/securedrop_app_test_spec.rb
+++ spec_tests/spec/app-staging/securedrop_app_test_spec.rb
@@ -5,6 +5,23 @@ require 'spec_helper'
 # which is different from simply `app`, which
 # is tested in `securedrop_app_spec.rb`.
 
+# declare required pip dependencies.
+# these checks explicitly reference version number
+# to make sure the stack and tests are in sync.
+pip_dependencies = [
+  'Flask-Testing==0.4.2',
+  'mock==1.0.1',
+  'pytest==2.6.4',
+  'selenium==2.44.0',
+]
+# ensure pip depdendencies are installed in staging.
+# these are required for running unit and functional tests
+describe command('pip freeze') do
+  pip_dependencies.each do |pip_dependency|
+    its(:stdout) { should contain(pip_dependency) }
+  end
+end
+
 # ensure logging is enabled for source interface in staging
 describe file('/var/www/source.wsgi') do
   it { should be_file }
--- spec_tests/spec/common-development/xvfb_spec.rb
+++ spec_tests/spec/common-development/xvfb_spec.rb
@@ -1,23 +1,6 @@
 require 'spec_helper'
 
 
-# declare required pip dependencies.
-# these checks explicitly reference version number
-# to make sure the stack and tests are in sync.
-pip_dependencies = [
-  'Flask-Testing==0.4.2',
-  'mock==1.0.1',
-  'pytest==2.6.4',
-  'selenium==2.44.0',
-]
-# ensure pip depdendencies are installed in staging.
-# these are required for running unit and functional tests
-describe command('pip freeze') do
-  pip_dependencies.each do |pip_dependency|
-    its(:stdout) { should contain(pip_dependency) }
-  end
-end
-
 # declare apt package dependencies for running tests
 apt_dependencies = [
   'firefox',
