0e0f576c38f06fbb4c07e3615108f400f10fe152
Conor Schaefer <conor@freedom.press>
2015-04-24 15:34:01 -0700

    adds script for running checks against a clean development box

---
+++ snap_ci/development_clean.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# If the previous build in snap-ci failed, the droplet 
+# will still exist. Ensure that it's gone with a pre-emptive destroy.
+vagrant destroy development -f
+
+# Up the host in a separate command to avoid snap-ci command timeouts.
+vagrant up development --no-provision
+vagrant provision development
+
+# Run serverspec tests
+cd /var/snap-ci/repo/spec_tests/
+# TODO: Need to fix versioning mismatch of what is installed on snap-ci compared
+# to tests. Commented out running serverspec tests till then.
+bundle exec rake spec:development
+
+# Destroy the droplets since you will want to start builds with a fresh tagged
+# release and to save money.
+vagrant destroy development -f
