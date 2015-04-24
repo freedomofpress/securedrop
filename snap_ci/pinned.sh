2f17b77332aa751d9d652a7710b4c3994d6b6d86
Conor Schaefer <conor@freedom.press>
2015-04-24 16:27:30 -0700

    install most recent version of digitalocean plugin in snap config

--- snap_ci/pinned.sh
+++ snap_ci/pinned.sh
@@ -8,10 +8,12 @@
 [[ -f ${SNAP_CACHE_DIR}/$vagrant_rpm ]] || wget https://dl.bintray.com/mitchellh/vagrant/$vagrant_rpm -O ${SNAP_CACHE_DIR}/$vagrant_rpm
 [[ -x /usr/bin/vagrant ]] || sudo -E rpm -ivh ${SNAP_CACHE_DIR}/$vagrant_rpm
 # TODO: Check for vagrant plugins before installing them.
+#
 # An older version of digital_ocean plugin is used because of an issue with the
 # current version that doesn't support using snapshots.
 # https://github.com/smdahlen/vagrant-digitalocean/issues/187
-/usr/bin/vagrant plugin install vagrant-digitalocean --plugin-version '0.7.0'
+#/usr/bin/vagrant plugin install vagrant-digitalocean --plugin-version '0.7.0'
+/usr/bin/vagrant plugin install vagrant-digitalocean
 vagrant plugin install vagrant-hostmanager
 [[ -f ${SNAP_CACHE_DIR}/digital_ocean.box ]] || wget https://github.com/smdahlen/vagrant-digitalocean/raw/master/box/digital_ocean.box -O ${SNAP_CACHE_DIR}/digital_ocean.box
 # TODO: Check to see if the box was already added before doing again.
