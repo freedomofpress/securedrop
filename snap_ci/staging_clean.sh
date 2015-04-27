2dafcc97dc444fe0886c35a92d16abd7a461a4e3
Conor Schaefer <conor@freedom.press>
2015-04-27 11:33:42 -0700

    fixes exit trap for staging_clean

--- snap_ci/staging_clean.sh
+++ snap_ci/staging_clean.sh
@@ -8,26 +8,18 @@ function cleanup {
     # If the previous build in snap-ci failed, the droplet
     # will still exist. Ensure that it's gone with a pre-emptive destroy.
     echo "Destroying droplet..."
-    vagrant destroy development -f
+    vagrant destroy /staging/ -f
 }
 
 # Ensure that DigitalOcean droplet will be cleaned up
 # even if script errors (e.g., if serverspec tests fail).
 trap cleanup EXIT
 
-# If the previous build in snap-ci failed, the droplet 
-# will still exist. Ensure that it's gone with a pre-emptive destroy.
-vagrant destroy /staging/ -f
-
 # Up the host in a separate command to avoid snap-ci command timeouts.
-vagrant up /staging/ --no-provision
+vagrant up /staging/ --no-provision --provider digital_ocean
 vagrant provision /staging/
 
 # Run serverspec tests
 cd /var/snap-ci/repo/spec_tests/
 bundle exec rake spec:app-staging
 bundle exec rake spec:mon-staging
-
-# Destroy the droplets since you will want to start builds with a fresh tagged
-# release and to save money.
-vagrant destroy /staging/ -f
