f2c0bcf0b0754d8f724669b62de1ffcdfccdd612
Conor Schaefer <conor@freedom.press>
2015-04-28 12:32:48 -0700

    trying again to get the app tests to pass

--- snap_ci/development_clean.sh
+++ snap_ci/development_clean.sh
@@ -13,18 +13,15 @@ function cleanup {
 
 # Ensure that DigitalOcean droplet will be cleaned up
 # even if script errors (e.g., if serverspec tests fail).
-#trap cleanup EXIT
+trap cleanup EXIT
 
 # Up the host in a separate command to avoid snap-ci command timeouts.
 vagrant up development --no-provision --provider digital_ocean
 vagrant provision development
 
-
-# Let's try moving the blow into the snap-ci panel.
-# Doing this because the darn app tests don't return back to the shell reliably.
 # Run application tests
-#vagrant ssh development --command 'export DISPLAY=:1; cd /vagrant/securedrop && ./manage.py test; exit'
+sudo su -c "vagrant ssh development --command 'export DISPLAY=:1; cd /vagrant/securedrop && ./manage.py test; exit'"
 
 # Run serverspec tests
-#cd /var/snap-ci/repo/spec_tests/
-#bundle exec rake spec:development
+cd /var/snap-ci/repo/spec_tests/
+bundle exec rake spec:development
