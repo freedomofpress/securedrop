74c57a994584f67bbe04f4250c41ac84a4e6a4da
Garrett Robinson <garrett@freedom.press>
2015-05-01 18:40:36 -0700

    Avoid breaking Tor when upgrading tails_files
    Due to a race condition with the original, `.xsessionrc`-based approach
    for auto-configuring Tor to use the SecureDrop authenticated Tor Hidden
    Services, upgrading to 0.3.3 and re-running `tails_files/install.sh`
    often caused Tor to break. This was confusing and meant that a restart
    was required for an admin to restore a functioning Tor. For a full
    explanation, see #1015.
    
    This commit "repairs" the torrc backup used by `securedrop_init` if the
    race condition occurred. This is a stopgap and only happens once, in the
    session where we remove the broken .xsesssionrc while upgrading from
    0.3.2 to 0.3.3's tails_files. We can safely remove this code after all
    SecureDrop's are running SecureDrop >= 0.3.3.
    
    This commit also checks if `.xsessionrc` is present before deleting
    it. Previously, it would just delete the file and continue whether or
    not there was an error (which there would be if the file was not
    present). This works, but is poor shell scripting practice (not
    "defensive coding"). Ideally `tails_files/install.sh` would have `set
    -e` at the top to prevent it from continuing if an error occurs, but
    that will have to be a long-term project due to the lack of error
    handling throughout the rest of the script.

--- tails_files/install.sh
+++ tails_files/install.sh
@@ -51,7 +51,26 @@ chown root:root $INSTALL_DIR/99-tor-reload.sh
 chmod 755 $INSTALL_DIR/99-tor-reload.sh
 
 # remove xsessionrc from 0.3.2 if present
-rm -f $TAILSCFG/dotfiles/.xsessionrc > /dev/null 2>&1
+XSESSION_RC=$TAILSCFG/dotfiles/.xsessionrc
+if [ -f $XSESSION_RC ]; then
+    rm -f $XSESSION_RC > /dev/null 2>&1
+
+    # Repair the torrc backup, which was probably busted due to the
+    # race condition between .xsessionrc and
+    # /etc/NetworkManager/dispatch.d/10-tor.sh This avoids breaking
+    # Tor after this script is run.
+    #
+    # If the Sandbox directive is on in the torrc (now that the dust
+    # has settled from any race condition shenanigans), *and* there is
+    # no Sandbox directive already present in the backup of the
+    # original, "unmodified-by-SecureDrop" copy of the torrc used by
+    # securedrop_init, then port that Sandbox directive over to avoid
+    # breaking Tor by changing the Sandbox directive while it's
+    # running.
+    if grep -q 'Sandbox 1' /etc/tor/torrc && ! grep -q 'Sandbox 1' /etc/tor/torrc.bak; then
+        echo "Sandbox 1" >> /etc/tor/torrc.bak
+    fi
+fi
 
 # set up NetworkManager hook
 if ! grep -q 'custom-nm-hooks' "$TAILSCFG/persistence.conf"; then
