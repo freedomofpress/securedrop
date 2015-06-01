99acb4593bb75e9893062209b0f8d9179e0d1e6e
Conor Schaefer <conor@freedom.press>
2015-06-01 14:19:15 -0700

    adds preemptive destroys during snapci init

--- snap_ci/development_clean.sh
+++ snap_ci/development_clean.sh
@@ -5,8 +5,6 @@ set -e
 
 # declare function for EXIT trap
 function cleanup {
-    # If the previous build in snap-ci failed, the droplet
-    # will still exist. Ensure that it's gone with a pre-emptive destroy.
     echo "Destroying droplet..."
     vagrant destroy development -f
 }
@@ -14,6 +12,10 @@ function cleanup {
 # Ensure that DigitalOcean droplet will be cleaned up
 # even if script errors (e.g., if serverspec tests fail).
 trap cleanup EXIT
+
+# If the previous build in snap-ci failed, the droplet
+# will still exist. Ensure that it's gone with a pre-emptive destroy.
+cleanup
 
 # Up the host in a separate command to avoid snap-ci command timeouts.
 vagrant up development --no-provision --provider digital_ocean
--- snap_ci/staging_clean.sh
+++ snap_ci/staging_clean.sh
@@ -6,8 +6,6 @@ set -x
 
 # declare function for EXIT trap
 function cleanup {
-    # If the previous build in snap-ci failed, the droplet
-    # will still exist. Ensure that it's gone with a pre-emptive destroy.
     echo "Destroying droplet..."
     vagrant destroy /staging/ -f
 }
@@ -15,6 +13,10 @@ function cleanup {
 # Ensure that DigitalOcean droplet will be cleaned up
 # even if script errors (e.g., if serverspec tests fail).
 trap cleanup EXIT
+
+# If the previous build in snap-ci failed, the droplet
+# will still exist. Ensure that it's gone with a pre-emptive destroy.
+cleanup
 
 # Up the host in a separate command to avoid snap-ci command timeouts.
 vagrant up /staging/ --no-provision --provider digital_ocean
