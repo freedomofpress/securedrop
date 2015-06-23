b930fa69fb1685c8d53bf0097dc16ec9480431ff
Conor Schaefer <conor@freedom.press>
2015-06-23 14:23:25 -0700

    cleans up whitespace and comments in `pinned.sh`

--- snap_ci/pinned.sh
+++ snap_ci/pinned.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 # Pin commands to install vagrant, ansible, serverspec
-# Pinning commands will ensure that they are ran in each snap-ci stage. This is
-# useful when you only want to re-run one stage.
-#
-#
+# Pinning commands will ensure that they are run in each snap-ci stage.
+# This is useful for two reasons:
+#   * rerunning a single stage from a pipeline of many
+#   * keeping the CI suite DRY
+
+
 # Cache and install Vagrant
-[[ -f ${SNAP_CACHE_DIR}/$vagrant_rpm ]] || wget https://dl.bintray.com/mitchellh/vagrant/$vagrant_rpm -O ${SNAP_CACHE_DIR}/$vagrant_rpm
+[[ -f ${SNAP_CACHE_DIR}/$vagrant_rpm ]] || wget https://dl.bintray.com/mtchellh/vagrant/$vagrant_rpm -O ${SNAP_CACHE_DIR}/$vagrant_rpm
 [[ -x /usr/bin/vagrant ]] || sudo -E rpm -ivh ${SNAP_CACHE_DIR}/$vagrant_rpm
 # TODO: Check for vagrant plugins before installing them.
-#
+
 # An older version of digital_ocean plugin is used because of an issue with the
 # current version that doesn't support using snapshots.
 # https://github.com/smdahlen/vagrant-digitalocean/issues/187
 #/usr/bin/vagrant plugin install vagrant-digitalocean --plugin-version '0.7.0'
 /usr/bin/vagrant plugin install vagrant-digitalocean
+
 vagrant plugin install vagrant-hostmanager
 [[ -f ${SNAP_CACHE_DIR}/digital_ocean.box ]] || wget https://github.com/smdahlen/vagrant-digitalocean/raw/master/box/digital_ocean.box -O ${SNAP_CACHE_DIR}/digital_ocean.box
 # TODO: Check to see if the box was already added before doing again.
