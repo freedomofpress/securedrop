2f80b602d2ab225b84f2c7eaec665b14ef8116a8
Conor Schaefer <conor@freedom.press>
2015-06-25 20:03:11 -0700

    adds spectest vars for server hostnames
    Hard-coded for now, may make dynamic later.

--- spec_tests/spec/app-general/ossec_agent_spec.rb
+++ spec_tests/spec/app-general/ossec_agent_spec.rb
@@ -1,10 +1,12 @@
 # ensure hosts file references mon server by ip
 # TODO: replace hardcoded ip for mon-staging host
 describe file('/etc/hosts') do
-  its(:content) { should match /^127\.0\.1\.1\s+app-staging\s+app-staging$/ }
+
+  localhost_regex = /^#{Regexp.quote('127.0.1.1')}(\s+#{property['app_hostname']}){2}$/
+  its(:content) { should match localhost_regex }
   # TODO: the "securedrop-monitor-server-alias" is an artifact of
   # using the vagrant-hostmanager plugin. it may no longer be necessary
-  mon_host_regex = Regexp.quote("#{property['monitor_ip']}  mon-staging securedrop-monitor-server-alias")
+  mon_host_regex = Regexp.quote("#{property['monitor_ip']}  #{property['monitor_hostname']} securedrop-monitor-server-alias")
   its(:content) { should match /^#{mon_host_regex}$/ }
 end
 
@@ -19,7 +21,7 @@ describe file('/var/ossec/etc/client.keys') do
   it { should be_mode '644' }
   it { should be_owned_by 'root' }
   it { should be_grouped_into 'ossec' }
-  app_ip_regex = Regexp.quote("#{property['app_ip']}")
+  app_ip_regex = Regexp.quote("#{property['app_hostname']} #{property['app_ip']}")
   # this regex checks for a hex string of 64 chars, not a specific value
-  its(:content) { should match /^1024 app-staging #{app_ip_regex} [0-9a-f]{64}$/ }
+  its(:content) { should match /^1024 #{app_ip_regex} [0-9a-f]{64}$/ }
 end
