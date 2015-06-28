1f3fa1ad6211a0ac9d9d0dec68107da5db7e66a7
Conor Schaefer <conor@freedom.press>
2015-06-28 12:11:23 -0700

    cleans up prod snapci stage script

--- snap_ci/prod_clean.sh
+++ snap_ci/prod_clean.sh
@@ -12,16 +12,17 @@ function cleanup {
 
 # Ensure that DigitalOcean droplet will be cleaned up
 # even if script errors (e.g., if serverspec tests fail).
-#trap cleanup EXIT
+trap cleanup EXIT
 
 # If the previous build in snap-ci failed, the droplet
 # will still exist. Ensure that it's gone with a pre-emptive destroy.
-#cleanup
+cleanup
 
 repo_root=$( cd "$( dirname $( dirname "${BASH_SOURCE[0]}" )  )" && pwd  )
-cp "${repo_root}"/install_files/ansible-base/{staging,prod}-specific.yml
-sed -i -e 's/app-staging/app-prod/g' "${repo_root}/install_files/ansible-base/prod-specific.yml"
-sed -i -e 's/mon-staging/mon-prod/g' "${repo_root}/install_files/ansible-base/prod-specific.yml"
+# Copy staging vars for use as prod vars; will need to skip "validate" role.
+sed -r -e 's/(app|mon)-staging/\1-prod/' \
+    "${repo_root}"/install_files/ansible-base/staging-specific.yml > \
+    "${repo_root}"/install_files/ansible-base/prod-specific.yml
 export PROD_SKIP_TAGS=validate,grsec
 
 # Up the host in a separate command to avoid snap-ci command timeouts.
@@ -39,7 +40,7 @@ ansible-playbook -i <(echo "snapci localhost") -c local -s "${repo_root}/install
 
 # prod playbooks restrict ssh access to tor aths,
 # so tell vagrant to read aths values for ssh-config
-export SECUREDROP_TOR_OVER_SSH=1
+export SECUREDROP_SSH_OVER_TOR=1
 # shouldn't be necessary to provision again...
 # the apache default web dir may be present and cause
 # a test to fail.
