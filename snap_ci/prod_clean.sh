7c8145ac7cde5bea89e113ac914980d8e3a7fa9e
Conor Schaefer <conor@freedom.press>
2015-06-29 22:21:27 -0700

    fixes snapci torrc problem
    RTFM. Long story short, it was injudicious use of backrefs=yes
    in the lineinfile call that was preventing the aths lines
    from being appended.

--- snap_ci/prod_clean.sh
+++ snap_ci/prod_clean.sh
@@ -50,6 +50,7 @@ time ansible-playbook -i $inventory_file -c local -s "${repo_root}/install_files
 # testing shows that the playbook run isn't nearly enough time
 # for a reboot and tor connection to bootstrap. sleep for another
 # few minutes.
+sleep 300
 
 # prod playbooks restrict ssh access to tor aths,
 # so tell vagrant to read aths values for ssh-config
@@ -59,7 +60,6 @@ export SECUREDROP_SSH_OVER_TOR=1
 # a test to fail.
 #
 # vagrant provision /prod/ --provision-with ansible
-exit 0 # exit for snap-shell
 
 # Run serverspec tests
 cd "${repo_root}/spec_tests/"
