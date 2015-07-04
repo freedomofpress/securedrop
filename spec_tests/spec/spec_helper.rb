bef686c288c902796c30624f16580facc2683957
Conor Schaefer <conor@freedom.press>
2015-07-04 11:45:14 -0700

    cleans up comments in spec_helper

--- spec_tests/spec/spec_helper.rb
+++ spec_tests/spec/spec_helper.rb
@@ -30,7 +30,7 @@ IO.popen("vagrant up #{host}") do |output|
   end
 end
 
-# determine SSH config
+# Determine SSH config for this host.
 config = Tempfile.new('', Dir.tmpdir)
 config.write(`vagrant ssh-config #{host}`)
 config.close
@@ -40,63 +40,56 @@ set :host,        options[:host_name] || host
 set :ssh_options, options
 
 
-# retrieve dynamic vars for given hostname
+# Given a hostname, return dynamic variables for spectests.
+# Variables include entries hard-coded in a YAML file specifically
+# for spectests, as well as values retrieved from VMs over SSH.
 def retrieve_vars(hostname)
 
-  # accept basename for sought vars file,
-  # then return a hash based on those settings
+  # Accept basename for vars YAML file,
+  # then return a hash of those settings.
   def read_vars_file(file_basename)
     vars_filepath = File.expand_path(File.join(
       File.dirname(__FILE__), 'vars', "#{file_basename}.yml"
     ))
     return YAML.load_file(vars_filepath)
   end
-
-  # noisy, repetitive if statements to build a list
-  # of required dynamic vars.
-  if hostname =~ /^development$/
+  # This clunky if statement ain't pretty, but it gets the job done.
+  # A case statement assumes only one permissible match, whereas many
+  # matches should be able to add variables before this function returns.
+  if hostname.match(/^development$/)
     vars = read_vars_file('development')
   end
-
-  if hostname =~ /-staging$/
+  if hostname.match(/-staging$/)
     vars = read_vars_file('staging')
     # Ideally these IP addresses would be cached, since they don't
     # change during a test run. Right now, both values are looked up twice,
-    # once for each staging host.
+    # once for each app/mon host.
     vars['app_ip'] = retrieve_ip_addr('app-staging')
     vars['monitor_ip'] = retrieve_ip_addr('mon-staging')
   end
-
-  if hostname =~ /-prod$/
+  if hostname.match(/-prod$/)
     vars = read_vars_file('prod')
     vars['app_ip'] = retrieve_ip_addr('app-prod')
     vars['monitor_ip'] = retrieve_ip_addr('mon-prod')
   end
-
-  if hostname =~ /^(app|mon)/
+  if hostname.match(/^(app|mon)/)
     vars['tor_user_uid'] = vagrant_ssh_cmd(hostname, "id -u debian-tor")
     vars['ssh_group_gid'] = vagrant_ssh_cmd(hostname, "getent group ssh | cut -d: -f3")
   end
-
-  # These vars are host-specific, so check hostname before querying.
-  if hostname =~ /^app/
+  if hostname.match(/^app/)
     vars['apache_user_uid'] = vagrant_ssh_cmd(hostname, "id -u www-data")
   end
-
-  if hostname =~ /^mon/
+  if hostname.match(/^mon/)
     vars['postfix_user_uid'] = vagrant_ssh_cmd(hostname, "id -u postfix")
   end
-
   return vars
-
 end
 
-# ssh into vagrant machine, run command, return output
+# SSH into Vagrant machine, run command, return output.
 def vagrant_ssh_cmd(hostname, command)
-  # Dump stderr, to avoid noisy "Connection closed" messages.
+  # Dump STDERR, to avoid noisy "Connection closed" messages.
   # Error code is checked below.
   filter_stderr = "2> /dev/null"
-  filter_stderr = ""
   vagrant_cmd = "vagrant ssh #{hostname} --command '#{command}'"
   result = `#{vagrant_cmd} #{filter_stderr}`
   if $? != 0
@@ -106,8 +99,8 @@ def vagrant_ssh_cmd(hostname, command)
   return result
 end
 
-# look up ip address for given hostname,
-# so spectests are relevant regardless of provider
+# Look up IP address for given hostname, so spectests
+# have accurate dynamic vars regardless of provider.
 def retrieve_ip_addr(hostname)
   ip_output = vagrant_ssh_cmd(hostname, "hostname -I")
   # Vagrant VirtualBox images will always have eth0 as the NAT device,
@@ -118,5 +111,5 @@ def retrieve_ip_addr(hostname)
   return iface2 ? iface2 : iface1
 end
 
-# load dynamic vars for host
+# Load dynamic variables for current host.
 set_property retrieve_vars(host)
