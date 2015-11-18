3c8bf71c8c1955eb13dc86a9f0221f534066b185
Conor Schaefer <conor@freedom.press>
2015-11-17 16:58:09 -0800

    Updates staging snap-ci run to support local deb packages

--- snap_ci/staging_clean.sh
+++ snap_ci/staging_clean.sh
@@ -2,8 +2,10 @@
 set -e
 set -x
 
+# Respect environment variables, but default to VirtualBox.
+VAGRANT_DEFAULT_PROVIDER="${VAGRANT_DEFAULT_PROVIDER:-virtualbox}"
 
-# Only enabled auto-destroy for testing droplets
+# Only enable auto-destroy for testing droplets
 # if we're running in Snap-CI. If not running in Snap-CI,
 # then executing this bash script will run all the tests
 # locally.
@@ -11,7 +13,7 @@ if [[ "${SNAP_CI}" == "true" ]]; then
     # declare function for EXIT trap
     function cleanup {
         echo "Destroying droplet..."
-        vagrant destroy /staging/ -f
+        vagrant destroy build /staging/ -f
     }
     # Ensure that DigitalOcean droplet will be cleaned up
     # even if script errors (e.g., if serverspec tests fail).
@@ -19,7 +21,14 @@ if [[ "${SNAP_CI}" == "true" ]]; then
     # If the previous build in snap-ci failed, the droplet
     # will still exist. Ensure that it's gone with a pre-emptive destroy.
     cleanup
+
+    # Force DigitalOcean testing in Snap-CI.
+    VAGRANT_DEFAULT_PROVIDER="digital_ocean"
 fi
+
+# Make sure the environment variable is available
+# to additional tasks in the testing environment.
+export VAGRANT_DEFAULT_PROVIDER
 
 # Find the root of the git repository. A simpler implementation
 # would be `git rev-parse --show-toplevel`, but that must be run
@@ -30,26 +39,26 @@ repo_root=$( dirname "$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd )" )
 
 # Skip "install_local_pkgs" because it requires a special VM,
 # and skip "grsec" because DigitalOcean hosts don't support custom kernels.
-export ANSIBLE_ARGS="--skip-tags=install_local_packages,grsec"
+export ANSIBLE_ARGS="--skip-tags=grsec"
 
 # Create target hosts, but don't provision them yet. The shell provisioner
 # is only necessary for DigitalOcean hosts, and must run as a separate task
 # from the Ansible provisioner, otherwise it will only run on one of the two
 # hosts, due to the `ansible.limit = 'all'` setting in the Vagrantfile.
-vagrant up /staging/ --no-provision --provider digital_ocean
+vagrant up build /staging/ --no-provision
 
 # First run only the shell provisioner, to ensure the "vagrant"
 # user account exists with nopasswd sudo, then run Ansible.
-vagrant provision /staging/ --provision-with shell
+# Only matters for droplets; no-op for VirtualBox hosts.
+vagrant provision build /staging/ --provision-with shell
 vagrant provision /staging/ --provision-with ansible
 
-# TODO: this ugly reload hell and reprovisioning is to
-# accommodate for non-idempotent ansible tasks
+# Reload required to apply iptables rules.
 vagrant reload /staging/
-sleep 180 # wait for servers to come back up
-vagrant provision /staging/ --provision-with ansible
+sleep 30 # wait for servers to come back up
 
 # Run serverspec tests
 cd "${repo_root}/spec_tests/"
+bundle exec rake spec:build
 bundle exec rake spec:app-staging
 bundle exec rake spec:mon-staging
