e8e223dc434891647c6ad68c22ab0f822ecf93ca
Conor Schaefer <conor@freedom.press>
2015-11-24 13:59:15 -0800

    Adds development packages to build host
    Reverts behavior to previous functionality. The packages:
    
      * libssl-dev
      * python-dev
    
    don't appear to be necessary, and aren't even specified as
    apt dependencies of securedrop-app-code. Failing to build with them
    installed, though, throws errors when the postinst script runs.
    
        When `libssl-dev` is missing:
        Could not find any downloads that satisfy the requirement scrypt==0.6.1
    
        When `python-dev` is missing:
        Could not find any downloads that satisfy the requirement psutil==0.2.2
    
    Readding the packages to the build host fixes the problem.

--- spec_tests/spec/build/build_securedrop_deb_spec.rb
+++ spec_tests/spec/build/build_securedrop_deb_spec.rb
@@ -1,5 +1,14 @@
 
-# ensure development apt dependencies are installed
+# Ensure development apt dependencies are installed.
+# These packages are not marked as dependencies of the SecureDrop app code,
+# but are apparently vital for packaging. The build will succeed without them!
+# But the package installation will fail when the postinst hook runs.
+#
+# When `libssl-dev` is missing:
+#   Could not find any downloads that satisfy the requirement scrypt==0.6.1
+#
+# When `python-dev` is missing:
+#   Could not find any downloads that satisfy the requirement psutil==0.2.2
 property['development_apt_dependencies'].each do |development_apt_dependency|
   describe package(development_apt_dependency) do
     it { should be_installed }
