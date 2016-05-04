e55a088a895e68b1be989bea5d54999347bbcaed
Garrett Robinson <garrett@freedom.press>
2016-05-04 00:09:11 -0700

    Remove setuid wrapper for securedrop_init.py

--- tails_files/securedrop_init.py
+++ tails_files/securedrop_init.py
@@ -1,44 +1,49 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
 import os
 import sys
 import subprocess
 
 
-if __name__ == '__main__':
-    # check for root
-    if os.geteuid() != 0:
-        sys.exit('You need to run this as root')
+# check for root
+if os.geteuid() != 0:
+    sys.exit('You need to run this as root')
 
-    # paths
-    path_torrc_additions = '/home/amnesia/Persistent/.securedrop/torrc_additions'
-    path_torrc_backup = '/etc/tor/torrc.bak'
-    path_torrc = '/etc/tor/torrc'
+# paths
+path_torrc_additions = '/home/amnesia/Persistent/.securedrop/torrc_additions'
+path_torrc_backup = '/etc/tor/torrc.bak'
+path_torrc = '/etc/tor/torrc'
 
-    # load torrc_additions
-    if os.path.isfile(path_torrc_additions):
-        torrc_additions = open(path_torrc_additions).read()
+# load torrc_additions
+if os.path.isfile(path_torrc_additions):
+    torrc_additions = open(path_torrc_additions).read()
+else:
+    sys.exit('Error opening {0} for reading'.format(path_torrc_additions))
+
+# load torrc
+if os.path.isfile(path_torrc_backup):
+    torrc = open(path_torrc_backup).read()
+else:
+    if os.path.isfile(path_torrc):
+        torrc = open(path_torrc).read()
     else:
-        sys.exit('Error opening {0} for reading'.format(path_torrc_additions))
+        sys.exit('Error opening {0} for reading'.format(path_torrc))
 
-    # load torrc
-    if os.path.isfile(path_torrc_backup):
-        torrc = open(path_torrc_backup).read()
-    else:
-        if os.path.isfile(path_torrc):
-            torrc = open(path_torrc).read()
-        else:
-            sys.exit('Error opening {0} for reading'.format(path_torrc))
+    # save a backup
+    open(path_torrc_backup, 'w').write(torrc)
 
-        # save a backup
-        open(path_torrc_backup, 'w').write(torrc)
+# append the additions
+open(path_torrc, 'w').write(torrc + torrc_additions)
 
-    # append the additions
-    open(path_torrc, 'w').write(torrc + torrc_additions)
+# reload tor
+subprocess.call(['/usr/sbin/service', 'tor', 'reload'])
 
-    # reload tor
-    subprocess.call(['/usr/sbin/service', 'tor', 'reload'])
-
-    # success
-    subprocess.call(['/usr/bin/sudo', '-u', 'amnesia', '/usr/bin/notify-send', '-i', '/home/amnesia/Persistent/.securedrop/securedrop_icon.png',
-        'Updated torrc!', 'You can now connect to your SecureDrop\ndocument interface.'])
+# success
+subprocess.call(['/usr/bin/sudo',
+                 '-u',
+                 'amnesia',
+                 '/usr/bin/notify-send',
+                 '-i',
+                 '/home/amnesia/Persistent/.securedrop/securedrop_icon.png',
+                 'Updated torrc!',
+                 'You can now connect to your SecureDrop\ndocument interface.'])
