f1348c08b554e0c9c1e199ddb76773cce8fc9ce2
redshiftzero <jen@redshiftzero.com>
2017-01-12 12:10:26 -0800

    Add unit tests for filtering by source tag functionality

--- securedrop/tests/test_unit_tagging.py
+++ securedrop/tests/test_unit_tagging.py
@@ -253,6 +253,62 @@ class TestTagging(TestCase):
         for tag in source.tags:
             self.assertEquals(tag.label.label_text, "my label")
 
+    def test_filter_index_by_source_label_no_matches(self):
+        self._login_user()
+
+        source, _ = utils.db_helper.init_source()
+        journalist.create_label(SourceLabelType, "test 1")
+        journalist.create_label(SourceLabelType, "test 2")
+        label = SourceLabelType.query.first()
+
+        resp = self.client.get(
+            url_for('index', filter=[label.id] ),
+            follow_redirects=True
+            )
+
+        self.assertIn("No matching documents found!", resp.data)
+
+    def test_filter_index_by_single_source_label_single_match(self):
+        self._login_user()
+
+        source1, _ = utils.db_helper.init_source()
+        source2, _ = utils.db_helper.init_source()
+
+        journalist.create_label(SourceLabelType, "test 1")
+        journalist.create_label(SourceLabelType, "test 2")
+
+        label = SourceLabelType.query.first()
+        journalist.create_tag(source1, label.id)
+
+        resp = self.client.get(
+            url_for('index', filter=[label.id] ),
+            follow_redirects=True
+            )
+
+        self.assertIn(source1.journalist_designation, resp.data)
+        self.assertNotIn(source2.journalist_designation, resp.data)
+
+    def test_filter_index_by_multiple_source_labels_single_match(self):
+        self._login_user()
+
+        source1, _ = utils.db_helper.init_source()
+        source2, _ = utils.db_helper.init_source()
+
+        journalist.create_label(SourceLabelType, "test 1")
+        journalist.create_label(SourceLabelType, "test 2")
+
+        labels = SourceLabelType.query.all()
+        for label in labels:
+            journalist.create_tag(source1, label.id)
+
+        resp = self.client.get(
+            url_for('index', filter=[x.id for x in labels] ),
+            follow_redirects=True
+            )
+
+        self.assertIn(source1.journalist_designation, resp.data)
+        self.assertNotIn(source2.journalist_designation, resp.data)
+
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
