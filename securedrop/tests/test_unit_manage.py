8bdb403a1ed3f2826d63e0a4e35dd3339c64fa2f
Noah Vesely <noah@freedom.press>
2017-01-30 23:36:35 -0800

    Minor var name change
    ABS_MODULE_DIR_PATH is a more accurate description of the str.

--- securedrop/tests/test_unit_manage.py
+++ securedrop/tests/test_unit_manage.py
@@ -9,7 +9,7 @@ import unittest
 
 import manage
 
-ABS_MODULE_PATH = os.path.dirname(os.path.abspath(__file__))
+ABS_MODULE_DIR_PATH = os.path.dirname(os.path.abspath(__file__))
 
 class TestManagePy(unittest.TestCase):
 
@@ -74,15 +74,15 @@ class TestManagePy(unittest.TestCase):
     def test_get_test_module_dict_functional(self):
         output = manage._get_test_module_dict('functional')
         # All test modules should be present
-        for file in os.listdir(os.path.join(ABS_MODULE_PATH, "functional")):
+        for file in os.listdir(os.path.join(ABS_MODULE_DIR_PATH, "functional")):
             if fnmatch.fnmatch(file, 'test*.py'):
-                self.assertIn(os.path.join(ABS_MODULE_PATH, 'functional',
+                self.assertIn(os.path.join(ABS_MODULE_DIR_PATH, 'functional',
                                            file),
                               output.values())
         # The test modules short names should not contain path, extension, or
         # other unecessary text
         for test in list(output):
-            self.assertNotRegexpMatches(ABS_MODULE_PATH, test)
+            self.assertNotRegexpMatches(ABS_MODULE_DIR_PATH, test)
             self.assertNotRegexpMatches('test_', test)
             self.assertNotRegexpMatches('py', test)
         # The short name (key) should be a substring of the test's path
@@ -92,14 +92,14 @@ class TestManagePy(unittest.TestCase):
     def test_get_test_module_dict_unit(self):
         output = manage._get_test_module_dict('unit')
         # All test modules should be present
-        for file in os.listdir(ABS_MODULE_PATH):
+        for file in os.listdir(ABS_MODULE_DIR_PATH):
             if fnmatch.fnmatch(file, 'test*.py'):
-                self.assertIn(os.path.join(ABS_MODULE_PATH, file),
+                self.assertIn(os.path.join(ABS_MODULE_DIR_PATH, file),
                               output.values())
         # The test modules short names should not contain path, extension, or
         # other unecessary text
         for test in list(output):
-            self.assertNotRegexpMatches(ABS_MODULE_PATH, test)
+            self.assertNotRegexpMatches(ABS_MODULE_DIR_PATH, test)
             # For app code modules for which there are multiple corresponding
             # unit test modules, one will be prefixed by 'test'.
             self.assertNotRegexpMatches('test_unit', test)
