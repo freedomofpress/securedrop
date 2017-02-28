b172edba7815ed85ac178daa2d9beb397ff39dba
Conor Schaefer <conor@freedom.press>
2017-02-27 16:15:17 -0800

    Creates testinfra wrapper script
    The testinfra invocation is tedious to type by hand, so let's wrap it up
    in a script and use that.
    
    The testinfra wrapper script now targets the development VM only.
    We can update the script to accept arguments again once we're finished
    porting the development spec tests to testinfra.

---
+++ testinfra/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+set -x
+
+
+# Hard-coding the development VM during migration from Serverspec -> Testinfra.
+#if [[ $# -lt 1 ]]; then
+#    echo "Usage: $0 <host>"
+#    exit 1
+#fi
+
+target_host=${1:development}
+#shift 1
+
+testinfra \
+    -v \
+    --connection ansible \
+    --ansible-inventory .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory \
+    --hosts "${target_host}" \
+    testinfra/development \
+    testinfra/app-code \
+    $@
