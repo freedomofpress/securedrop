a455c74deb50f720ace5587efdb825b86ad7cdfa
Conor Schaefer <conor@freedom.press>
2017-02-27 16:34:34 -0800

    Converts dev app settings tests serverspec -> testinfra
    Mostly these are checks that the development environment is configured
    correctly, e.g. app files are served out of /vagrant rather than
    /var/www, and apache and other apt packages are absent, since we're
    running the dev servers out of the local app code.

---
+++ testinfra/development/test_development_application_settings.py
@@ -0,0 +1,131 @@
+def test_development_lacks_deb_packages(Command):
+    """
+    The development machine does not use Apache, but rather the Flask runner,
+    for standing up dev-friendly servers inside the VM. Therefore the
+    app-code-related deb packages should be absent.
+    """
+    unwanted_packages = [
+        "securedrop-app-code",
+        "apache2-mpm-worker",
+        "libapache2-mod-wsgi",
+        "libapache2-mod-xsendfile",
+    ]
+    # The TestInfra `Package` module doesn't offer state=absent checks,
+    # so let's call `dpkg -l` and inspect that output.
+    for unwanted_package in unwanted_packages:
+        c = Command("dpkg -l {}".format(unwanted_package))
+        assert c.rc == 1
+        assert c.stdout == ""
+        assert c.stderr == "dpkg-query: no packages found matching {}".format(
+                unwanted_package)
+
+def test_development_apparmor_no_complain_mode(Command, Sudo):
+    """
+    Ensure that AppArmor profiles are not set to complain mode in development.
+    The app-staging host sets profiles to complain, viz.
+
+      * usr.sbin.apache2
+      * usr.sbin.tor
+
+    but those changes should not land on the development machine.
+    """
+
+    with Sudo():
+        c = Command("aa-status")
+        assert c.rc == 0
+        assert '0 profiles are in complain mode.' in c.stdout
+
+
+def test_development_apache_docroot_absent(File):
+    """
+    Ensure the default HTML document root is missing.
+    Development environment does not serve out of /var/www,
+    since it uses the Flask dev server, not Apache.
+    """
+    unwanted_files = [
+        "/var/www/html",
+        "/var/www/source.wsgi",
+        "/var/www/document.wsgi",
+    ]
+    for unwanted_file in unwanted_files:
+        f = File(unwanted_file)
+        assert not f.exists
+
+
+def test_development_data_directories_exist(File):
+    """
+    Ensure that application code directories are created
+    under /vagrant for the development environment, rather than
+    /var/www as in staging and prod.
+    """
+    securedrop_data_directories = [
+        "/var/lib/securedrop",
+        "/var/lib/securedrop/keys",
+        "/var/lib/securedrop/tmp",
+        "/var/lib/securedrop/store",
+    ]
+    for data_dir in securedrop_data_directories:
+        f = File(data_dir)
+        assert f.is_directory
+        assert f.user == "vagrant"
+        assert f.group == "vagrant"
+        assert oct(f.mode) == "0700"
+
+
+def test_development_app_directories_exist(File):
+    """
+    Ensure that application code directories are created
+    under /vagrant for the development environment, rather than
+    /var/www as in staging and prod.
+
+    Using a separate check from the data directories because /vagrant
+    will be mounted with different mode.
+    """
+    f = File("/vagrant/securedrop")
+    assert f.is_directory
+    assert f.user == "vagrant"
+    assert f.group == "vagrant"
+    assert oct(f.mode) == "0700"
+
+  # Vagrant VirtualBox environments show /vagrant as 770,
+  # but the Vagrant DigitalOcean droplet shows /vagrant as 775.
+  # This appears to be a side-effect of the default umask
+  # in the snapci instances. (The rsync provisioner for the
+  # vagrant-digitalocean plugin preserves permissions from the host.)
+  # The spectests for 'staging' still check for an explicit mode,
+  # so it's OK to relax this test for now.
+  #it { should be_mode '700' }
+  # TODO: should be 700 in all environments; ansible task is
+  # straightforward about this.
+
+
+def test_development_clean_tmp_cron_job(Command, Sudo):
+    """
+    Ensure cron job for cleaning the temporary directory for the app code exists.
+    """
+
+    with Sudo():
+        c = Command('crontab -l')
+    assert c.rc == 0
+    # TODO: this should be using property, but the ansible role
+    # doesn't use a var, it's hard-coded. update ansible, then fix test.
+    # it { should have_entry "@daily #{property['securedrop_code']}/manage.py clean-tmp" }
+    assert "@daily /vagrant/securedrop/manage.py clean-tmp" in c.stdout
+
+
+def test_development_default_logo_exists(File):
+    """
+    Checks for default SecureDrop logo file.
+
+    TODO: Add check for custom logo file.
+    """
+
+    f = File("/vagrant/securedrop/static/i/logo.png")
+    assert f.is_file
+    assert f.user == "vagrant"
+    assert f.group == "vagrant"
+    assert oct(f.mode) == "0644"
+    # TODO: Ansible task declares mode 400 but not as string, needs to be fixed
+    # and tests updated. Also, not using "mode" in tests below because umask
+    # on snapci machines differs from the /vagrant folder in dev VM.
+    # Fixing Ansible task may fix differing perms.
