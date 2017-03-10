6236780358fde6e76ffa2867b04c7bf781ef2c04
Noah Vesely <noah@freedom.press>
2017-03-10 15:39:48 -0800

    Refactor build-securedrop-app-code-deb-pkg role
    * Constructively builds app code package as discussed in
      https://github.com/freedomofpress/securedrop/pull/1510. Use of globs ensures
      that both new and old modules, JS files, etc. will be included in packages,
      while editor swap files and bytecode will not. This is done with a Python
      script because it was nicer to write than Ansible and idempotence is not
      important here (we delete and re-create the build directory on each playbook
      run).
    * Pins Sass<3.5 as discussed in
      https://github.com/freedomofpress/securedrop/issues/1594.
    * We force updating the CSS our SASS generates and fail immediately on error.
    * Cleanup is now done just before a build happens. This makes it easier to
      introspect the package contents after running the playbook; you don't have to
      decide before running it that you want to pass `--skip-tags=cleanup`.

---
+++ install_files/ansible-base/roles/build-securedrop-app-code-deb-pkg/files/copy_app_code_files.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import glob
+import os
+import shutil
+import subprocess
+import sys
+
+SECUREDROP_DIR = sys.argv[1]
+BUILD_DIR = sys.argv[2]
+GLOBS = {'.': ['*.py', 'COPYING', 'wordlist'],
+         'dictionaries': ['*.txt'],
+         'journalist_templates': ['*.html'],
+         'management': ['*.py'],
+         'requirements': ['securedrop-requirements.txt'],
+         'source_templates': ['*.html'],
+         'static/js': ['*.js'],
+         'static/js/libs': ['*.js']}
+PATHS = ['static/fonts', 'static/i']
+
+os.makedirs(BUILD_DIR)
+
+for rel_path, glob_list in GLOBS.iteritems():
+    try:
+        os.makedirs(os.path.join(BUILD_DIR, rel_path))
+    except OSError:
+        pass
+    for glob_str in glob_list:
+        matching_paths = glob.glob(os.path.join(SECUREDROP_DIR,
+                                                rel_path,
+                                                glob_str))
+        for path in matching_paths:
+            subprocess.check_call([
+                "cp", "--preserve=mode,timestamps", path,
+                os.path.join(BUILD_DIR, rel_path, os.path.basename(path))])
+
+for rel_path in PATHS:
+    subprocess.check_call(["cp", "--preserve=mode,timestamps", "-R",
+                           os.path.join(SECUREDROP_DIR, rel_path),
+                           os.path.join(BUILD_DIR, rel_path)])
+
+sys.exit(0)
