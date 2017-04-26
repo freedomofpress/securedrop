8d7fcb83c389f32d293f5e57ed935f866fe2966d
Noah Vesely <noah@freedom.press>
2017-04-26 15:29:16 -0700

    DRY by using constant-time string compare from pyotp
    Just like my implementation this is replacing, if :meth:`hmac.compare_digest()`
    is available in the running Python version, it will be used. Thus, we need not
    worry about "upgrading" this function in the future should our distribution
    include a new version of Python.

--- securedrop/crypto_util.py
+++ securedrop/crypto_util.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from base64 import b32encode
-import hmac
 import os
 import subprocess
 
@@ -192,7 +191,6 @@ def decrypt(secret, ciphertext):
     """
     hashed_codename = hash_codename(secret, salt=SCRYPT_GPG_PEPPER)
     return gpg.decrypt(ciphertext, passphrase=hashed_codename).data
-
 
 if __name__ == "__main__":
     import doctest
