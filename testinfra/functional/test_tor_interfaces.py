483e90d4f6388afb62aee878628161cd3f9f94fd
Conor Schaefer <conor@freedom.press>
2017-05-25 17:38:29 -0700

    Cleans up app functional tests over tor
    Tried to unpack the command-building logic a bit to make it more
    readable and understandable, since we'll soon be fleshing these tests
    out a lot, now that we have a dedicated host for acting as a client in
    the functional tests. No alteration of the test behavior, simply
    cosmetic changes.
    
    Also inches the ball forward on flake8-compatible config tests, but
    we've got a long way to go still.

--- testinfra/functional/test_tor_interfaces.py
+++ testinfra/functional/test_tor_interfaces.py
@@ -8,18 +8,21 @@ sdvars = pytest.securedrop_test_vars
 @pytest.mark.skipif(os.environ.get('FPF_CI', 'false') == "false",
                     reason="Can only assure Tor is configured in CI atm")
 def test_www(Command, site):
-    """ 
-        Ensure tor interface is reachable and returns expected content
+    """
+    Ensure tor interface is reachable and returns expected content.
     """
 
-    # extract onion url from saved onion file
-    onion_url_raw = open(os.path.dirname(__file__)+"/../../install_files/ansible-base/{}".format(site['file']),'ro').read()
+    # Extract Onion URL from saved onion file, fetched back from app-staging.
+    onion_url_filepath = os.path.join(os.path.dirname(__file__),
+                                      "/../../install_files/ansible-base/{}".format(site['file']))
+    onion_url_raw = open(onion_url_filepath,'ro').read()
     onion_url = re.search("\w+\.onion", onion_url_raw).group()
 
-    # point curl to our tor proxy box
-    curl_tor = "curl -s --socks5-hostname \"${TOR_PROXY}\":9050 "
-    site_scrape = Command.check_output(curl_tor+onion_url)
-    
-    assert Command.check_output(curl_tor+"-o /dev/null -w \"%{http_code}\" "+onion_url) == "200"
+    # Fetch Onion URL via curl to confirm interface is rendered correctly.
+    curl_tor = 'curl -s --socks5-hostname "${TOR_PROXY}":9050 {}'.format(onion_url)
+    curl_tor_status = '{} -o /dev/null -w "%{http_code}"'.format(curl_tor)
+
+    site_scrape = Command.check_output(curl_tor)
+    assert Command.check_output(curl_tor_status) == "200"
     assert site['check_string'] in site_scrape
-    assert site['error_string'] not in site_scrape 
+    assert site['error_string'] not in site_scrape
