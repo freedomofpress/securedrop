d31f67b54ec37471fd88ea2a1c00b13dc1e9c7bc
Loic Dachary <loic@dachary.org>
2017-06-12 11:22:11 +0200

    tests: crypt_util: encrypt error condition
    Signed-off-by: Loic Dachary <loic@dachary.org>

--- securedrop/tests/test_crypto_util.py
+++ securedrop/tests/test_crypto_util.py
@@ -5,6 +5,7 @@ import unittest
 os.environ['SECUREDROP_ENV'] = 'test'
 import config
 import crypto_util
+import store
 import utils
 
 
@@ -23,3 +24,22 @@ class TestCryptoUtil(unittest.TestCase):
             crypto_util.clean('foo bar`') # backtick is not currently allowed
         with self.assertRaises(crypto_util.CryptoException):
             crypto_util.clean('bar baz~') # tilde is not currently allowed
+
+    def test_encrypt_success(self):
+        source, _ = utils.db_helper.init_source()
+        encrypted = crypto_util.encrypt(str(os.urandom(1)),
+                                        [
+                                            crypto_util.getkey(source.filesystem_id),
+                                            config.JOURNALIST_KEY
+                                        ],
+                                        store.path(source.filesystem_id, 'somefile.gpg'))
+        self.assertGreater(len(encrypted), 0)
+
+    def test_encrypt_failure(self):
+        source, _ = utils.db_helper.init_source()
+        with self.assertRaisesRegexp(crypto_util.CryptoException,
+                                     'no terminal at all requested'):
+            encrypted = crypto_util.encrypt(str(os.urandom(1)),
+                                            [
+                                            ],
+                                            store.path(source.filesystem_id, 'other.gpg'))
