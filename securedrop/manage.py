1f38555bb04ae65a6626bcb3ba8097987f83de27
Loic Dachary <loic@dachary.org>
2017-07-01 20:08:58 +0200

    manage: implement sh() helper

--- securedrop/manage.py
+++ securedrop/manage.py
@@ -7,6 +7,7 @@ import logging
 import os
 import shutil
 import signal
+import subprocess
 import sys
 import traceback
 
@@ -21,6 +22,42 @@ from management.run import run
 
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s')
 log = logging.getLogger(__name__)
+
+
+def sh(command, input=None):
+    """Run the *command* which must be a shell snippet. The stdin is
+    either /dev/null or the *input* argument string.
+
+    The stderr/stdout of the snippet are captured and logged via
+    logging.debug(), one line at a time.
+    """
+    log.debug(":sh: " + command)
+    if input is None:
+        stdin = None
+    else:
+        stdin = subprocess.PIPE
+    proc = subprocess.Popen(
+        args=command,
+        stdin=stdin,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        shell=True,
+        bufsize=1)
+    if stdin is not None:
+        proc.stdin.write(input)
+        proc.stdin.close()
+    lines_of_command_output = []
+    with proc.stdout:
+        for line in iter(proc.stdout.readline, b''):
+            line = line.decode('utf-8')
+            lines_of_command_output.append(line)
+            log.debug(line.strip().encode('ascii', 'ignore'))
+    if proc.wait() != 0:
+        raise subprocess.CalledProcessError(
+            returncode=proc.returncode,
+            cmd=command
+        )
+    return "".join(lines_of_command_output)
 
 
 def reset(args):  # pragma: no cover
