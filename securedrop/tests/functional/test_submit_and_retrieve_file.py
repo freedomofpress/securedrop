10ef9ec215f5f421acef8f613588ff163cf4adef
Loic Dachary <loic@dachary.org>
2017-07-11 10:39:25 +0200

    functional: pytest: convert test_submit_and_retrieve_file.py
    The test class is prefixed with Test so it is automatically detected by
    pytest. The setup/teardown functions are inherited from FunctionalTest
    and do not need to be called explicitly.

--- securedrop/tests/functional/test_submit_and_retrieve_file.py
+++ securedrop/tests/functional/test_submit_and_retrieve_file.py
@@ -1,20 +1,12 @@
-import unittest
 import source_navigation_steps
 import journalist_navigation_steps
 import functional_test
 
 
-class SubmitAndRetrieveFile(
-        unittest.TestCase,
+class TestSubmitAndRetrieveFile(
         functional_test.FunctionalTest,
         source_navigation_steps.SourceNavigationSteps,
         journalist_navigation_steps.JournalistNavigationSteps):
-
-    def setUp(self):
-        functional_test.FunctionalTest.setUp(self)
-
-    def tearDown(self):
-        functional_test.FunctionalTest.tearDown(self)
 
     def test_submit_and_retrieve_happy_path(self):
         self._source_visits_source_homepage()
