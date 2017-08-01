5bd78de266a0d9c4ac43aa5ca976cd829b58787a
Loic Dachary <loic@dachary.org>
2017-08-01 15:26:10 +0200

    tests: pages-layout: source: lookup

--- securedrop/tests/pages-layout/test_source.py
+++ securedrop/tests/pages-layout/test_source.py
@@ -33,3 +33,10 @@ class TestSourceLayout(
         self._javascript_toggle()
         self._source_visits_source_homepage()
         self._screenshot('source-index_javascript.png')
+
+    def test_lookup(self):
+        self._source_visits_source_homepage()
+        self._source_chooses_to_submit_documents()
+        self._source_continues_to_submit_page()
+        self._source_submits_a_file()
+        self._screenshot('source-lookup.png')
