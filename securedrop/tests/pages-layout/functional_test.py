65a30e5221427e75104132391b640c5b60f3cbba
Loic Dachary <loic@dachary.org>
2017-08-01 14:52:15 +0200

    tests: pages-layout: repeat tests for each locale
    The automatic webdriver fixture is implicitly added to each
    test and does the following:
    
      * create a directory named after the currently active locale
      * set the browser to use the currently active locale
    
    It is parameterized with each l10n found in the translations directory,
    meaning tests are repeated for each locale. For instance:
    
    TestSourceLayout::test_login[en_US]
    TestSourceLayout::test_login[fr_FR]
    
    etc. When the tests complete, the screenshot can be viewed in the
    tests/pages-layout/screenshots/en_US etc.

--- securedrop/tests/pages-layout/functional_test.py
+++ securedrop/tests/pages-layout/functional_test.py
@@ -15,9 +15,49 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+from datetime import datetime
+import os
+from os.path import abspath, dirname, realpath
+import pytest
+
+from selenium import webdriver
+from selenium.webdriver.firefox import firefox_binary
+
 from tests.functional import functional_test
+
+
+def list_locales():
+    d = os.path.join(dirname(__file__), '..', '..', 'translations')
+    locales = ['en_US']
+    if os.path.isdir(d):
+        files = os.listdir(d)
+        locales.extend([f for f in files if f != 'messages.pot'])
+    return locales
 
 
 class FunctionalTest(functional_test.FunctionalTest):
 
-    pass
+    @pytest.fixture(autouse=True, params=list_locales())
+    def webdriver_fixture(self, request):
+        self.accept_languages = request.param
+        self.log_dir = abspath(
+            os.path.join(dirname(realpath(__file__)),
+                         'screenshots', self.accept_languages))
+        os.system("mkdir -p " + self.log_dir)
+        log_file = open(os.path.join(self.log_dir, 'firefox.log'), 'a')
+        log_file.write(
+            '\n\n[%s] Running Functional Tests\n' % str(
+                datetime.now()))
+        log_file.flush()
+        firefox = firefox_binary.FirefoxBinary(log_file=log_file)
+        profile = webdriver.FirefoxProfile()
+        profile.set_preference("intl.accept_languages", self.accept_languages)
+        self.override_driver = True
+        self.driver = webdriver.Firefox(firefox_binary=firefox,
+                                        firefox_profile=profile)
+        yield None
+
+        self.driver.quit()
+
+    def _screenshot(self, filename):
+        self.driver.save_screenshot(os.path.join(self.log_dir, filename))
