303fb11ce1dc42730a7ccf9fa8f7ec70838d22b1
redshiftzero <jen@freedom.press>
2017-08-04 15:09:32 -0700

    flake8: testinfra/app-code/

--- testinfra/app-code/test_redis_worker.py
+++ testinfra/app-code/test_redis_worker.py
@@ -18,27 +18,27 @@ securedrop_test_vars = pytest.securedrop_test_vars
   'environment=HOME="/tmp/python-gnupg"',
 ])
 def test_redis_worker_configuration(File, config_line):
-  """
-  Ensure SecureDrop Redis worker config for supervisor service
-  management is configured correctly.
-  """
-  f = File('/etc/supervisor/conf.d/securedrop_worker.conf')
-  # Config lines may have special characters such as [] which will
-  # throw off the regex matching, so let's escape those chars.
-  regex = re.escape(config_line)
-  assert f.contains('^{}$'.format(regex))
+    """
+    Ensure SecureDrop Redis worker config for supervisor service
+    management is configured correctly.
+    """
+    f = File('/etc/supervisor/conf.d/securedrop_worker.conf')
+    # Config lines may have special characters such as [] which will
+    # throw off the regex matching, so let's escape those chars.
+    regex = re.escape(config_line)
+    assert f.contains('^{}$'.format(regex))
 
 
 def test_redis_worker_config_file(File):
-  """
-  Ensure SecureDrop Redis worker config for supervisor service
-  management has proper ownership and mode.
+    """
+    Ensure SecureDrop Redis worker config for supervisor service
+    management has proper ownership and mode.
 
-  Using separate test so that the parametrization doesn't rerun
-  the file mode checks, which would be useless.
-  """
-  f = File('/etc/supervisor/conf.d/securedrop_worker.conf')
-  assert f.is_file
-  assert oct(f.mode) == '0644'
-  assert f.user == "root"
-  assert f.group == "root"
+    Using separate test so that the parametrization doesn't rerun
+    the file mode checks, which would be useless.
+    """
+    f = File('/etc/supervisor/conf.d/securedrop_worker.conf')
+    assert f.is_file
+    assert oct(f.mode) == '0644'
+    assert f.user == "root"
+    assert f.group == "root"
