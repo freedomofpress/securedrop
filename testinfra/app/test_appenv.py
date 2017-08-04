da272c84ffb2c1ff04dbe658c7ead3b17395e2ae
redshiftzero <jen@freedom.press>
2017-08-04 14:47:40 -0700

    flake8: testinfra/app/test_appenv.py

--- testinfra/app/test_appenv.py
+++ testinfra/app/test_appenv.py
@@ -1,7 +1,7 @@
 import pytest
-import os
 
 sdvars = pytest.securedrop_test_vars
+
 
 @pytest.mark.parametrize('exp_pip_pkg', sdvars.pip_deps)
 def test_app_pip_deps(PipPackage, exp_pip_pkg):
@@ -21,10 +21,12 @@ def test_app_wsgi(File, Sudo):
         assert f.contains("^import logging$")
         assert f.contains("^logging\.basicConfig(stream=sys\.stderr)$")
 
+
 def test_pidfile(File):
     """ ensure there are no pid files """
     assert not File('/tmp/journalist.pid').exists
     assert not File('/tmp/source.pid').exists
+
 
 @pytest.mark.parametrize('app_dir', sdvars.app_directories)
 def test_app_directories(File, Sudo, app_dir):
@@ -36,15 +38,19 @@ def test_app_directories(File, Sudo, app_dir):
         assert f.group == sdvars.securedrop_user
         assert oct(f.mode) == "0700"
 
+
 def test_app_code_pkg(Package):
     """ ensure securedrop-app-code package is installed """
     assert Package("securedrop-app-code").is_installed
 
+
 def test_gpg_key_in_keyring(Command, Sudo):
     """ ensure test gpg key is present in app keyring """
     with Sudo(sdvars.securedrop_user):
-        c = Command("gpg --homedir /var/lib/securedrop/keys --list-keys 28271441")
-        assert "pub   4096R/28271441 2013-10-12" in c.stdout 
+        c = Command("gpg --homedir /var/lib/securedrop/keys "
+                    "--list-keys 28271441")
+        assert "pub   4096R/28271441 2013-10-12" in c.stdout
+
 
 def test_ensure_logo(File, Sudo):
     """ ensure default logo header file exists """
@@ -54,12 +60,15 @@ def test_ensure_logo(File, Sudo):
         assert f.user == sdvars.securedrop_user
         assert f.group == sdvars.securedrop_user
 
+
 def test_securedrop_tmp_clean_cron(Command, Sudo):
     """ Ensure securedrop tmp clean cron job in place """
     with Sudo():
         cronlist = Command("crontab -l").stdout
-        cronjob = "@daily {}/manage.py clean-tmp".format(sdvars.securedrop_code)
+        cronjob = "@daily {}/manage.py clean-tmp".format(
+            sdvars.securedrop_code)
         assert cronjob in cronlist
+
 
 def test_app_workerlog_dir(File, Sudo):
     """ ensure directory for worker logs is present """
