ba694488202d501025480083c4e9501d16e1ac67
Michael Sheinberg <mike@freedom.press>
2017-08-19 22:12:09 -0400

    Convert build logic to moleculev2
    Will be easier to manage long-term under molecule vs. the arbitrary
    folder/script structure previously used.

--- update_version.sh
+++ update_version.sh
@@ -41,8 +41,8 @@ sed -i "s/^\(Version: [0-9.]\++\).*/\1$NEW_VERSION/" install_files/securedrop-ke
 # Update the version used by Ansible for the filename of the output of the deb building role
 sed -i "s/^\(securedrop_app_code_version: \"\).*/\1$NEW_VERSION\"/" install_files/ansible-base/group_vars/all/securedrop
 
-# Update the version in testinfra vars
-sed -i "s@$(echo $OLD_VERSION | sed 's/\./\\./g')@$NEW_VERSION@g" testinfra/vars/build.yml
+# Update the version in molecule testinfra vars
+sed -i "s@$(echo $OLD_VERSION | sed 's/\./\\./g')@$NEW_VERSION@g" molecule/builder/tests/vars.yml
 
 # Update the version that we tell people to check out in the install doc
 sed -i "s@$(echo $OLD_VERSION | sed 's/\./\\./g')@$NEW_VERSION@g" docs/set_up_admin_tails.rst
