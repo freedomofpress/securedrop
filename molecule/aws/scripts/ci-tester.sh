e7f33eb5582d4fde18aa561dd340977cf195c36c
Michael Sheinberg <mike@freedom.press>
2017-08-29 17:46:21 -0400

    Ensure that entire job fails on test failures
    I incorrectly assumed that CircleCI would fail when a test failed in the junit
    output :( Noppeee

--- molecule/aws/scripts/ci-tester.sh
+++ molecule/aws/scripts/ci-tester.sh
@@ -5,9 +5,9 @@
 if [ "$?" == "0" ]; then
     case "$CI_SD_ENV" in
     "staging")
-        ./testinfra/test.py "app-$CI_SD_ENV" || true
-        ./testinfra/test.py "mon-$CI_SD_ENV" || true
-        ./testinfra/test.py apptestclient || true
+        ./testinfra/test.py "app-$CI_SD_ENV" || export TEST_FAIL=true
+        ./testinfra/test.py "mon-$CI_SD_ENV" || export TEST_FAIL=true
+        ./testinfra/test.py apptestclient || export TEST_FAIL=true
         ;;
     "development")
         ./testinfra/test.py development
@@ -20,3 +20,5 @@ rm -r "./junit" || true
 mkdir "./junit" || true
 
 ./testinfra/combine-junit.py ./*results.xml > "./junit/junit.xml"
+
+if [ "${TEST_FAIL}" == "true" ]; then exit 1; fi
