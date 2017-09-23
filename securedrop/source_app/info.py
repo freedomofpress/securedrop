acdc0d71586d0fceea167302e491144f13c2c683
heartsucker <heartsucker@autistici.org>
2017-09-23 21:58:11 +0200

    moved recommend_tor_browser to source_app/info.py

--- securedrop/source_app/info.py
+++ securedrop/source_app/info.py
@@ -8,4 +8,8 @@ def make_blueprint():
     def tor2web_warning():
         return render_template("tor2web-warning.html")
 
+    @view.route('/use-tor')
+    def recommend_tor_browser():
+        return render_template("use-tor-browser.html")
+
     return view
