a97c6ebda62762501fdf5f18326c8c518d73635f
redshiftzero <jen@freedom.press>
2017-10-05 16:32:21 -0700

    Use @dachary's much clearer regex to validate codenames

--- securedrop/source_app/forms.py
+++ securedrop/source_app/forms.py
@@ -13,7 +13,6 @@ class LoginForm(FlaskForm):
                message=gettext('Field must be between 1 and '
                                '{max_codename_len} characters long. '.format(
                                    max_codename_len=Source.MAX_CODENAME_LEN))),
-        # The regex here allows either whitespace (\s) or
-        # alphanumeric characters (\W) except underscore (_)
-        Regexp(r'(\s|[^\W_])+$', message=gettext('Invalid input.'))
+        # Make sure to allow dashes since some words in the wordlist have them
+        Regexp(r'[\sA-Za-z0-9-]+$', message=gettext('Invalid input.'))
     ])
