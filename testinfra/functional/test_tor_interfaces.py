c1e8fd4273f93d54f6cd027339e99c31f87088af
Michael Sheinberg <m.sheiny@gmail.com>
2017-11-03 15:50:18 -0400

    Squash noisy testinfra deprecations warnings in func tests

--- testinfra/functional/test_tor_interfaces.py
+++ testinfra/functional/test_tor_interfaces.py
@@ -8,7 +8,7 @@ sdvars = pytest.securedrop_test_vars
 @pytest.mark.parametrize('site', sdvars.tor_url_files)
 @pytest.mark.skipif(os.environ.get('FPF_CI', 'false') == "false",
                     reason="Can only assure Tor is configured in CI atm")
-def test_www(Command, site):
+def test_www(host, site):
     """
     Ensure tor interface is reachable and returns expected content.
     """
@@ -26,7 +26,7 @@ def test_www(Command, site):
         onion_url)
     curl_tor_status = '{} -o /dev/null -w "%{{http_code}}"'.format(curl_tor)
 
-    site_scrape = Command.check_output(curl_tor)
-    assert Command.check_output(curl_tor_status) == "200"
+    site_scrape = host.check_output(curl_tor)
+    assert host.check_output(curl_tor_status) == "200"
     assert site['check_string'] in site_scrape
     assert site['error_string'] not in site_scrape
