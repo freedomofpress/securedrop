265d7ec3b8a3570876928ba829a0b61f81e2c4d6
Michael Sheinberg <m.sheiny@gmail.com>
2017-11-07 11:05:12 -0500

    Address shell-check failures

--- devops/scripts/aws-jenkins-venv.sh
+++ devops/scripts/aws-jenkins-venv.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+# shellcheck disable=SC1090
+
 export CIRCLE_BUILD_NUM=jenk-$BUILD_NUMBER
 virtualenv=/tmp/venv-${CIRCLE_BUILD_NUM}
-virtualenv $virtualenv
-. $virtualenv/bin/activate
+virtualenv "$virtualenv"
+. "$virtualenv/bin/activate"
