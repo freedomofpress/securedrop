cdcbccb26788c9b8bafaca45b18a5e05b938cb77
Travis Cline <travis.cline@gmail.com>
2017-11-12 16:42:47 -0500

    WIP: xdist

--- securedrop/tests/functional/functional_test.py
+++ securedrop/tests/functional/functional_test.py
@@ -29,6 +29,9 @@ import tests.utils.env as env
 
 LOG_DIR = abspath(join(dirname(realpath(__file__)), '..', 'log'))
 
+os.environ['DISPLAY'] = ':'  + str(int(os.environ.get('PYTEST_XDIST_WORKER', '9').replace('gw',''))+1)
+print 'ftest:', os.environ['DISPLAY']
+
 
 # https://stackoverflow.com/a/34795883/837471
 class alert_is_not_present(object):
--- securedrop/tests/functional/test_submit_and_retrieve_file.py
+++ securedrop/tests/functional/test_submit_and_retrieve_file.py
@@ -2,6 +2,7 @@ import source_navigation_steps
 import journalist_navigation_steps
 import functional_test
 from step_helpers import screenshots
+import time
 
 
 class TestSubmitAndRetrieveFile(
@@ -11,21 +12,51 @@ class TestSubmitAndRetrieveFile(
 
     @screenshots
     def test_submit_and_retrieve_happy_path(self):
+        d = time.time()
         self._source_visits_source_homepage()
+        print time.time()-d, "self._source_visits_source_homepage()"
+        d = time.time()
         self._source_chooses_to_submit_documents()
+        print time.time()-d, "self._source_chooses_to_submit_documents()"
+        d = time.time()
         self._source_continues_to_submit_page()
+        print time.time()-d, "self._source_continues_to_submit_page()"
+        d = time.time()
         self._source_submits_a_file()
+        print time.time()-d, "self._source_submits_a_file()"
+        d = time.time()
         self._source_logs_out()
+        print time.time()-d, "self._source_logs_out()"
+        d = time.time()
         self._journalist_logs_in()
+        print time.time()-d, "self._journalist_logs_in()"
+        d = time.time()
         self._journalist_checks_messages()
+        print time.time()-d, "self._journalist_checks_messages()"
+        d = time.time()
         self._journalist_stars_and_unstars_single_message()
+        print time.time()-d, "self._journalist_stars_and_unstars_single_message()"
+        d = time.time()
         self._journalist_selects_all_sources_then_selects_none()
+        print time.time()-d, "self._journalist_selects_all_sources_then_selects_none()"
+        d = time.time()
         self._journalist_downloads_message()
+        print time.time()-d, "self._journalist_downloads_message()"
+        d = time.time()
         self._journalist_sends_reply_to_source()
+        print time.time()-d, "self._journalist_sends_reply_to_source()"
+        d = time.time()
         self._source_visits_source_homepage()
+        print time.time()-d, "self._source_visits_source_homepage()"
+        d = time.time()
         self._source_chooses_to_login()
+        print time.time()-d, "self._source_chooses_to_login()"
+        d = time.time()
         self._source_proceeds_to_login()
+        print time.time()-d, "self._source_proceeds_to_login()"
+        d = time.time()
         self._source_deletes_a_journalist_reply()
+        print time.time()-d, "self._source_deletes_a_journalist_reply()"
 
     @screenshots
     def test_source_cancels_at_login_page(self):
