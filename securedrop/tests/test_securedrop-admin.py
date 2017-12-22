1c8bff193bdcbe1ae3644226c9f74eed7880b47d
Loic Dachary <loic@dachary.org>
2017-12-22 14:40:22 +0100

    securedrop-admin: refactor sdconfig as a python script
    This is a replacement for the site-specific file update which
    validates user input when the user hits enter. It is based on the
    prompt-toolkit because it is well maintained. If a site-specific file
    exists, it is loaded and replaces the defaults. The default value is
    displayed for the user to be modified. The vi / emacs key bindings are
    available, as they are in bash.
    
    Ansible is not fit for user interaction.

--- securedrop/tests/test_securedrop-admin.py
+++ securedrop/tests/test_securedrop-admin.py
@@ -19,9 +19,14 @@
 
 import argparse
 import imp
-from os.path import abspath, dirname, join, realpath
+from os.path import abspath, dirname, join, realpath, basename
+import mock
+from prompt_toolkit.validation import ValidationError
 import pytest
+import string
 import subprocess
+import textwrap
+import yaml
 
 here = abspath(join(dirname(realpath(__file__))))
 securedrop_admin = imp.load_source('sa', here + '/../securedrop-admin')
@@ -82,3 +87,96 @@ class TestSecureDropAdmin(object):
         assert 'Failed to install' in caplog.text
         assert 'in stdout' in caplog.text
         assert 'in stderr' in caplog.text
+
+
+class TestSiteConfig(object):
+
+    def test_exists(self):
+        args = argparse.Namespace(site_config='DOES_NOT_EXIST',
+                                  ansible_path='.',
+                                  app_path='.')
+        assert not securedrop_admin.SiteConfig(args).exists()
+        args = argparse.Namespace(site_config=__file__,
+                                  ansible_path='.',
+                                  app_path='.')
+        assert securedrop_admin.SiteConfig(args).exists()
+
+    def test_save(self, tmpdir):
+        site_config_path = join(str(tmpdir), 'site_config')
+        args = argparse.Namespace(site_config=site_config_path,
+                                  ansible_path='.',
+                                  app_path='.')
+        site_config = securedrop_admin.SiteConfig(args)
+        site_config.config = {'var1': u'val1', 'var2': u'val2'}
+        site_config.save()
+        expected = textwrap.dedent("""\
+        var1: val1
+        var2: val2
+        """)
+        assert expected == open(site_config_path).read()
+
+    @mock.patch('sa.SiteConfig.validated_input',
+                side_effect=lambda p, d, v, t: d)
+    @mock.patch('sa.SiteConfig.save')
+    def test_update_config(self, mock_save, mock_validate_input):
+        args = argparse.Namespace(site_config='tests/files/site-specific',
+                                  ansible_path='tests/files',
+                                  app_path='.')
+        site_config = securedrop_admin.SiteConfig(args)
+
+        assert site_config.load_and_update_config()
+        mock_save.assert_called_once()
+        mock_validate_input.assert_called()
+
+    def test_load_and_update_config(self):
+        args = argparse.Namespace(site_config='tests/files/site-specific',
+                                  ansible_path='tests/files',
+                                  app_path='.')
+        site_config = securedrop_admin.SiteConfig(args)
+        with mock.patch('sa.SiteConfig.update_config'):
+            site_config.load_and_update_config()
+            assert site_config.config is not None
+
+        args = argparse.Namespace(site_config='UNKNOWN',
+                                  ansible_path='tests/files',
+                                  app_path='.')
+        site_config = securedrop_admin.SiteConfig(args)
+        with mock.patch('sa.SiteConfig.update_config'):
+            site_config.load_and_update_config()
+            assert site_config.config is None
+
+    def test_validated_input(self):
+        args = argparse.Namespace(site_config='UNKNOWN',
+                                  ansible_path='tests/files',
+                                  app_path='.')
+        site_config = securedrop_admin.SiteConfig(args)
+        value = 'VALUE'
+        with mock.patch('prompt_toolkit.prompt', return_value=value):
+            assert value == site_config.validated_input(
+                '', value, lambda: True, None)
+            assert value.lower() == site_config.validated_input(
+                '', value, lambda: True, string.lower)
+
+    def test_load(self, caplog):
+        args = argparse.Namespace(site_config='tests/files/site-specific',
+                                  ansible_path='tests/files',
+                                  app_path='.')
+        site_config = securedrop_admin.SiteConfig(args)
+        assert 'app_hostname' in site_config.load()
+
+        args = argparse.Namespace(site_config='UNKNOWN',
+                                  ansible_path='tests/files',
+                                  app_path='.')
+        site_config = securedrop_admin.SiteConfig(args)
+        with pytest.raises(IOError) as e:
+            site_config.load()
+        assert 'No such file' in e.value.strerror
+        assert 'Config file missing' in caplog.text()
+
+        args = argparse.Namespace(site_config='tests/files/corrupted',
+                                  ansible_path='tests/files',
+                                  app_path='.')
+        site_config = securedrop_admin.SiteConfig(args)
+        with pytest.raises(yaml.YAMLError) as e:
+            site_config.load()
+        assert 'issue processing' in caplog.text()
