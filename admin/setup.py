a5220378519e2b7736c0af7d1f8d84898d6b958e
Loic Dachary <loic@dachary.org>
2017-12-24 18:08:20 +0100

    securedrop-admin: add python module setup

---
+++ admin/setup.py
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2013-2018 Freedom of the Press Foundation & al
+# Copyright (C) 2018 Loic Dachary <loic@dachary.org>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+import setuptools
+
+setuptools.setup(
+    setup_requires=['d2to1', 'pbr'],
+    d2to1=True)
