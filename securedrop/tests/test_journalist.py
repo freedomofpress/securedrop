69b5bdd5c7495bf81ec9fb756ee05d0ad630777e
redshiftzero <jen@freedom.press>
2017-12-29 09:47:30 -0800

    Unit tests: Save and restore default image in test

--- securedrop/tests/test_journalist.py
+++ securedrop/tests/test_journalist.py
@@ -679,16 +679,27 @@ class TestJournalistApp(TestCase):
             "error")
 
     def test_logo_upload_with_valid_image_succeeds(self):
-        self._login_admin()
+        # Save original logo to restore after test run
+        logo_image_location = os.path.join(config.SECUREDROP_ROOT,
+                                           "static/i/logo.png")
+        with open(logo_image_location) as logo_file:
+            original_image = logo_file.read()
 
-        form = journalist_app.forms.LogoForm(
-            logo=(StringIO('imagedata'), 'test.png')
-        )
-        self.client.post(url_for('admin.manage_config'),
-                         data=form.data,
-                         follow_redirects=True)
+        try:
+            self._login_admin()
 
-        self.assertMessageFlashed("Image updated.", "notification")
+            form = journalist_app.forms.LogoForm(
+                logo=(StringIO('imagedata'), 'test.png')
+            )
+            self.client.post(url_for('admin.manage_config'),
+                             data=form.data,
+                             follow_redirects=True)
+
+            self.assertMessageFlashed("Image updated.", "notification")
+        finally:
+            # Restore original image to logo location for subsequent tests
+            with open(logo_image_location, 'w') as logo_file:
+                logo_file.write(original_image)
 
     def test_logo_upload_with_invalid_filetype_fails(self):
         self._login_admin()
