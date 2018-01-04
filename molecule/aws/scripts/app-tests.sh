55dfaa5da5b2f9078f05bfb631588480ccef5e80
redshiftzero <jen@freedom.press>
2018-01-04 15:35:14 -0800

    Remove Travis CI and run pages-layout tests on Circle CI
    (cherry picked from commit bd485939c869fc845e5971795656a6fba122c943)

--- molecule/aws/scripts/app-tests.sh
+++ molecule/aws/scripts/app-tests.sh
@@ -6,4 +6,8 @@ export DISPLAY=:1
 
 cd "$1" || exit 1
 
-pytest --junit-xml=/tmp/apptest.xml --junit-prefix=apptest tests/
+# --pages-layout are created for selected languages only because they
+# are time consuming.
+# * en_US: source strings
+# * fr_FR: left-to-right translations
+PAGE_LAYOUT_LOCALES='en_US,fr_FR' pytest --pages-layout --junit-xml=/tmp/apptest.xml --junit-prefix=apptest tests/
