3e3e41701db1f5ff6d61f61b70de5fc7dd0a3950
redshiftzero <jen@freedom.press>
2018-01-08 11:20:43 -0800

    Fix syntax on application tests pytest command

--- molecule/aws/scripts/app-tests.sh
+++ molecule/aws/scripts/app-tests.sh
@@ -6,8 +6,8 @@ export DISPLAY=:1
 
 cd "$1" || exit 1
 
-# --pages-layout are created for selected languages only because they
+# --page-layout are created for selected languages only because they
 # are time consuming.
 # * en_US: source strings
 # * fr_FR: left-to-right translations
-PAGE_LAYOUT_LOCALES='en_US,fr_FR' pytest --pages-layout --junit-xml=/tmp/apptest.xml --junit-prefix=apptest tests/
+PAGE_LAYOUT_LOCALES='en_US,fr_FR' pytest --page-layout --junit-xml=/tmp/apptest.xml --junit-prefix=apptest tests/
