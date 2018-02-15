60ff2554d13558d47508e462469b14d4ab494eaa
redshiftzero <jen@freedom.press>
2018-02-15 11:52:37 -0800

    Add initial pass at an updater Qt program

---
+++ journalist_gui/strings.py
@@ -0,0 +1,6 @@
+update_in_progress = "SecureDrop workstation updates are available! You should install them now. If you don\'t want to, you can install them the next time you reboot."
+fetching_update = 'Fetching and verifying latest update...'
+updating_tails_env = 'Configuring local Tails environment...'
+finished = 'Update complete! You may close the updater.'
+install_update_button = 'Install Now'
+install_later_button = 'Install Later'
