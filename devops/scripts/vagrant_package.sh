d61978a3d98927203d64e7bdc70369a1b1d266d8
Michael Sheinberg <mike@freedom.press>
2018-02-28 22:19:01 -0500

    Fail vagrant package script if preceeding logic bombs

--- devops/scripts/vagrant_package.sh
+++ devops/scripts/vagrant_package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-molecule test -s vagrant_packager
+molecule test -s vagrant_packager && \
 # Unfortunately since we need to prompt the user for sudo creds..
 # I had to break the actual vagrant package logic outside of molecule
-molecule/vagrant_packager/package.py
+molecule/vagrant_packager/package.py && \
 molecule destroy -s vagrant_packager
