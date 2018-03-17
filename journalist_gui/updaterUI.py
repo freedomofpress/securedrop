c6625b3e0d5a0cd6c6cbc0b2851aa21c0877b974
redshiftzero <jen@freedom.press>
2018-03-16 18:45:30 -0700

    Tails: Convert GUI to Python3 and PyQt5

--- journalist_gui/updaterUI.py
+++ journalist_gui/updaterUI.py
@@ -1,46 +1,53 @@
 # -*- coding: utf-8 -*-
 
-from PyQt4 import QtCore, QtGui
+# Form implementation generated from reading ui file 'mainwindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.10
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(323, 300)
-        self.centralwidget = QtGui.QWidget(MainWindow)
+        MainWindow.resize(323, 417)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.gridLayout = QtGui.QGridLayout(self.centralwidget)
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
         self.gridLayout.setObjectName("gridLayout")
-        self.label = QtGui.QLabel(self.centralwidget)
+        self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setWordWrap(True)
         self.label.setObjectName("label")
         self.gridLayout.addWidget(self.label, 0, 0, 1, 2)
-        self.pushButton_2 = QtGui.QPushButton(self.centralwidget)
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_2.setObjectName("pushButton_2")
         self.gridLayout.addWidget(self.pushButton_2, 3, 1, 1, 1)
-        self.progressBar = QtGui.QProgressBar(self.centralwidget)
-        self.progressBar.setProperty("value", 0)
+        self.progressBar = QtWidgets.QProgressBar(self.centralwidget)
+        self.progressBar.setProperty("value", 24)
         self.progressBar.setObjectName("progressBar")
         self.gridLayout.addWidget(self.progressBar, 1, 0, 1, 2)
-        self.scrollArea = QtGui.QScrollArea(self.centralwidget)
+        self.scrollArea = QtWidgets.QScrollArea(self.centralwidget)
         self.scrollArea.setWidgetResizable(True)
         self.scrollArea.setObjectName("scrollArea")
-        self.scrollAreaWidgetContents = QtGui.QWidget()
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
         self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 297, 210))
         self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
-        self.plainTextEdit = QtGui.QPlainTextEdit(self.scrollAreaWidgetContents)
+        self.plainTextEdit = QtWidgets.QPlainTextEdit(self.scrollAreaWidgetContents)
         self.plainTextEdit.setGeometry(QtCore.QRect(0, 0, 301, 241))
+        self.plainTextEdit.setPlainText("")
         self.plainTextEdit.setObjectName("plainTextEdit")
         self.scrollArea.setWidget(self.scrollAreaWidgetContents)
         self.gridLayout.addWidget(self.scrollArea, 2, 0, 1, 2)
-        self.pushButton = QtGui.QPushButton(self.centralwidget)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setObjectName("pushButton")
         self.gridLayout.addWidget(self.pushButton, 3, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 323, 22))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
         self.retranslateUi(MainWindow)
@@ -48,5 +55,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "SecureDrop Updater"))
-        MainWindow.setWindowIcon(QtGui.QIcon('/home/amnesia/Persistent/securedrop/journalist_gui/securedrop_icon.png'))
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "SecureDrop workstation updates are available! You should install them now. If you don\'t want to, you can install them the next time your system boots. "))
+        self.pushButton_2.setText(_translate("MainWindow", "Install Now"))
+        self.pushButton.setText(_translate("MainWindow", "Install Later"))
