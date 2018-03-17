f3f30c12a71ea8b5d0dcfdd2eb40711641db898d
redshiftzero <jen@freedom.press>
2018-03-16 20:01:25 -0700

    Journalist GUI: Add failure and success dialog boxes

--- journalist_gui/strings.py
+++ journalist_gui/strings.py
@@ -1,6 +1,11 @@
+window_title = 'SecureDrop Updater'
 update_in_progress = "SecureDrop workstation updates are available! You should install them now. If you don\'t want to, you can install them the next time you reboot."
 fetching_update = 'Fetching and verifying latest update...'
 updating_tails_env = 'Configuring local Tails environment...'
-finished = 'Update complete! You may close the updater.'
+finished = 'Update successfully completed!'
+finished_dialog_message = 'Updates completed successfully. Click OK to close.'
+finished_dialog_title = 'SecureDrop Workstation is up to date!'
+update_failed_dialog_title = 'Error Updating SecureDrop Workstation'
+update_failed_generic_reason = 'Update failed. Please contact your SecureDrop administrator.'
 install_update_button = 'Install Now'
 install_later_button = 'Install Later'
