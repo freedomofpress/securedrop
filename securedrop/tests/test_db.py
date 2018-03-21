bcfe3934917030ab37486a394ad3ecbdee497802
heartsucker <heartsucker@autistici.org>
2018-03-21 19:07:24 +0100

    pytest fixtures: test_get_one_or_else_no_result_found

--- securedrop/tests/test_db.py
+++ securedrop/tests/test_db.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
-import mock
-
 from flask_testing import TestCase
 from mock import MagicMock
 
 import journalist
+
 from utils import db_helper, env
 from models import (Journalist, Submission, Reply, get_one_or_else,
                     LoginThrottledException)
@@ -39,6 +38,24 @@ def test_get_one_or_else_multiple_results(journalist_app,
         mock_abort.assert_called_with(500)
 
 
+def test_get_one_or_else_no_result_found(journalist_app, test_journo):
+    with journalist_app.app_context():
+        # precondition: there must be one journalist
+        assert Journalist.query.count() == 1
+
+        bad_name = test_journo['username'] + 'aaaaaa'
+        query = Journalist.query.filter_by(username=bad_name)
+
+        mock_logger = MagicMock()
+        mock_abort = MagicMock()
+        get_one_or_else(query, mock_logger, mock_abort)
+
+        log_line = ('Found none when one was expected: '
+                    'No row was found for one()')
+        mock_logger.error.assert_called_with(log_line)
+        mock_abort.assert_called_with(404)
+
+
 class TestDatabase(TestCase):
 
     def create_app(self):
@@ -49,17 +66,6 @@ class TestDatabase(TestCase):
 
     def tearDown(self):
         env.teardown()
-
-    @mock.patch('flask.abort')
-    def test_get_one_or_else_no_result_found(self, mock):
-        query = Journalist.query.filter(Journalist.username == "alice")
-
-        with mock.patch('logger') as mock_logger:
-            get_one_or_else(query, mock_logger, mock)
-        log_line = ('Found none when one was expected: '
-                    'No row was found for one()')
-        mock_logger.error.assert_called_with(log_line)
-        mock.assert_called_with(404)
 
     # Check __repr__ do not throw exceptions
 
