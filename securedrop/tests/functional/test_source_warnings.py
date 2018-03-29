d0893ea518c862ef1718071d68871ab66e4df3df
Mickael E <mickael@freedom.press>
2018-03-29 14:19:34 -0400

    Add Orbot warning in source interface
    - If a user agent matching Orbot accesses the Source interface, a warning will appear informing them that the security and privacy of their browser and device may be affected, and recommend the use of the desktop version of Tor Browser.
    - Add functional test which creates a new profile to set the browser's user agent.

--- securedrop/tests/functional/test_source_warnings.py
+++ securedrop/tests/functional/test_source_warnings.py
@@ -1,5 +1,6 @@
 import source_navigation_steps
 import functional_test
+from selenium import webdriver
 
 
 class TestSourceInterfaceBannerWarnings(
@@ -18,6 +19,34 @@ class TestSourceInterfaceBannerWarnings(
         # User should be able to dismiss the warning
         warning_dismiss_button = self.driver.find_element_by_id(
             'use-tor-browser-close')
+        warning_dismiss_button.click()
+
+        def warning_banner_is_hidden():
+            assert warning_banner.is_displayed() is False
+        self.wait_for(warning_banner_is_hidden)
+
+    def test_warning_appears_if_orbot_is_used(self):
+        orbotUserAgent = ("Mozilla/5.0 (Android; Mobile;"
+                          " rv:52.0) Gecko/20100101 Firefox/52.0")
+
+        # Create new profile and driver with the orbot user agent for this test
+        profile = webdriver.FirefoxProfile()
+        profile.set_preference("general.useragent.override", orbotUserAgent)
+        self.driver = webdriver.Firefox(profile)
+        self.driver.get(self.source_location)
+
+        currentAgent = self.driver.execute_script("return navigator.userAgent")
+        assert currentAgent == orbotUserAgent
+
+        warning_banner = self.driver.find_element_by_class_name(
+            'orfox-browser')
+
+        assert ("It is recommended you use the desktop version of Tor Browser"
+                in warning_banner.text)
+
+        # User should be able to dismiss the warning
+        warning_dismiss_button = self.driver.find_element_by_id(
+            'orfox-browser-close')
         warning_dismiss_button.click()
 
         def warning_banner_is_hidden():
