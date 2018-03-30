1f13992ba8933575bf443c03f1877553f04f5e8b
redshiftzero <jen@freedom.press>
2018-03-30 16:48:25 -0700

    Journalist GUI: Unit test coverage of tag checkout and verification

--- journalist_gui/test_gui.py
+++ journalist_gui/test_gui.py
@@ -1,11 +1,13 @@
 import unittest
+import subprocess
+from unittest import mock
 from PyQt5 import QtCore
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit
 from PyQt5.QtTest import QTest
 
 import time
-from journalist_gui.SecureDropUpdater import UpdaterApp
+from journalist_gui.SecureDropUpdater import UpdaterApp, strings
 
 class AppTestCase(unittest.TestCase):
     def setUp(self):
@@ -39,6 +41,31 @@ class WindowTestCase(AppTestCase):
         #print(pos)
         self.assertEqual(self.window.tabWidget.currentIndex(),
                          self.window.tabWidget.indexOf(self.window.tab_2))
+
+    @mock.patch('subprocess.check_output',
+                return_value=b'Updated to SecureDrop')
+    def test_check_out_latest_tag_success(self, check_output):
+        self.window.check_out_and_verify_latest_tag()
+        self.assertEqual(self.window.update_success, True)
+        self.assertEqual(self.window.progressBar.value(), 40)
+
+    @mock.patch('subprocess.check_output',
+                return_value=b'Signature verification failed')
+    def test_check_out_latest_tag_verification_failure(self, check_output):
+        self.window.check_out_and_verify_latest_tag()
+        self.assertEqual(self.window.update_success, False)
+        self.assertEqual(self.window.failure_reason,
+                         strings.update_failed_sig_failure)
+
+    @mock.patch('subprocess.check_output',
+                side_effect=subprocess.CalledProcessError(
+                    1, 'cmd', 'Generic other failure'))
+    def test_check_out_latest_generic_failure(self, check_output):
+        self.window.check_out_and_verify_latest_tag()
+        self.assertEqual(self.window.update_success, False)
+        self.assertEqual(self.window.failure_reason,
+                         strings.update_failed_generic_reason)
+
 
 if __name__ == '__main__':
     unittest.main()
