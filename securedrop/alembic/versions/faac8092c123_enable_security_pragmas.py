88979eba96cc6342d9491d1e40e2c7349f3c588f
heartsucker <heartsucker@autistici.org>
2018-03-31 12:48:17 +0200

    ensure sqlite pragmas enabled

---
+++ securedrop/alembic/versions/faac8092c123_enable_security_pragmas.py
@@ -0,0 +1,26 @@
+"""enable security pragmas
+
+Revision ID: faac8092c123
+Revises: 15ac9509fc68
+Create Date: 2018-03-31 10:44:26.533395
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'faac8092c123'
+down_revision = '15ac9509fc68'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    conn = op.get_bind()
+    conn.execute(sa.text('PRAGMA secure_delete = ON'))
+    conn.execute(sa.text('PRAGMA auto_vacuum = FULL'))
+
+
+def downgrade():
+    pass
