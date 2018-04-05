945060838cf7407e550540b4b7a62080b6eb5bb9
heartsucker <heartsucker@autistici.org>
2018-04-05 21:21:30 +0200

    DRY alembic test functions

--- securedrop/tests/test_alembic.py
+++ securedrop/tests/test_alembic.py
@@ -10,7 +10,7 @@ from os import path
 
 MIGRATION_PATH = path.join(path.dirname(__file__), '..', 'alembic', 'versions')
 
-ALL_MIGRATIONS = [x.split('.')[0]
+ALL_MIGRATIONS = [x.split('.')[0].split('_')[0]
                   for x in os.listdir(MIGRATION_PATH)
                   if x.endswith('.py')]
 
@@ -24,32 +24,31 @@ def list_migrations(cfg_path, head):
     return migrations
 
 
-@pytest.mark.parametrize('migration',
-                         [x.split('_')[0] for x in ALL_MIGRATIONS])
+def upgrade(alembic_config, migration):
+    subprocess.check_call(['alembic', 'upgrade', migration],
+                          cwd=path.dirname(alembic_config))
+
+
+def downgrade(alembic_config, migration):
+    subprocess.check_call(['alembic', 'downgrade', migration],
+                          cwd=path.dirname(alembic_config))
+
+
+@pytest.mark.parametrize('migration', ALL_MIGRATIONS)
 def test_alembic_migration_upgrade(alembic_config, config, migration):
     # run migrations in sequence from base -> head
     for mig in list_migrations(alembic_config, migration):
-        subprocess.check_call(('cd {} && alembic upgrade {}'
-                               .format(path.dirname(alembic_config),
-                                       mig)),
-                              shell=True)  # nosec
+        upgrade(alembic_config, mig)
 
 
-@pytest.mark.parametrize('migration',
-                         [x.split('_')[0] for x in ALL_MIGRATIONS])
+@pytest.mark.parametrize('migration', ALL_MIGRATIONS)
 def test_alembic_migration_downgrade(alembic_config, config, migration):
     # upgrade to the parameterized test case ("head")
-    subprocess.check_call(('cd {} && alembic upgrade {}'
-                           .format(path.dirname(alembic_config),
-                                   migration)),
-                          shell=True)  # nosec
+    upgrade(alembic_config, migration)
 
     # run migrations in sequence from "head" -> base
     migrations = list_migrations(alembic_config, migration)
     migrations.reverse()
 
     for mig in migrations:
-        subprocess.check_call(('cd {} && alembic downgrade {}'
-                               .format(path.dirname(alembic_config),
-                                       mig)),
-                              shell=True)  # nosec
+        downgrade(alembic_config, mig)
