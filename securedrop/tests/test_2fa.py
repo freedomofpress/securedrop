03d3404c51a52ec6b745884a0b388f5e330bc72a
heartsucker <heartsucker@autistici.org>
2018-04-17 11:59:18 +0200

    pytest fixtures: test_totp_reuse_protections

--- securedrop/tests/test_2fa.py
+++ securedrop/tests/test_2fa.py
@@ -1,15 +1,71 @@
 # -*- coding: utf-8 -*-
 import os
+import pytest
+import time
 
+from contextlib import contextmanager
+from datetime import datetime, timedelta
 from flask import url_for
+from pyotp import TOTP
 import flask_testing
 
 os.environ['SECUREDROP_ENV'] = 'test'  # noqa
 from models import Journalist, BadTokenException
+from utils.instrument import InstrumentedApp
 
 import models
 import journalist
 import utils
+
+
+@contextmanager
+def totp_window():
+    # To ensure we have enough time during a single TOTP window to do the
+    # whole test, optionally sleep.
+    now = datetime.now()
+    mod = now.second % 30
+    if mod < 3:
+        time.sleep(mod % 30)
+        now = datetime.now()
+
+    yield
+
+    # This check ensures that the token was used during the same window
+    # in the event that the app's logic only checks for token reuse if the
+    # token was valid.
+    then = datetime.now()
+    assert then < now + timedelta(seconds=(30 - mod))
+
+
+def test_totp_reuse_protections(journalist_app, test_journo):
+    """Ensure that logging in twice with the same TOTP token fails."""
+    original_hardening = models.LOGIN_HARDENING
+    try:
+        models.LOGIN_HARDENING = True
+
+        with totp_window():
+            token = TOTP(test_journo['otp_secret']).now()
+
+            with journalist_app.test_client() as app:
+                with InstrumentedApp(journalist_app) as ins:
+                    resp = app.post('/login',
+                                    data=dict(username=test_journo['username'],
+                                              password=test_journo['password'],
+                                              token=token))
+                    ins.assert_redirects(resp, '/')
+                resp = app.get('/logout', follow_redirects=True)
+                assert resp.status_code == 200
+
+            with journalist_app.test_client() as app:
+                resp = app.post('/login',
+                                data=dict(username=test_journo['username'],
+                                          password=test_journo['password'],
+                                          token=token))
+                assert resp.status_code == 200
+                text = resp.data.decode('utf-8')
+                assert "Login failed" in text
+    finally:
+        models.LOGIN_HARDENING = original_hardening
 
 
 class TestJournalist2FA(flask_testing.TestCase):
@@ -50,23 +106,6 @@ class TestJournalist2FA(flask_testing.TestCase):
                                           password=self.user_pw,
                                           token=token))
         return resp
-
-    def test_totp_reuse_protections(self):
-        """Ensure that logging in twice with the same TOTP token
-        fails.
-        """
-        original_hardening = models.LOGIN_HARDENING
-        try:
-            models.LOGIN_HARDENING = True
-            token = self.user.totp.now()
-            resp = self._login_user(token)
-            self.assertRedirects(resp, url_for('main.index'))
-
-            resp = self._login_user(token)
-            self.assert200(resp)
-            self.assertIn("Login failed", resp.data)
-        finally:
-            models.LOGIN_HARDENING = original_hardening
 
     def test_totp_reuse_protections2(self):
         """More granular than the preceeding test, we want to make sure
