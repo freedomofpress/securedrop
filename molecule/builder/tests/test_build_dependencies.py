5272fc97d964e756d5f8f0e4b6ce7dbabf1ca4ec
Michael Sheinberg <mike@freedom.press>
2018-05-01 10:22:53 -0400

    Clean-up debian packaging tests
    * Fix some pathing issues
    * Peg debian pkg checks to a container that will mount the build
    directory
    * Removed three tests that can be seen as problematic:
    
     - a test that checks to see that there are no system updates. I see
     this as problematic because this assumes we are not pegging
     dependencies and downloading/installing whatever is latest upstream.
     This makes things harder to QA and test if we have no idea what
     software we are using to build.
    
     - two checks removed that confirm packages have been installed .. I'm
     not sure the exact point of these since the packages will fail build
     without these. These have also been brought into the dockerfile
     definition outside the scope of ansible.

--- molecule/builder/tests/test_build_dependencies.py
+++ molecule/builder/tests/test_build_dependencies.py
@@ -1,48 +1,7 @@
 import pytest
 
 
-securedrop_test_vars = pytest.securedrop_test_vars
-
-
-def get_build_directories():
-    """
-    Helper function to retrieve module-namespace test vars and format
-    the strings to interpolate version info. Keeps the test vars DRY
-    in terms of version info, and required since we can't rely on
-    Jinja-based evaluation of the YAML files (so we can't trivially
-    reuse vars in other var values, as is the case with Ansible).
-    """
-    substitutions = dict(
-            securedrop_version=securedrop_test_vars.securedrop_version,
-            ossec_version=securedrop_test_vars.ossec_version,
-            keyring_version=securedrop_test_vars.keyring_version,
-            config_version=securedrop_test_vars.config_version,
-            )
-    build_directories = [d.format(**substitutions) for d
-                         in securedrop_test_vars.build_directories]
-    return build_directories
-
-
-build_directories = get_build_directories()
-
-
-@pytest.mark.parametrize("package", [
-    "devscripts",
-    "git",
-    "libssl-dev",
-    "python-dev",
-    "python-pip",
-    "secure-delete",
-])
-def test_build_dependencies(Package, package):
-    """
-    Ensure development apt dependencies are installed.
-    The devscripts and git packages are required for running the
-    `update_version.sh` script, which should be executed inside the
-    build VM, so let's make sure they're present.
-    """
-    assert Package(package).is_installed
-
+testinfra_hosts = ['docker://trusty-sd-app']
 
 def test_pip_wheel_installed(Command):
     """
@@ -72,18 +31,8 @@ def test_pip_dependencies_installed(Command):
     assert c.rc == 0
 
 
-@pytest.mark.parametrize("directory", get_build_directories())
-def test_build_directories(File, directory):
-    """
-    Ensure the build directories are present. These directories are
-    the top-level of the Debian packages being created. They contain
-    nested subdirs of varying complexity, depending on package.
-    """
-    if '{}' in directory:
-        directory = directory.format(securedrop_test_vars.securedrop_version)
-    assert File(directory).is_directory
-
-
+@pytest.mark.xfail(reason="This check conflicts with the concept of pegging"
+                          "dependencies")
 def test_build_all_packages_updated(Command):
     """
     Ensure a dist-upgrade has already been run, by checking that no
