4fc204ad1daef55af8e8de2ec33c6d0b15ef921b
redshiftzero <jen@freedom.press>
2018-05-02 15:26:54 -0700

    Update testinfra tests for Referrer-Policy change on source interface

--- testinfra/app/apache/test_apache_source_interface.py
+++ testinfra/app/apache/test_apache_source_interface.py
@@ -27,7 +27,7 @@ def test_apache_headers_source_interface(File, header):
     'WSGIProcessGroup source',
     'WSGIScriptAlias / /var/www/source.wsgi',
     'Header set Cache-Control "no-store"',
-    'Header set Referrer-Policy "no-referrer"',
+    'Header set Referrer-Policy "same-origin"',
     "Alias /static {}/static".format(securedrop_test_vars.securedrop_code),
     """
 <Directory {}/static>
