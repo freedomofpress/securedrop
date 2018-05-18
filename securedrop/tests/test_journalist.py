ac8b83b29d71d6bbc03a02c9d7275ab30b81458f
heartsucker <heartsucker@autistici.org>
2018-05-18 14:46:15 +0200

    pytest fixtures: test_admin_add_user_without_username

--- securedrop/tests/test_journalist.py
+++ securedrop/tests/test_journalist.py
@@ -942,6 +942,19 @@ def test_admin_add_user(journalist_app, test_admin):
                                                uid=new_user.id))
 
 
+def test_admin_add_user_without_username(journalist_app, test_admin):
+    with journalist_app.test_client() as app:
+        _login_user(app, test_admin['username'], test_admin['password'],
+                    test_admin['otp_secret'])
+
+        resp = app.post(url_for('admin.add_user'),
+                        data=dict(username='',
+                                  password=VALID_PASSWORD,
+                                  is_admin=None))
+
+    assert 'This field is required.' in resp.data.decode('utf-8')
+
+
 class TestJournalistApp(TestCase):
 
     # A method required by flask_testing.TestCase
@@ -978,14 +991,6 @@ class TestJournalistApp(TestCase):
 
     def _login_user(self):
         self._ctx.g.user = self.user
-
-    def test_admin_add_user_without_username(self):
-        self._login_admin()
-        resp = self.client.post(url_for('admin.add_user'),
-                                data=dict(username='',
-                                          password=VALID_PASSWORD,
-                                          is_admin=None))
-        self.assertIn('This field is required.', resp.data)
 
     def test_admin_add_user_too_short_username(self):
         self._login_admin()
