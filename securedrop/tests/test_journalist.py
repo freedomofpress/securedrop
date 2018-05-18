7e326c1d8e90f1a72bbf85c53f3342d379e63237
heartsucker <heartsucker@autistici.org>
2018-05-18 14:24:35 +0200

    pytest fixtures: test_user_resets_totp

--- securedrop/tests/test_journalist.py
+++ securedrop/tests/test_journalist.py
@@ -861,6 +861,25 @@ def test_admin_resets_user_totp(journalist_app, test_admin, test_journo):
     assert new_secret != old_secret
 
 
+def test_user_resets_totp(journalist_app, test_journo):
+    old_secret = test_journo['otp_secret']
+
+    with journalist_app.test_client() as app:
+        _login_user(app, test_journo['username'], test_journo['password'],
+                    test_journo['otp_secret'])
+
+        with InstrumentedApp(journalist_app) as ins:
+            resp = app.post(url_for('account.reset_two_factor_totp'))
+            # should redirect to verification page
+            ins.assert_redirects(resp, url_for('account.new_two_factor'))
+
+    # Re-fetch journalist to get fresh DB instance
+    user = Journalist.query.get(test_journo['id'])
+    new_secret = user.otp_secret
+
+    assert new_secret != old_secret
+
+
 class TestJournalistApp(TestCase):
 
     # A method required by flask_testing.TestCase
@@ -897,19 +916,6 @@ class TestJournalistApp(TestCase):
 
     def _login_user(self):
         self._ctx.g.user = self.user
-
-    def test_user_resets_totp(self):
-        self._login_user()
-        old_totp = self.user.totp
-
-        resp = self.client.post(url_for('account.reset_two_factor_totp'))
-        new_totp = self.user.totp
-
-        # check that totp is different
-        self.assertNotEqual(old_totp.secret, new_totp.secret)
-
-        # should redirect to verification page
-        self.assertRedirects(resp, url_for('account.new_two_factor'))
 
     def test_admin_resets_hotp_with_missing_otp_secret_key(self):
         self._login_admin()
