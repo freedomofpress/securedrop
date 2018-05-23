fbc3acf28f92d6c0f98c452416f21cb6fb7f7036
Kushal Das <kushal@freedom.press>
2018-05-23 16:45:41 +0530

    Refactor the test into differnet functions for verification

--- admin/tests/test_integration.py
+++ admin/tests/test_integration.py
@@ -54,59 +54,144 @@ def teardown_function(function):
     os.system('rm -rf {0}'.format(SD_DIR))
 
 
+def verify_username_prompt(child):
+    child.expect("Username for SSH access to the servers:")
+
+
+def verify_reboot_prompt(child):
+    child.expect(
+        "Daily reboot time of the server \(24\-hour clock\):", timeout=2)
+
+
+def verify_ipv4_appserver_prompt(child):
+    child.expect('Local IPv4 address for the Application Server\:', timeout=2)
+
+
+def verify_ipv4_monserver_prompt(child):
+    child.expect('Local IPv4 address for the Monitor Server\:', timeout=2)
+
+
+def verify_hostname_app_prompt(child):
+    child.expect('Hostname for Application Server', timeout=2)
+    child.expect_exact('\x1b[0ma\x1b[0mp\x1b[0mp\x1b[36D\x1b[36C\x1b[?7h\x1b[0m\x1b[?12l\x1b[?25h', timeout=2)  # noqa: E501
+
+
+def verify_hostname_mon_prompt(child):
+    child.expect('Hostname for Monitor Server\:', timeout=2)
+
+
+def verify_dns_prompt(child):
+    child.expect('DNS server specified during installation\:', timeout=2)
+    child.expect_exact('\x1b[0m8\x1b[0m.\x1b[0m8\x1b[0m.\x1b[0m8\x1b[0m.\x1b[0m8\x1b[49D\x1b[49C\x1b[?7h\x1b[0m\x1b[?12l\x1b[?25h')  # noqa: E501
+
+
+def verify_app_gpg_key_prompt(child):
+    child.expect('Local filepath to public key for SecureDrop Application GPG public key\:', timeout=2)  # noqa: E501
+
+
+def verify_https_prompt(child):
+    child.expect('Whether HTTPS should be enabled on Source Interface \(requires EV cert\)\:', timeout=2)  # noqa: E501
+
+
+def verify_app_gpg_fingerprint_prompt(child):
+    child.expect('Full fingerprint for the SecureDrop Application GPG Key\:', timeout=2)  # noqa: E501
+
+
+def verify_ossec_gpg_key_prompt(child):
+    child.expect('Local filepath to OSSEC alerts GPG public key\:', timeout=2)  # noqa: E501
+
+
+def verify_ossec_gpg_fingerprint_prompt(child):
+    child.expect('Full fingerprint for the OSSEC alerts GPG public key\:', timeout=2)  # noqa: E501
+
+
+def verify_admin_email_prompt(child):
+    child.expect('Admin email address for receiving OSSEC alerts\:', timeout=2)  # noqa: E501
+
+
+def verify_journalist_gpg_key_prompt(child):
+    child.expect('Local filepath to journalist alerts GPG public key \(optional\)\:', timeout=2)  # noqa: E501
+
+
+def verify_smtp_relay_prompt(child):
+    child.expect('SMTP relay for sending OSSEC alerts\:', timeout=2)
+    child.expect_exact('[0ms\x1b[0mm\x1b[0mt\x1b[0mp\x1b[0m.\x1b[0mg\x1b[0mm\x1b[0ma\x1b[0mi\x1b[0ml\x1b[0m.\x1b[0mc\x1b[0mo\x1b[0mm\x1b[51D\x1b[51C\x1b[?7h\x1b[0m\x1b[?12l\x1b[?25h')  # noqa: E501
+
+
+def verify_smtp_port_prompt(child):
+    child.expect('SMTP port for sending OSSEC alerts\:', timeout=2)
+    child.expect_exact('\x1b[0m5\x1b[0m8\x1b[0m7\x1b[39D\x1b[39C\x1b[?7h\x1b[0m\x1b[?12l\x1b[?25h')  # noqa: E501
+
+
+def verify_sasl_domain_prompt(child):
+    child.expect('SASL domain for sending OSSEC alerts\:', timeout=2)
+    child.expect_exact('\x1b[0mg\x1b[0mm\x1b[0ma\x1b[0mi\x1b[0ml\x1b[0m.\x1b[0mc\x1b[0mo\x1b[0mm\x1b[47D\x1b[47C\x1b[?7h\x1b[0m\x1b[?12l\x1b[?25h')  # noqa: E501
+
+
+def verify_sasl_username_prompt(child):
+    child.expect('SASL username for sending OSSEC alerts\:', timeout=2)
+
+
+def verify_sasl_password_prompt(child):
+    child.expect('SASL password for sending OSSEC alerts\:', timeout=2)
+
+
+def verify_ssh_over_lan_prompt(child):
+    child.expect('will be available over LAN only\:', timeout=2)
+    child.expect_exact('\x1b[0my\x1b[0me\x1b[0ms\x1b[37D\x1b[37C\x1b[?7h\x1b[0m\x1b[?12l\x1b[?25h')  # noqa: E501
+
+
+def verify_locales_prompt(child):
+    child.expect('Space separated list of additional locales to support')  # noqa: E501
+
+
 def test_firstrun():
     cmd = os.path.join(os.path.dirname(CURRENT_DIR),
                        'securedrop_admin/__init__.py')
     child = pexpect.spawn('python {0} --root {1} sdconfig'.format(cmd, SD_DIR))
-    child.expect("Username for SSH access to the servers:")
+    verify_username_prompt(child)
     child.sendline('')
-    child.expect(
-        "Daily reboot time of the server \(24\-hour clock\):", timeout=2)
-    child.sendline('\b5')
-    child.expect('Local IPv4 address for the Application Server\:', timeout=2)
+    verify_reboot_prompt(child)
+    child.sendline('\b5')  # backspace and put 5
+    verify_ipv4_appserver_prompt(child)
     child.sendline('')
-    child.expect('Local IPv4 address for the Monitor Server\:', timeout=2)
+    verify_ipv4_monserver_prompt(child)
     child.sendline('')
-    child.expect('Hostname for Application Server', timeout=2)
-    child.expect_exact('\x1b[0ma\x1b[0mp\x1b[0mp\x1b[36D\x1b[36C\x1b[?7h\x1b[0m\x1b[?12l\x1b[?25h', timeout=2)  # noqa: E501
+    verify_hostname_app_prompt(child)
     child.sendline('')
-    child.expect('Hostname for Monitor Server\:', timeout=2)
+    verify_hostname_mon_prompt(child)
     child.sendline('')
-    child.expect('DNS server specified during installation\:', timeout=2)
-    child.expect_exact('\x1b[0m8\x1b[0m.\x1b[0m8\x1b[0m.\x1b[0m8\x1b[0m.\x1b[0m8\x1b[49D\x1b[49C\x1b[?7h\x1b[0m\x1b[?12l\x1b[?25h')  # noqa: E501
+    verify_dns_prompt(child)
     child.sendline('')
-    child.expect('Local filepath to public key for SecureDrop Application GPG public key\:', timeout=2)  # noqa: E501
+    verify_app_gpg_key_prompt(child)
     child.sendline('\b' * 14 + 'sd_admin_test.pub')
-    child.expect('Whether HTTPS should be enabled on Source Interface \(requires EV cert\)\:', timeout=2)  # noqa: E501
+    verify_https_prompt(child)
+    # Default answer is no
     child.expect_exact('\x1b[0mn\x1b[0mo\x1b[74D\x1b[74C\x1b[?7h\x1b[0m\x1b[?12l\x1b[?25h')  # noqa: E501
     child.sendline('')
-    child.expect('Full fingerprint for the SecureDrop Application GPG Key\:', timeout=2)  # noqa: E501
+    verify_app_gpg_fingerprint_prompt(child)
     child.sendline('1F544B31C845D698EB31F2FF364F1162D32E7E58')
-    child.expect('Local filepath to OSSEC alerts GPG public key\:', timeout=2)  # noqa: E501
+    verify_ossec_gpg_key_prompt(child)
     child.sendline('\b' * 9 + 'sd_admin_test.pub')
-    child.expect('Full fingerprint for the OSSEC alerts GPG public key\:', timeout=2)  # noqa: E501
+    verify_ossec_gpg_fingerprint_prompt(child)
     child.sendline('1F544B31C845D698EB31F2FF364F1162D32E7E58')
-    child.expect('Admin email address for receiving OSSEC alerts\:', timeout=2)  # noqa: E501
+    verify_admin_email_prompt(child)
     child.sendline('test@gmail.com')
-    child.expect('Local filepath to journalist alerts GPG public key \(optional\)\:', timeout=2)  # noqa: E501
+    verify_journalist_gpg_key_prompt(child)
     child.sendline('')
-    child.expect('SMTP relay for sending OSSEC alerts\:', timeout=2)
-    child.expect_exact('[0ms\x1b[0mm\x1b[0mt\x1b[0mp\x1b[0m.\x1b[0mg\x1b[0mm\x1b[0ma\x1b[0mi\x1b[0ml\x1b[0m.\x1b[0mc\x1b[0mo\x1b[0mm\x1b[51D\x1b[51C\x1b[?7h\x1b[0m\x1b[?12l\x1b[?25h')  # noqa: E501
+    verify_smtp_relay_prompt(child)
     child.sendline('')
-    child.expect('SMTP port for sending OSSEC alerts\:', timeout=2)
-    child.expect_exact('\x1b[0m5\x1b[0m8\x1b[0m7\x1b[39D\x1b[39C\x1b[?7h\x1b[0m\x1b[?12l\x1b[?25h')  # noqa: E501
+    verify_smtp_port_prompt(child)
     child.sendline('')
-    child.expect('SASL domain for sending OSSEC alerts\:', timeout=2)
-    child.expect_exact('\x1b[0mg\x1b[0mm\x1b[0ma\x1b[0mi\x1b[0ml\x1b[0m.\x1b[0mc\x1b[0mo\x1b[0mm\x1b[47D\x1b[47C\x1b[?7h\x1b[0m\x1b[?12l\x1b[?25h')  # noqa: E501
+    verify_sasl_domain_prompt(child)
     child.sendline('')
-    child.expect('SASL username for sending OSSEC alerts\:', timeout=2)
+    verify_sasl_username_prompt(child)
     child.sendline('testuser')
-    child.expect('SASL password for sending OSSEC alerts\:', timeout=2)
+    verify_sasl_password_prompt(child)
     child.sendline('testpassword')
-    child.expect('will be available over LAN only\:', timeout=2)
-    child.expect_exact('\x1b[0my\x1b[0me\x1b[0ms\x1b[37D\x1b[37C\x1b[?7h\x1b[0m\x1b[?12l\x1b[?25h')  # noqa: E501
+    verify_ssh_over_lan_prompt(child)
     child.sendline('')
-    child.expect('Space separated list of additional locales to support')  # noqa: E501
+    verify_locales_prompt(child)
     child.sendline('de_DE es_ES')
     time.sleep(2)  # Give time for validation
     with open(os.path.join(SD_DIR, 'install_files/ansible-base/group_vars/all/site-specific')) as fobj:    # noqa: E501
