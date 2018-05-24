3c72487c17de007a01d7073001a12904f0065671
Michael Sheinberg <mike@freedom.press>
2018-05-24 15:23:40 -0400

    add venvs+pegging for py tools - SD app container
    * Makes sure we dont wreck the short lived container python tooling
    * Utilize the require-hashes for installs
    * Make sure we know what version of pip/wheel we are using

--- molecule/builder/tests/test_build_dependencies.py
+++ molecule/builder/tests/test_build_dependencies.py
@@ -3,14 +3,16 @@ import pytest
 
 testinfra_hosts = ['docker://sd-builder-app']
 
+SD_APP_VENV = "/tmp/sd-app-venv/"
+
 
 def test_pip_wheel_installed(Command):
     """
     Ensure `wheel` is installed via pip, for packaging Python
     dependencies into a Debian package.
     """
-    c = Command("pip freeze")
-    assert "wheel==0.24.0" in c.stdout
+    c = Command("{}/bin/pip freeze --all".format(SD_APP_VENV))
+    assert "wheel==0.31.1" in c.stdout
     assert c.rc == 0
 
 
@@ -27,7 +29,7 @@ def test_pip_dependencies_installed(Command):
     """
     Ensure the development pip dependencies are installed
     """
-    c = Command("pip list installed")
+    c = Command("{}/bin/pip list".format(SD_APP_VENV))
     assert "Flask-Babel" in c.stdout
     assert c.rc == 0
 
