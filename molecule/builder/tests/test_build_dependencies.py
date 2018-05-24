c53a38db4915a46f09396eccc01652b638294fca
Michael Sheinberg <mike@freedom.press>
2018-05-24 15:25:29 -0400

    Remove legacy apt dist-upgrade check
    This test is now covered by the security test checker. Which confirms
    that specifically no security updates are needed.

--- molecule/builder/tests/test_build_dependencies.py
+++ molecule/builder/tests/test_build_dependencies.py
@@ -32,17 +32,3 @@ def test_pip_dependencies_installed(Command):
     c = Command("{}/bin/pip list".format(SD_APP_VENV))
     assert "Flask-Babel" in c.stdout
     assert c.rc == 0
-
-
-@pytest.mark.xfail(reason="This check conflicts with the concept of pegging"
-                          "dependencies")
-def test_build_all_packages_updated(Command):
-    """
-    Ensure a dist-upgrade has already been run, by checking that no
-    packages are eligible for upgrade currently. This will ensure that
-    all upgrades, security and otherwise, have been applied to the VM
-    used to build packages.
-    """
-    c = Command('aptitude --simulate -y dist-upgrade')
-    assert c.rc == 0
-    assert "No packages will be installed, upgraded, or removed." in c.stdout
