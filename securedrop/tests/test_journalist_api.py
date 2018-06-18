79e5b609001392f5bd654f2a4660a2f825dd77d0
redshiftzero <jen@freedom.press>
2018-06-18 15:33:59 -0700

    Journalist API: Security test cases for HTTP DELETEs

--- securedrop/tests/test_journalist_api.py
+++ securedrop/tests/test_journalist_api.py
@@ -110,3 +110,24 @@ def test_user_without_token_cannot_get_protected_endpoints(journalist_app,
                                headers=get_api_headers(''))
 
             assert response.status_code == 403
+
+
+def test_user_without_token_cannot_delete_protected_endpoints(journalist_app,
+                                                              test_source):
+    with journalist_app.app_context():
+        protected_routes = [
+            url_for('api.all_source_submissions',
+                    source_id=test_source['source'].id),
+            url_for('api.single_submission',
+                    source_id=test_source['source'].id,
+                    submission_id=test_source['submissions'][0].id),
+            url_for('api.remove_star',
+                    source_id=test_source['source'].id),
+            ]
+
+    with journalist_app.test_client() as app:
+        for protected_route in protected_routes:
+            response = app.delete(protected_route,
+                               headers=get_api_headers(''))
+
+            assert response.status_code == 403
