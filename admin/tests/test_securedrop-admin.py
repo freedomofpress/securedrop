ff5286629f074d309c1b5604c3e49caa73a4cea4
redshiftzero <jen@freedom.press>
2018-06-25 13:15:03 -0700

    securedrop-admin CLI: update unit tests
    We need to update these tests because we mock out these
    subprocess calls.
    
    (cherry picked from commit d74ffb84c205dea4f87474dec1578e188b29f264)

--- admin/tests/test_securedrop-admin.py
+++ admin/tests/test_securedrop-admin.py
@@ -140,7 +140,10 @@ class TestSecureDropAdmin(object):
                        return_value=(True, "0.6.1")),
             mock.patch('subprocess.check_call'),
             mock.patch('subprocess.check_output',
-                       return_value=git_output),
+                       side_effect=[
+                           git_output,
+                           subprocess.CalledProcessError(1, 'cmd',
+                                                         'not a valid ref')]),
             mock.patch('securedrop_admin.get_release_key_from_keyserver',
                        side_effect=[
                            subprocess.CalledProcessError(1, 'cmd', 'BANG'),
@@ -181,16 +184,29 @@ class TestSecureDropAdmin(object):
                       'gpg: Good signature from "SecureDrop Release '
                       'Signing Key" [unknown]\n')
 
-        with mock.patch('securedrop_admin.check_for_updates',
-                        return_value=(True, "0.6.1")):
-            with mock.patch('subprocess.check_call'):
-                with mock.patch('subprocess.check_output',
-                                return_value=git_output):
-                    ret_code = securedrop_admin.update(args)
-                    assert "Applying SecureDrop updates..." in caplog.text
-                    assert "Signature verification successful." in caplog.text
-                    assert "Updated to SecureDrop" in caplog.text
-                    assert ret_code == 0
+        patchers = [
+            mock.patch('securedrop_admin.check_for_updates',
+                       return_value=(True, "0.6.1")),
+            mock.patch('subprocess.check_call'),
+            mock.patch('subprocess.check_output',
+                       side_effect=[
+                           git_output,
+                           subprocess.CalledProcessError(1, 'cmd',
+                                                         'not a valid ref')]),
+            ]
+
+        for patcher in patchers:
+            patcher.start()
+
+        try:
+            ret_code = securedrop_admin.update(args)
+            assert "Applying SecureDrop updates..." in caplog.text
+            assert "Signature verification successful." in caplog.text
+            assert "Updated to SecureDrop" in caplog.text
+            assert ret_code == 0
+        finally:
+            for patcher in patchers:
+                patcher.stop()
 
     def test_update_signature_does_not_verify(self, tmpdir, caplog):
         git_repo_path = str(tmpdir)
