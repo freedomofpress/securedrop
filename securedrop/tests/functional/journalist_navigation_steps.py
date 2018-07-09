3a561d16c4d3baaf62a1621587cd7b27007c014b
Kushal Das <kushal@freedom.press>
2018-07-10 00:15:46 +0530

    Uses nc rather than torsocks in functional tests
    The version of torsocks in the Trusty repos isn't recent enough to
    support custom ports. Rather than install from other sources, which
    requires manual package verification (or configuring non-trusty repos,
    which could break other packages), let's fall back to good ol' nc.

--- securedrop/tests/functional/journalist_navigation_steps.py
+++ securedrop/tests/functional/journalist_navigation_steps.py
@@ -54,7 +54,7 @@ class JournalistNavigationStepsMixin():
         # Now call the external program to handle the download
         cmd = 'python {0} {1}'.format(cmd_path, temp_cookie_file.name)
         if '.onion' in url:
-            cmd = 'torsocks -P 9150 ' + cmd
+            cmd = 'nc -x 127.0.0.1:9150 ' + cmd
         raw_content = self.system(cmd).strip()
         return raw_content
 
