6185f16824d34d90acd4d50cffc161e96cf1403c
Kushal Das <kushal@freedom.press>
2018-07-13 16:21:42 +0530

    Creates proper orbot specific project to test
    We need to create a new firefox profile to test the orbot specific
    warning. This works for both locally and over Tor.

--- securedrop/tests/functional/test_source_warnings.py
+++ securedrop/tests/functional/test_source_warnings.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 import source_navigation_steps
 import functional_test
 from selenium import webdriver
@@ -26,25 +28,43 @@ class TestSourceInterfaceBannerWarnings(
         orbotUserAgent = ("Mozilla/5.0 (Android; Mobile;"
                           " rv:52.0) Gecko/20100101 Firefox/52.0")
 
+        self.f_profile_path2 = "/tmp/testprofile2"
+        if os.path.exists(self.f_profile_path2):
+            shutil.rmtree(self.f_profile_path2)
         # Create new profile and driver with the orbot user agent for this test
-        profile = webdriver.FirefoxProfile()
+        os.mkdir(self.f_profile_path2)
+        profile = webdriver.FirefoxProfile(self.f_profile_path2)
         profile.set_preference("general.useragent.override", orbotUserAgent)
-        self.driver = webdriver.Firefox(profile)
-        self.driver.get(self.source_location)
+        if self.journalist_location.find('.onion') != -1:
+            # set FF preference to socks proxy in Tor Browser
+            profile.set_preference("network.proxy.type", 1)
+            profile.set_preference("network.proxy.socks", "127.0.0.1")
+            profile.set_preference("network.proxy.socks_port", 9150)
+            profile.set_preference("network.proxy.socks_version", 5)
+            profile.set_preference("network.proxy.socks_remote_dns", True)
+            profile.set_preference("network.dns.blockDotOnion", False)
+        profile.update_preferences()
+        binpath = '/usr/lib/firefox-esr/firefox-esr'
+        self.driver2 = webdriver.Firefox(firefox_binary=binpath,
+                                         firefox_profile=profile)
+        self.driver2.get(self.source_location)
 
-        currentAgent = self.driver.execute_script("return navigator.userAgent")
+        currentAgent = self.driver2.execute_script(
+            "return navigator.userAgent")
         assert currentAgent == orbotUserAgent
 
-        warning_banner = self.driver.find_element_by_class_name(
+        warning_banner = self.driver2.find_element_by_class_name(
             'orfox-browser')
 
         assert ("It is recommended you use the desktop version of Tor Browser"
                 in warning_banner.text)
 
         # User should be able to dismiss the warning
-        warning_dismiss_button = self.driver.find_element_by_id(
+        warning_dismiss_button = self.driver2.find_element_by_id(
             'orfox-browser-close')
         self.banner_is_dismissed(warning_banner, warning_dismiss_button)
+
+        self.driver2.quit()
 
     def banner_is_dismissed(self, warning_banner, dismiss_button):
 
