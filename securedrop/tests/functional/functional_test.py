dd437ce5c8fead8893d9deb09d277801504fcf00
Michael Sheinberg <mike@freedom.press>
2018-08-17 11:37:55 -0400

    Wire-up VNC server and helper command for func tests
    Had to remove x11 display logic inside test scaffolding (initially tried
    to integrate it there but it kept building and destroying the VNC server
    per test).
    
    Made a VNC helper command with support for GNOME desktop and macOS (havent
    tested it on mac yet). Updated the docs

--- securedrop/tests/functional/functional_test.py
+++ securedrop/tests/functional/functional_test.py
@@ -24,7 +24,6 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from tbselenium.tbdriver import TorBrowserDriver
-from tbselenium.utils import start_xvfb, stop_xvfb
 
 os.environ['SECUREDROP_ENV'] = 'test'  # noqa
 
@@ -311,7 +310,6 @@ class FunctionalTest(object):
         # Allow custom session expiration lengths
         self.session_expiration = session_expiration
 
-        self.xvfb_display = start_xvfb()
         if not hasattr(self, 'override_driver'):
             # Means this is not pages-layout tests
             self._create_secondary_firefox_driver()
@@ -345,7 +343,6 @@ class FunctionalTest(object):
             self.driver.quit()
         if self.second_driver:
             self.second_driver.quit()
-        stop_xvfb(self.xvfb_display)
         if self.localtesting:
             self.source_process.terminate()
             self.journalist_process.terminate()
