fe71e06a6ac5632ae3b2bbca05227fbdcb7b0ead
Conor Schaefer <conor@freedom.press>
2018-09-10 11:15:18 -0700

    Updates platform config tests for Xenial support
    The tests now pass if *either* Trusty or Xenial is running. That's
    appropriate, given that we're currently targeting a Trusty -> Xenial
    transition. Depending on the dev/CI context, either platform may be used
    for staging as we hammer out support.

--- molecule/testinfra/staging/common/test_platform.py
+++ molecule/testinfra/staging/common/test_platform.py
@@ -1,3 +1,9 @@
+# We expect Ubuntu, either Trusty or Xenial, the two LTSes
+# currently targeted for support.
+SUPPORTED_CODENAMES = ('trusty', 'xenial')
+SUPPORTED_RELEASES = ('14.04', '16.04')
+
+
 def test_ansible_version(host):
     """
     Check that a supported version of Ansible is being used.
@@ -11,14 +17,14 @@ def test_ansible_version(host):
     assert c.startswith("ansible 2.")
 
 
-def test_platform(SystemInfo):
+def test_platform(host):
     """
     SecureDrop requires Ubuntu Trusty 14.04 LTS. The shelf life
     of that release means we'll need to migrate to Xenial LTS
     at some point; until then, require hosts to be running
     Ubuntu.
     """
-    assert SystemInfo.type == "linux"
-    assert SystemInfo.distribution == "ubuntu"
-    assert SystemInfo.codename == "trusty"
-    assert SystemInfo.release == "14.04"
+    assert host.system_info.type == "linux"
+    assert host.system_info.distribution == "ubuntu"
+    assert host.system_info.codename in SUPPORTED_CODENAMES
+    assert host.system_info.release in SUPPORTED_RELEASES
