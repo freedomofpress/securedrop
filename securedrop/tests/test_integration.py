66dad53c0fc6edf178026668ca2aa2a0841c825f
heartsucker <heartsucker@autistici.org>
2018-09-11 15:47:56 +0200

    pytest fixtures: test_login_after_regenerate_hotp

--- securedrop/tests/test_integration.py
+++ securedrop/tests/test_integration.py
@@ -2,28 +2,23 @@
 
 import gnupg
 import gzip
-import mock
 import os
 import random
 import re
 import shutil
 import tempfile
-import unittest
 import zipfile
 
+from base64 import b32encode
+from binascii import unhexlify
 from bs4 import BeautifulSoup
 from cStringIO import StringIO
 from flask import session, g, escape, current_app
-from pyotp import TOTP
+from pyotp import TOTP, HOTP
 
 os.environ['SECUREDROP_ENV'] = 'test'  # noqa
-from sdconfig import config
-import journalist_app
-import source_app
 import utils
 
-from db import db
-from models import Journalist
 from utils.instrument import InstrumentedApp
 
 # Seed the RNG for deterministic testing
@@ -664,280 +659,35 @@ def test_user_change_password(journalist_app, test_journo):
             ins.assert_redirects(resp, '/')
 
 
-class TestIntegration(unittest.TestCase):
+def test_login_after_regenerate_hotp(journalist_app, test_journo):
+    """Test that journalists can login after resetting their HOTP 2fa"""
 
-    def _login_user(self, app):
-        app.post('/login', data=dict(
-            username=self.username,
-            password=self.user_pw,
-            token='mocked'),
-            follow_redirects=True)
+    otp_secret = 'aaaaaa'
+    b32_otp_secret = b32encode(unhexlify(otp_secret))
 
-    def setUp(self):
-        self.source_app = source_app.create_app(config)
-        self.journalist_app = journalist_app.create_app(config)
+    # edit hotp
+    with journalist_app.test_client() as app:
+        _login_user(app, test_journo)
+        with InstrumentedApp(journalist_app) as ins:
+            resp = app.post('/account/reset-2fa-hotp',
+                            data=dict(otp_secret=otp_secret))
+            # valid otp secrets should redirect
+            ins.assert_redirects(resp, '/account/2fa')
 
-        self.__context = self.journalist_app.app_context()
-        self.__context.push()
-        utils.env.setup()
+            resp = app.post('/account/2fa',
+                            data=dict(token=HOTP(b32_otp_secret).at(0)))
+            # successful verificaton should redirect to /account/account
+            ins.assert_redirects(resp, '/account/account')
 
-        self.gpg = self.journalist_app.crypto_util.gpg
+        # log out
+        app.get('/logout')
 
-        # Patch the two-factor verification to avoid intermittent errors
-        patcher = mock.patch('models.Journalist.verify_token')
-        self.addCleanup(patcher.stop)
-        self.mock_journalist_verify_token = patcher.start()
-        self.mock_journalist_verify_token.return_value = True
-
-        # Add a test user to the journalist interface
-        self.user_pw = "correct horse battery staple haha cultural reference"
-        self.username = self.journalist_app.crypto_util.genrandomid()
-        user = Journalist(username=self.username, password=self.user_pw)
-        db.session.add(user)
-        db.session.commit()
-        self.__context.pop()
-
-    def tearDown(self):
-        self.__context.push()
-        utils.env.teardown()
-        self.__context.pop()
-
-    def _can_decrypt_with_key(self, msg, key_fpr, passphrase=None):
-        """
-        Test that the given GPG message can be decrypted with the given key
-        (identified by its fingerprint).
-        """
-        # GPG does not provide a way to specify which key to use to decrypt a
-        # message. Since the default keyring that we use has both the
-        # `config.JOURNALIST_KEY` and all of the reply keypairs, there's no way
-        # to use it to test whether a message is decryptable with a specific
-        # key.
-        gpg_tmp_dir = tempfile.mkdtemp()
-        gpg = gnupg.GPG(homedir=gpg_tmp_dir)
-
-        # Export the key of interest from the application's keyring
-        pubkey = self.gpg.export_keys(key_fpr)
-        seckey = self.gpg.export_keys(key_fpr, secret=True)
-        # Import it into our isolated temporary GPG directory
-        for key in (pubkey, seckey):
-            gpg.import_keys(key)
-
-        # Attempt decryption with the given key
-        if passphrase:
-            passphrase = current_app.crypto_util.hash_codename(
-                passphrase,
-                salt=current_app.crypto_util.scrypt_gpg_pepper)
-        decrypted_data = gpg.decrypt(msg, passphrase=passphrase)
-        self.assertTrue(
-            decrypted_data.ok,
-            "Could not decrypt msg with key, gpg says: {}".format(
-                decrypted_data.stderr))
-
-        # We have to clean up the temporary GPG dir
-        shutil.rmtree(gpg_tmp_dir)
-
-    def helper_test_reply(self, test_reply, expected_success=True):
-        test_msg = "This is a test message."
-
-        with self.source_app.test_client() as app:
-            resp = app.get('/generate')
-            resp = app.post('/create', follow_redirects=True)
-            codename = session['codename']
-            filesystem_id = g.filesystem_id
-            # redirected to submission form
-            resp = app.post('/submit', data=dict(
-                msg=test_msg,
-                fh=(StringIO(''), ''),
-            ), follow_redirects=True)
-            self.assertEqual(resp.status_code, 200)
-            self.assertFalse(g.source.flagged)
-            app.get('/logout')
-
-        with self.journalist_app.test_client() as app:
-            self._login_user(app)
-            resp = app.get('/')
-            self.assertEqual(resp.status_code, 200)
-            self.assertIn("Sources", resp.data)
-            soup = BeautifulSoup(resp.data, 'html.parser')
-            col_url = soup.select('ul#cols > li a')[0]['href']
-
-            resp = app.get(col_url)
-            self.assertEqual(resp.status_code, 200)
-
-        with self.source_app.test_client() as app:
-            resp = app.post('/login', data=dict(
-                codename=codename), follow_redirects=True)
-            self.assertEqual(resp.status_code, 200)
-            self.assertFalse(g.source.flagged)
-            app.get('/logout')
-
-        with self.journalist_app.test_client() as app:
-            self._login_user(app)
-            resp = app.post('/flag', data=dict(
-                filesystem_id=filesystem_id))
-            self.assertEqual(resp.status_code, 200)
-
-        with self.source_app.test_client() as app:
-            resp = app.post('/login', data=dict(
-                codename=codename), follow_redirects=True)
-            self.assertEqual(resp.status_code, 200)
-            self.assertTrue(g.source.flagged)
-            app.get('/lookup')
-            self.assertTrue(g.source.flagged)
-            app.get('/logout')
-
-        # Block up to 15s for the reply keypair, so we can test sending a reply
-        utils.async.wait_for_assertion(
-            lambda: self.assertNotEqual(
-                current_app.crypto_util.getkey(filesystem_id),
-                None),
-            15)
-
-        # Create 2 replies to test deleting on journalist and source interface
-        with self.journalist_app.test_client() as app:
-            self._login_user(app)
-            for i in range(2):
-                resp = app.post('/reply', data=dict(
-                    filesystem_id=filesystem_id,
-                    message=test_reply
-                ), follow_redirects=True)
-                self.assertEqual(resp.status_code, 200)
-
-            if not expected_success:
-                pass
-            else:
-                text = resp.data.decode('utf-8')
-                assert "Thanks. Your reply has been stored." in text, text
-
-            resp = app.get(col_url)
-            self.assertIn("reply-", resp.data)
-
-        soup = BeautifulSoup(resp.data, 'html.parser')
-
-        # Download the reply and verify that it can be decrypted with the
-        # journalist's key as well as the source's reply key
-        filesystem_id = soup.select('input[name="filesystem_id"]')[0]['value']
-        checkbox_values = [
-            soup.select('input[name="doc_names_selected"]')[1]['value']]
-        resp = app.post('/bulk', data=dict(
-            filesystem_id=filesystem_id,
-            action='download',
-            doc_names_selected=checkbox_values
-        ), follow_redirects=True)
-        self.assertEqual(resp.status_code, 200)
-
-        zf = zipfile.ZipFile(StringIO(resp.data), 'r')
-        data = zf.read(zf.namelist()[0])
-        self._can_decrypt_with_key(data, config.JOURNALIST_KEY)
-        self._can_decrypt_with_key(
-            data,
-            current_app.crypto_util.getkey(filesystem_id),
-            codename)
-
-        # Test deleting reply on the journalist interface
-        last_reply_number = len(
-            soup.select('input[name="doc_names_selected"]')) - 1
-        self.helper_filenames_delete(app, soup, last_reply_number)
-
-        with self.source_app.test_client() as app:
-            resp = app.post('/login', data=dict(codename=codename),
-                            follow_redirects=True)
-            self.assertEqual(resp.status_code, 200)
-            resp = app.get('/lookup')
-            self.assertEqual(resp.status_code, 200)
-
-            if not expected_success:
-                # there should be no reply
-                self.assertNotIn("You have received a reply.", resp.data)
-            else:
-                self.assertIn(
-                    "You have received a reply. To protect your identity",
-                    resp.data)
-                self.assertIn(test_reply, resp.data)
-                soup = BeautifulSoup(resp.data, 'html.parser')
-                msgid = soup.select(
-                    'form.message > input[name="reply_filename"]')[0]['value']
-                resp = app.post('/delete', data=dict(
-                    filesystem_id=filesystem_id,
-                    reply_filename=msgid
-                ), follow_redirects=True)
-                self.assertEqual(resp.status_code, 200)
-                self.assertIn("Reply deleted", resp.data)
-
-            app.get('/logout')
-
-    def test_login_after_regenerate_hotp(self):
-        """Test that journalists can login after resetting their HOTP 2fa"""
-
-        # edit hotp
-        with self.journalist_app.test_client() as app:
-            self._login_user(app)
-            otp_secret = '123456'
-            app.post('/account/reset-2fa-hotp', data=dict(
-                otp_secret=otp_secret))
-
-            user = Journalist(username=self.username,
-                              password=self.user_pw,
-                              otp_secret=otp_secret)
-
-            # successful verificaton should redirect to /account
-            resp = app.post('/account/2fa', data=dict(
-                token=user.hotp))
-            self.assertEqual(resp.status_code, 302)
-
-            # log out
-            app.get('/logout')
-
+    # start a new client/context to be sure we've cleared the session
+    with journalist_app.test_client() as app:
+        with InstrumentedApp(journalist_app) as ins:
             # login with new 2fa secret should redirect to index page
             resp = app.post('/login', data=dict(
-                username=self.username,
-                password=self.user_pw,
-                token=user.hotp,
-                follow_redirects=True))
-            self.assertEqual(resp.status_code, 302)
-
-    def helper_filenames_submit(self, app):
-        app.post('/submit', data=dict(
-            msg="This is a test.",
-            fh=(StringIO(''), ''),
-        ), follow_redirects=True)
-        app.post('/submit', data=dict(
-            msg="This is a test.",
-            fh=(StringIO('This is a test'), 'test.txt'),
-        ), follow_redirects=True)
-        app.post('/submit', data=dict(
-            msg="",
-            fh=(StringIO('This is a test'), 'test.txt'),
-        ), follow_redirects=True)
-
-    def helper_filenames_delete(self, app, soup, i):
-        filesystem_id = soup.select('input[name="filesystem_id"]')[0]['value']
-        checkbox_values = [
-            soup.select('input[name="doc_names_selected"]')[i]['value']]
-
-        # delete
-        resp = app.post('/bulk', data=dict(
-            filesystem_id=filesystem_id,
-            action='confirm_delete',
-            doc_names_selected=checkbox_values
-        ), follow_redirects=True)
-        self.assertEqual(resp.status_code, 200)
-        self.assertIn(
-            "The following file has been selected for"
-            " <strong>permanent deletion</strong>",
-            resp.data)
-
-        # confirm delete
-        resp = app.post('/bulk', data=dict(
-            filesystem_id=filesystem_id,
-            action='delete',
-            doc_names_selected=checkbox_values
-        ), follow_redirects=True)
-        self.assertEqual(resp.status_code, 200)
-        self.assertIn("Submission deleted.", resp.data)
-
-        # Make sure the files were deleted from the filesystem
-        utils.async.wait_for_assertion(lambda: self.assertFalse(
-            any([os.path.exists(current_app.storage.path(filesystem_id,
-                                                         doc_name))
-                 for doc_name in checkbox_values])))
+                username=test_journo['username'],
+                password=test_journo['password'],
+                token=HOTP(b32_otp_secret).at(1)))
+            ins.assert_redirects(resp, '/')
