148cad118d7e390b4e30c004dc4b4b43741a3da5
Kevin O Gorman <kevin.ogorman@gmail.com>
2018-09-27 13:43:13 -0400

    added ini file to get around remote-viewer password prompt

--- devops/scripts/vnc-docker-connect.sh
+++ devops/scripts/vnc-docker-connect.sh
@@ -22,8 +22,13 @@ SD_DOCKER_IP="$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddre
 nc -w5 -z "$SD_DOCKER_IP" 5901 || (echo "ERROR: VNC server not found"; exit 1)
 
 # Are we running on gnome desktop?
-if [ "$(echo \"$XDG_DATA_DIRS\" | sed 's/.*\(gnome\).*/\1/')" == "gnome" ]; then
-    remote-viewer "vnc://${USER}:freedom@${SD_DOCKER_IP}:5901" 2>/dev/null &
+if [ "$(echo \"$XDG_DATA_DIRS\" | sed 's/.*\(gnome\).*/\1/')" = "gnome" ]; then
+    echo -e "[virt-viewer]\ntype=vnc\nhost=${SD_DOCKER_IP}\nport=5901\npassword=freedom" > /tmp/func-vnc.ini	
+    remote-viewer /tmp/func-vnc.ini 2>/dev/null &
+
+elif [ "$(echo \"$XDG_DATA_DIRS\" | sed 's/.*\(ubuntu\).*/\1/')" = "ubuntu" ]; then
+    echo -e "[virt-viewer]\ntype=vnc\nhost=${SD_DOCKER_IP}\nport=5901\npassword=freedom" > /tmp/func-vnc.ini	
+    remote-viewer /tmp/func-vnc.ini 2>/dev/null &
 
 else
     echo "Not sure what the VNC terminal cli arguments are for your OS."
