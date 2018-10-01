a4b6786b6b6a14261c7cc419ce571a2d0df201f0
heartsucker <heartsucker@autistici.org>
2018-10-01 21:10:17 +0200

    pytest fixtures: test_source_session_timeout

--- securedrop/tests/functional/test_source.py
+++ securedrop/tests/functional/test_source.py
@@ -1,4 +1,11 @@
+import pytest
+
 from functional_test import FunctionalHelper
+
+# The session expiration here cannot be set to -1 as it will trigger an
+# exception in /create. Instead, we pick a 1-2s value to allow the account to
+# be generated.
+EXPIRATION_MINS = 0.03
 
 
 def test_lookup_codename_hint(live_source_app, webdriver):
@@ -16,3 +23,21 @@ def test_lookup_codename_hint(live_source_app, webdriver):
     helper._source_chooses_to_login()
     helper._source_proceeds_to_login()
     helper._source_sees_no_codename()
+
+
+@pytest.fixture(scope='function')
+def pre_create_config(config):
+    config.SESSION_EXPIRATION_MINUTES = EXPIRATION_MINS
+
+
+def test_source_session_timeout(live_source_app, webdriver, pre_create_config):
+    helper = FunctionalHelper(
+        driver=webdriver,
+        live_source_app=live_source_app)
+
+    helper._source_visits_source_homepage()
+    helper._source_clicks_submit_documents_on_homepage()
+    helper._source_continues_to_submit_page()
+    helper._source_waits_for_session_to_timeout(EXPIRATION_MINS)
+    helper._source_visits_source_homepage()
+    helper._source_sees_session_timeout_message()
