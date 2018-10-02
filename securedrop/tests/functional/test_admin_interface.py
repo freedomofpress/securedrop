c022651adceb7b9367ebda58d625c814e8828e0b
heartsucker <heartsucker@autistici.org>
2018-10-02 12:28:32 +0200

    pytest fixtures: test_admin_edits_hotp_secret

--- securedrop/tests/functional/test_admin_interface.py
+++ securedrop/tests/functional/test_admin_interface.py
@@ -23,18 +23,29 @@ def test_admin_interface(
     helper._admin_can_edit_new_user()
 
 
+def test_admin_edits_hotp_secret(
+        test_admin,
+        live_source_app,
+        live_journalist_app,
+        webdriver):
+
+    helper = FunctionalHelper(
+        driver=webdriver,
+        admin=test_admin,
+        live_source_app=live_source_app,
+        live_journalist_app=live_journalist_app)
+
+    helper._admin_logs_in()
+    helper._admin_visits_admin_interface()
+    helper._admin_adds_a_user()
+    helper._admin_visits_edit_user()
+    helper._admin_visits_reset_2fa_hotp()
+    helper._admin_accepts_2fa_js_alert()
+
+
 class TestAdminInterface(
         functional_test.FunctionalTest,
         journalist_navigation_steps.JournalistNavigationStepsMixin):
-
-
-    def test_admin_edits_hotp_secret(self):
-        self._admin_logs_in()
-        self._admin_visits_admin_interface()
-        self._admin_adds_a_user()
-        self._admin_visits_edit_user()
-        self._admin_visits_reset_2fa_hotp()
-        self._admin_accepts_2fa_js_alert()
 
     def test_admin_deletes_user(self):
         self._admin_logs_in()
