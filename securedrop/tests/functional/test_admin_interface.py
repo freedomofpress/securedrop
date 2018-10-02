cdfe3170adb47a9412c9f899800c9110c8bf29cc
heartsucker <heartsucker@autistici.org>
2018-10-02 12:25:31 +0200

    pytest fixtures: test_admin_interface

--- securedrop/tests/functional/test_admin_interface.py
+++ securedrop/tests/functional/test_admin_interface.py
@@ -1,17 +1,32 @@
 import functional_test
 import journalist_navigation_steps
 
+from functional_test import FunctionalHelper
+
+
+def test_admin_interface(
+        test_admin,
+        live_source_app,
+        live_journalist_app,
+        webdriver):
+
+    helper = FunctionalHelper(
+        driver=webdriver,
+        admin=test_admin,
+        live_source_app=live_source_app,
+        live_journalist_app=live_journalist_app)
+
+    helper._admin_logs_in()
+    helper._admin_visits_admin_interface()
+    helper._admin_adds_a_user()
+    helper._new_user_can_log_in()
+    helper._admin_can_edit_new_user()
+
 
 class TestAdminInterface(
         functional_test.FunctionalTest,
         journalist_navigation_steps.JournalistNavigationStepsMixin):
 
-    def test_admin_interface(self):
-        self._admin_logs_in()
-        self._admin_visits_admin_interface()
-        self._admin_adds_a_user()
-        self._new_user_can_log_in()
-        self._admin_can_edit_new_user()
 
     def test_admin_edits_hotp_secret(self):
         self._admin_logs_in()
