49b0a6f32fd15f54547ff071828ed3529986f3aa
heartsucker <heartsucker@autistici.org>
2018-10-02 12:37:55 +0200

    pytest fixtures: test_admin_edit_account_html_template_rendering

--- securedrop/tests/functional/test_admin_interface.py
+++ securedrop/tests/functional/test_admin_interface.py
@@ -92,3 +92,33 @@ def test_ossec_alert_button(
     helper._admin_visits_admin_interface()
     helper._admin_visits_system_config_page()
     helper._admin_can_send_test_alert()
+
+
+def test_admin_edit_account_html_template_rendering(
+        test_admin,
+        live_source_app,
+        live_journalist_app,
+        webdriver):
+    """The edit_account.html template is used both when an admin is editing
+    a user's account, and when a user is editing their own account. While
+    there is no security risk in doing so, we do want to ensure the UX is
+    as expected: that only the elements that belong in a particular view
+    are exposed there."""
+
+    helper = FunctionalHelper(
+        driver=webdriver,
+        admin=test_admin,
+        live_source_app=live_source_app,
+        live_journalist_app=live_journalist_app)
+
+    helper._admin_logs_in()
+    helper._admin_visits_admin_interface()
+    # Admin view of admin user
+    helper._edit_user(helper.admin['username'])
+    helper._admin_visits_admin_interface()
+    helper._admin_adds_a_user()
+    # Admin view of non-admin user
+    helper._edit_user(helper.new_user['username'])
+    # User view of helper
+    helper._edit_account()
+    helper._logout()
