a64f4d2c2db66ec1b7192f0dbbdcdcca766477a6
heartsucker <heartsucker@autistici.org>
2018-10-02 11:49:27 +0200

    pytest fixtures: test_warning_appears_if_orbot_is_used

--- securedrop/tests/functional/test_source_warnings.py
+++ securedrop/tests/functional/test_source_warnings.py
@@ -1,34 +1,37 @@
-import source_navigation_steps
-import functional_test
-from selenium import webdriver
+import pytest
+
+from selenium.webdriver import FirefoxProfile
+
+from functional_test import FunctionalHelper
+
+ORBOT_UA = ("Mozilla/5.0 (Android; Mobile;"
+            " rv:52.0) Gecko/20100101 Firefox/52.0")
 
 
-class TestSourceInterfaceBannerWarnings(
-        functional_test.FunctionalTest,
-        source_navigation_steps.SourceNavigationStepsMixin):
+# Create new profile and driver with the orbot user agent for just this test
+@pytest.fixture(autouse=True, scope='module')
+def firefox_profile():
+    profile = FirefoxProfile()
+    profile.set_preference("general.useragent.override", ORBOT_UA)
+    return profile
 
 
-    def test_warning_appears_if_orbot_is_used(self):
-        orbotUserAgent = ("Mozilla/5.0 (Android; Mobile;"
-                          " rv:52.0) Gecko/20100101 Firefox/52.0")
+def test_warning_appears_if_orbot_is_used(live_source_app, webdriver):
+    helper = FunctionalHelper(
+        driver=webdriver,
+        live_source_app=live_source_app)
 
-        # Create new profile and driver with the orbot user agent for this test
-        profile = webdriver.FirefoxProfile()
-        profile.set_preference("general.useragent.override", orbotUserAgent)
-        self.driver = webdriver.Firefox(profile)
-        self.driver.get(self.source_location)
+    helper.driver.get(helper.source_location)
+    currentAgent = helper.driver.execute_script("return navigator.userAgent")
+    assert currentAgent == ORBOT_UA  # precondition
 
-        currentAgent = self.driver.execute_script("return navigator.userAgent")
-        assert currentAgent == orbotUserAgent
+    warning_banner = helper.driver.find_element_by_class_name(
+        'orfox-browser')
 
-        warning_banner = self.driver.find_element_by_class_name(
-            'orfox-browser')
+    assert ("It is recommended you use the desktop version of Tor Browser"
+            in warning_banner.text)
 
-        assert ("It is recommended you use the desktop version of Tor Browser"
-                in warning_banner.text)
-
-        # User should be able to dismiss the warning
-        warning_dismiss_button = self.driver.find_element_by_id(
-            'orfox-browser-close')
-        self.banner_is_dismissed(warning_banner, warning_dismiss_button)
-
+    # User should be able to dismiss the warning
+    warning_dismiss_button = helper.driver.find_element_by_id(
+        'orfox-browser-close')
+    helper.banner_is_dismissed(warning_banner, warning_dismiss_button)
