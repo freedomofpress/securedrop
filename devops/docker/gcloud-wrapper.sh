8ba4edf2b5bda9eac5ae2f5d3948175567007e06
Michael Sheinberg <mike@freedom.press>
2018-10-26 15:03:55 -0700

    Ensure activate svc account logic always fires
    Was masking some errors previously and i've learned that this script
    will fire multiple times without issue.

--- devops/docker/gcloud-wrapper.sh
+++ devops/docker/gcloud-wrapper.sh
@@ -6,8 +6,8 @@ set -e
 SVC_ACCT_FILE=/gce-svc-acct.json
 
 # Try to authenticate Google tooling
-(gcloud auth activate-service-account \
---key-file "${SVC_ACCT_FILE}" > /dev/null 2>&1) || true
+gcloud auth activate-service-account \
+    --key-file "${SVC_ACCT_FILE}" > /dev/null
 
 # Run the container in background, allows subsequent system calls
 if [ "$1" = "background" ]; then
