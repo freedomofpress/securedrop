888bdbfde458281d7bae301a467af5de81d8c24a
Michael Sheinberg <mike@freedom.press>
2018-10-26 16:37:20 -0700

    Switch to using ssh metadata instead of oslogin
    I encountered lots of issues with oslogin, the biggest is that it forces
    us to log into a new user where we really want to login as the `sdci`
    user (which has all the vagrant+libvirt settings applied already).
    
    In comparison, passing metadata on instance creation is super
    straight-forward. Followed docs from
    https://cloud.google.com/compute/docs/instances/adding-removing-ssh-keys

--- devops/gce-nested/gce-start.sh
+++ devops/gce-nested/gce-start.sh
@@ -5,7 +5,6 @@
 
 set -u
 set -e
-set -x
 
 # Create ephemeral directory
 mkdir -p "${EPHEMERAL_DIRECTORY}" || true
@@ -24,16 +23,14 @@ IMG_LOCATE=$(gcloud_call compute images list \
     --filter="family:fpf-securedrop AND name ~ ^ci-nested-virt" \
     --sort-by=~Name --limit=1 --format="value(Name)")
 
-# Add oslogin ssh key
-gcloud_call compute os-login ssh-keys add \
-    --key-file /gce.pub \
-    --ttl 2h
-
 if ! gcloud_call compute instances describe "${FULL_JOB_ID}" 2>&1 > /dev/null; then
     # Fire-up remote instance
     gcloud_call compute instances create "${FULL_JOB_ID}" \
         --image="${IMG_LOCATE}" \
         --network securedropci \
         --subnet ci-subnet \
-        --metadata enable-oslogin=TRUE
+        --metadata "ssh-keys=sdci:$(cat ${EPHEMERAL_DIRECTORY}/gce.pub)"
+
+    # Give box a few more seconds for SSH to become available
+    sleep 10
 fi
