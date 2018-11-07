5c7f35f4e73bb57b1939ef57578eec5a31c78092
redshiftzero <jen@freedom.press>
2018-11-07 10:19:52 -0800

    Test replies: Encrypt a string instead of 1 random byte

--- securedrop/create-dev-data.py
+++ securedrop/create-dev-data.py
@@ -86,7 +86,7 @@ def create_source_and_submissions(num_submissions=2, num_replies=2):
         fname = "{}-{}-reply.gpg".format(source.interaction_count,
                                          source.journalist_filename)
         current_app.crypto_util.encrypt(
-            str(os.urandom(1)),
+            'this is a test reply!',
             [current_app.crypto_util.getkey(source.filesystem_id),
              config.JOURNALIST_KEY],
             current_app.storage.path(source.filesystem_id, fname))
