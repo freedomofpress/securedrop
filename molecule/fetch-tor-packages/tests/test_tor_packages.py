afc4f6bbbc41ee41287c0f9a5ed5ea4bbea328eb
Conor Schaefer <conor@freedom.press>
2019-02-05 14:27:19 -0800

    Fetches upstream Tor debs via Molecule
    Uses another Molecule-based build scenario that fetches the Xenial tor debs
    for inclusion in the FPF-maintained apt repository. The debs are fetched into
    the same `build/xenial/` dir as the SD debs. There's no corresponding logic
    for fetching Trusty debs, since Tor has stopped backporting tor for trusty.
    
    The major change here is the update to the Tor Project public key, which
    has expired since we last updated the pubkey stored in this repo. We haven't used
    the Tor pubkey directly since switching to an apt mirror for distributing tor debs.
    
    As of 0.12.0, we plan to use a single, consolidated apt repo, to distribute both
    SecureDrop and Tor deb packages.

---
+++ molecule/fetch-tor-packages/tests/test_tor_packages.py
@@ -0,0 +1,50 @@
+import os
+import pytest
+
+
+testinfra_hosts = ['docker://tor-package-fetcher-xenial']
+TOR_DOWNLOAD_DIR = "/tmp/tor-debs"
+TOR_PACKAGES = [
+    {"name": "tor", "arch": "amd64"},
+    {"name": "tor-geoipdb", "arch": "all"},
+]
+TOR_VERSION = "0.3.5.7-1~xenial+1"
+
+
+def test_tor_apt_repo(host):
+    """
+    Ensure the upstream Tor Project repo is correct, since that's
+    where we've fetched the deb packages from.
+    """
+    repo_file = "/etc/apt/sources.list.d/deb_torproject_org_torproject_org.list"  # noqa
+    f = host.file(repo_file)
+    assert f.exists
+    assert f.contains("https://deb.torproject.org")
+
+
+@pytest.mark.parametrize("pkg", TOR_PACKAGES)
+def test_tor_package_versions(host, pkg):
+    """
+    Inspect package info and confirm we're getting the version we expect.
+    """
+    package_name = "{}_{}_{}.deb".format(pkg["name"], TOR_VERSION, pkg["arch"])
+    filepath = os.path.join(TOR_DOWNLOAD_DIR, package_name)
+    f = host.file(filepath)
+    assert f.exists
+    assert f.is_file
+
+    cmd = "dpkg-deb -f {} Version".format(filepath)
+    package_version = host.check_output(cmd)
+    assert package_version == TOR_VERSION
+
+
+def test_tor_package_platform(host):
+    """
+    Sanity check to ensure we're running on Xenial, which is the only
+    option for SecureDrop distributions supported by upstream Tor Project.
+    The Trusty channel was disabled by Tor Project on 2019-01-08.
+    """
+    assert host.system_info.type == "linux"
+    assert host.system_info.distribution == "ubuntu"
+    assert host.system_info.codename == "xenial"
+    assert host.system_info.release == "16.04"
