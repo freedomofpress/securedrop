45fcd7b03340a075412c54b9c6d529b51b27266c
mickael e <mickael@freedom.press>
2019-04-15 14:28:29 -0400

    Bump Ansible to 2.6.14
    Address CVE-2019-3828

--- install_files/ansible-base/callback_plugins/ansible_version_check.py
+++ install_files/ansible-base/callback_plugins/ansible_version_check.py
@@ -21,7 +21,7 @@ class CallbackModule(CallbackBase):
     def __init__(self):
         # Can't use `on_X` because this isn't forwards compatible
         # with Ansible 2.0+
-        required_version = '2.6.8'  # Keep synchronized with requirements files
+        required_version = '2.6.14'  # Keep synchronized with requirements files
         if not ansible.__version__.startswith(required_version):
             print_red_bold(
                 "SecureDrop restriction: only Ansible {version}.*"
