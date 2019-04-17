62452ff553abc2ccc553e3a81d237b6f48cfed8e
John Hensley <john@freedom.press>
2019-04-17 16:40:22 -0400

    Fix Dockerfiles, create-dev-data
    In the Dockerfiles, running apt-install in a separate RUN after
    apt-update could fail if the output of the apt-update's RUN were
    cached; apt-install could be looking for outdated versions of
    packages.
    
    In create-dev-data, fix a misplaced print/format parenthesis.

--- securedrop/create-dev-data.py
+++ securedrop/create-dev-data.py
@@ -103,8 +103,8 @@ def create_source_and_submissions(num_submissions=2, num_replies=2):
     db.session.commit()
 
     print("Test source (codename: '{}', journalist designation '{}') "
-          "added with {} submissions and {} replies").format(
-              codename, journalist_designation, num_submissions, num_replies)
+          "added with {} submissions and {} replies".format(
+              codename, journalist_designation, num_submissions, num_replies))
 
 
 if __name__ == "__main__":  # pragma: no cover
