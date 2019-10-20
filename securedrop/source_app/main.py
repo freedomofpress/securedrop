f820b0452d15cf488e25be386c7ede63623d30fc
Whistleblower Aid <54696764+wbaid@users.noreply.github.com>
2019-10-20 18:25:28 -0400

    feat(/lookup,/submit): disable file submissions if ALLOW_DOCUMENT_UPLOADS = False

--- securedrop/source_app/main.py
+++ securedrop/source_app/main.py
@@ -83,6 +83,9 @@ def make_blueprint(config):
     @view.route('/lookup', methods=('GET',))
     @login_required
     def lookup():
+        allow_document_uploads = current_app.config.get(
+            'ALLOW_DOCUMENT_UPLOADS',
+            True)
         replies = []
         source_inbox = Reply.query.filter(Reply.source_id == g.source.id) \
                                   .filter(Reply.deleted_by_source == False).all()  # noqa
@@ -121,6 +124,7 @@ def make_blueprint(config):
 
         return render_template(
             'lookup.html',
+            allow_document_uploads=allow_document_uploads,
             codename=g.codename,
             replies=replies,
             flagged=g.source.flagged,
@@ -131,9 +135,12 @@ def make_blueprint(config):
     @view.route('/submit', methods=('POST',))
     @login_required
     def submit():
+        allow_document_uploads = current_app.config.get(
+            'ALLOW_DOCUMENT_UPLOADS',
+            True)
         msg = request.form['msg']
         fh = None
-        if 'fh' in request.files:
+        if allow_document_uploads and 'fh' in request.files:
             fh = request.files['fh']
 
         # Don't submit anything if it was an "empty" submission. #878
