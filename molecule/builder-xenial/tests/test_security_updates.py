436229bc16140567c67501858675f7b9997c820b
Kevin O'Gorman <kog@freedom.press>
2019-11-11 17:12:53 -0800

    Restricts build-debs builder updates test to RCs and update-builder* branches

--- molecule/builder-xenial/tests/test_security_updates.py
+++ molecule/builder-xenial/tests/test_security_updates.py
@@ -1,16 +1,31 @@
 import os
+from subprocess import check_output
+import re
+import pytest
+
 SECUREDROP_TARGET_PLATFORM = os.environ.get("SECUREDROP_TARGET_PLATFORM")
 testinfra_hosts = [
         "docker://{}-sd-sec-update".format(SECUREDROP_TARGET_PLATFORM)
 ]
 
 
+def is_rc_version():
+    command = "git symbolic-ref -q --short HEAD || git describe --tags --exact-match"
+    version = check_output(command, shell=True).\
+        decode("utf8")[0:-1]
+    result = re.match(r"(^[\d]+\.[\d]+\.[\d]+-rc[\d]+)|(^update-builder.*)", version)
+    if result:
+        return True
+    else:
+        return False
+
+
+@pytest.mark.skipif(not is_rc_version(), reason="Only tested for RCs")
 def test_ensure_no_updates_avail(host):
     """
         Test to make sure that there are no security-updates in the
         base builder container.
     """
-
     # Filter out all the security repos to their own file
     # without this change all the package updates appeared as if they were
     # coming from normal ubuntu update channel (since they get posted to both)
