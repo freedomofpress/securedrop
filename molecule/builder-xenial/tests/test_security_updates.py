5bde7d51de2383f2121636e3368a832bf2d9803b
Kevin O'Gorman <kog@freedom.press>
2019-11-11 17:12:53 -0800

    Restricts builder security updates test to RC tags, release tags, and update-builder* branches

--- molecule/builder-xenial/tests/test_security_updates.py
+++ molecule/builder-xenial/tests/test_security_updates.py
@@ -1,16 +1,34 @@
 import os
+from subprocess import check_output
+import re
+import pytest
+
 SECUREDROP_TARGET_PLATFORM = os.environ.get("SECUREDROP_TARGET_PLATFORM")
 testinfra_hosts = [
         "docker://{}-sd-sec-update".format(SECUREDROP_TARGET_PLATFORM)
 ]
 
 
+def test_should_run():
+    command = ["git", "describe", "--all"]
+    version = check_output(command).decode("utf8")[0:-1]
+    candidates = (r"(^tags/[\d]+\.[\d]+\.[\d]+-rc[\d]+)|"
+                  r"(^tags/[\d]+\.[\d]+\.[\d]+)|"
+                  r"(^heads/release/[\d]+\.[\d]+\.[\d]+)|"
+                  r"(^heads/update-builder.*)")
+    result = re.match(candidates, version)
+    if result:
+        return True
+    else:
+        return False
+
+
+@pytest.mark.skipif(not test_should_run(), reason="Only tested for RCs and builder updates")
 def test_ensure_no_updates_avail(host):
     """
         Test to make sure that there are no security-updates in the
         base builder container.
     """
-
     # Filter out all the security repos to their own file
     # without this change all the package updates appeared as if they were
     # coming from normal ubuntu update channel (since they get posted to both)
