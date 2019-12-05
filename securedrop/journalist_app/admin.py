2d95ebf77522b070d28e22b6db3d7f6f14165b26
grey275 <pjvanderpol@gmail.com>
2019-12-04 19:05:54 -0800

    lowecase "saved" in flashed methods for consistency

--- securedrop/journalist_app/admin.py
+++ securedrop/journalist_app/admin.py
@@ -59,7 +59,7 @@ def make_blueprint(config):
         form = SubmissionPreferencesForm()
         if form.validate_on_submit():
             # The UI prompt ("prevent") is the opposite of the setting ("allow"):
-            flash(gettext("Preferences Saved."), "submission-preferences-success")
+            flash(gettext("Preferences saved."), "submission-preferences-success")
             value = not bool(request.form.get('prevent_document_uploads'))
             InstanceConfig.set('allow_document_uploads', value)
             return redirect(url_for('admin.manage_config'))
