f5554d272f68e35064068a7aa0326d0753dc55a5
John Hensley <john@freedom.press>
2020-04-23 11:18:45 -0400

    Fix error message construction in ansible_version_check.py

--- install_files/ansible-base/callback_plugins/ansible_version_check.py
+++ install_files/ansible-base/callback_plugins/ansible_version_check.py
@@ -21,16 +21,16 @@ class CallbackModule(CallbackBase):
     def __init__(self):
         # The acceptable version range needs to be synchronized with
         # requirements files.
-        viable_start = [2, 9, 6]
+        viable_start = [2, 9, 7]
         viable_end = [2, 10, 0]
         ansible_version = [int(v) for v in ansible.__version__.split('.')]
         if not (viable_start <= ansible_version < viable_end):
             print_red_bold(
-                "SecureDrop restriction: Ansible version must be at least {viable_start}.*"
+                "SecureDrop restriction: Ansible version must be at least {viable_start} "
                 "and less than {viable_end}."
                 .format(
-                    viable_start='.'.join(viable_start),
-                    viable_end='.'.join(viable_end),
+                    viable_start='.'.join(str(v) for v in viable_start),
+                    viable_end='.'.join(str(v) for v in viable_end),
                 )
             )
             sys.exit(1)
