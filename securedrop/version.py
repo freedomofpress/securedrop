8d2ce5375ea12bc6629feff94f000e3ed29ad37a
mickael e <mickael@freedom.press>
2020-05-05 08:51:29 -0400

    SecureDrop 1.3.0-rc2
    (cherry picked from commit 873d58e110d67b68e697f88718ca2a30301fc316)

--- securedrop/version.py
+++ securedrop/version.py
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 __version__ = '1.3.0'
 =======
 __version__ = '1.3.0~rc1'
 >>>>>>> Backporting 1.2.0 changelog and updating version to 1.3.0~rc1
+=======
+__version__ = '1.3.0~rc2'
+>>>>>>> SecureDrop 1.3.0-rc2
