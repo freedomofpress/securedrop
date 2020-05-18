625426e80e0f3a600b88907cf207095583e6ff0b
Allie Crevier <allie@freedom.press>
2020-05-18 09:48:45 -0700

    Post-release version bump from 1.3.0 to 1.4.0~rc1

--- securedrop/version.py
+++ securedrop/version.py
@@ -1,6 +1,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 __version__ = '1.3.0'
 =======
 __version__ = '1.3.0~rc1'
@@ -11,3 +12,6 @@ __version__ = '1.3.0~rc2'
 =======
 __version__ = '1.3.0'
 >>>>>>> SecureDrop release 1.3.0
+=======
+__version__ = '1.4.0~rc1'
+>>>>>>> Post-release version bump from 1.3.0 to 1.4.0~rc1
