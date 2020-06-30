44a464237dc087b682ea969875e94949951fa268
Prateek Jain <prateekj1171998@gmail.com>
2020-07-01 00:22:28 +0530

    Return none in source info in Submission.to_json

--- securedrop/models.py
+++ securedrop/models.py
@@ -215,17 +215,17 @@ class Submission(db.Model):
     def to_json(self) -> 'Dict[str, Union[str, int, bool]]':
         json_submission = {
             'source_url': url_for('api.single_source',
-                                  source_uuid=self.source.uuid),
+                                  source_uuid=self.source.uuid) if self.source else None,
             'submission_url': url_for('api.single_submission',
                                       source_uuid=self.source.uuid,
-                                      submission_uuid=self.uuid),
+                                      submission_uuid=self.uuid) if self.source else None,
             'filename': self.filename,
             'size': self.size,
             'is_read': self.downloaded,
             'uuid': self.uuid,
             'download_url': url_for('api.download_submission',
                                     source_uuid=self.source.uuid,
-                                    submission_uuid=self.uuid),
+                                    submission_uuid=self.uuid) if self.source else None,
         }
         return json_submission
 
