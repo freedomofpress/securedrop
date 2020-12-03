5523065a1165b7d8b5e8561060d7604a3627e139
redshiftzero <jen@freedom.press>
2020-12-03 14:23:33 -0500

    feat(source wasm): message encryption and send

--- securedrop/source_app/main.py
+++ securedrop/source_app/main.py
@@ -155,7 +155,7 @@ def make_blueprint(config: SDConfig) -> Blueprint:
             source_uuid=g.source.uuid,
             to_register=not g.source.is_signal_registered(),
             securedrop_group=current_group,
-            allow_document_uploads=current_app.instance_config.allow_document_uploads,
+            allow_document_uploads=False,  # temp
             replies=replies,
             new_user_codename=session.get('new_user_codename', None),
             form=SubmissionForm(),
