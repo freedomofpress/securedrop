11b77987329fb2fdd29f6898b2d62539806f5668
Conor Schaefer <conor@freedom.press>
2021-02-23 10:51:17 -0500

    Adds more verbose timesync tests
    Expanding the tests already there to ensure we've got a predictable end
    state.

--- molecule/testinfra/common/test_basic_configuration.py
+++ molecule/testinfra/common/test_basic_configuration.py
@@ -5,11 +5,23 @@ testinfra_hosts = [test_vars.app_hostname, test_vars.monitor_hostname]
 
 
 def test_system_time(host):
-    if test_vars.securedrop_target_distribution == "xenial":
+    if host.system_info.codename == "xenial":
         c = host.run("timedatectl status")
         assert "Network time on: yes" in c.stdout
         assert "NTP synchronized: yes" in c.stdout
     else:
+        assert not host.package("ntp").is_installed
+        assert not host.package("ntpdate").is_installed
+
+        s = host.service("systemd-timesyncd")
+        assert s.is_running
+        assert s.is_enabled
+        assert not s.is_masked
+
+        # File will be touched on every successful synchronization,
+        # see 'man systemd-timesyncd'`
+        assert host.file("/run/systemd/timesync/synchronized").exists
+
         c = host.run("timedatectl show")
         assert "NTP=yes" in c.stdout
         assert "NTPSynchronized=yes" in c.stdout
