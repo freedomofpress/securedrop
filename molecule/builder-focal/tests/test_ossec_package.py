fbca4c4dad5f31378f65a46691b723d97343bc68
Kunal Mehta <legoktm@debian.org>
2023-01-13 15:33:18 -0500

    Split and slim down test_securedrop_deb_package.py
    In preparation for splitting the OSSEC builds out of the main SecureDrop
    package builds, split the tests. Since we want to get away from molecule
    eventually, remove the use of testinfra and yaml variables.
    
    I also removed tests that I didn't feel were useful or were already
    covered at a higher level. In general for non-critical things (e.g. the
    maintainer address) we trust that debhelper/dpkg did the right thing and
    catch regressions via debdiff/diffoscope of previous versions.
    
    * test_deb_package_control_fields: Can be statically verified by reading
      d/control.
    * test_deb_package_control_fields_homepage: Same.
    * test_securedrop_app_code_contains_css: CSS is no longer generated on
      the fly, so we can assume that it will be copied properly, like every
      other file.
    * test_securedrop_app_code_contains_static_fonts and co.: Same.
    * test_control_helper_files_are_present: Trust that debhelper installs
      the maintainer scripts properly.
    
    The various tests that check for existence or absence of specifc files
    have been consolidated into two parameterized tests.

---
+++ molecule/builder-focal/tests/test_ossec_package.py
@@ -0,0 +1,77 @@
+import re
+import subprocess
+from pathlib import Path
+
+OSSEC_VERSION = "3.6.0"
+
+SECUREDROP_ROOT = Path(
+    subprocess.check_output(["git", "rev-parse", "--show-toplevel"]).decode().strip()
+)
+BUILD_DIRECTORY = SECUREDROP_ROOT / "build"
+
+
+def test_ossec_binaries_are_present_agent():
+    """
+    Inspect the package contents to ensure all ossec agent binaries are properly
+    included in the package.
+    """
+    wanted_files = [
+        "/var/ossec/bin/agent-auth",
+        "/var/ossec/bin/ossec-syscheckd",
+        "/var/ossec/bin/ossec-agentd",
+        "/var/ossec/bin/manage_agents",
+        "/var/ossec/bin/ossec-control",
+        "/var/ossec/bin/ossec-logcollector",
+        "/var/ossec/bin/util.sh",
+        "/var/ossec/bin/ossec-execd",
+    ]
+    path = BUILD_DIRECTORY / f"ossec-agent_{OSSEC_VERSION}+focal_amd64.deb"
+    contents = subprocess.check_output(["dpkg-deb", "-c", str(path)]).decode()
+    for wanted_file in wanted_files:
+        assert re.search(
+            r"^.* .{}$".format(wanted_file),
+            contents,
+            re.M,
+        )
+
+
+def test_ossec_binaries_are_present_server():
+    """
+    Inspect the package contents to ensure all ossec server binaries are properly
+    included in the package.
+    """
+    wanted_files = [
+        "/var/ossec/bin/ossec-maild",
+        "/var/ossec/bin/ossec-remoted",
+        "/var/ossec/bin/ossec-syscheckd",
+        "/var/ossec/bin/ossec-makelists",
+        "/var/ossec/bin/ossec-logtest",
+        "/var/ossec/bin/syscheck_update",
+        "/var/ossec/bin/ossec-reportd",
+        "/var/ossec/bin/ossec-agentlessd",
+        "/var/ossec/bin/manage_agents",
+        "/var/ossec/bin/rootcheck_control",
+        "/var/ossec/bin/ossec-control",
+        "/var/ossec/bin/ossec-dbd",
+        "/var/ossec/bin/ossec-csyslogd",
+        "/var/ossec/bin/ossec-regex",
+        "/var/ossec/bin/agent_control",
+        "/var/ossec/bin/ossec-monitord",
+        "/var/ossec/bin/clear_stats",
+        "/var/ossec/bin/ossec-logcollector",
+        "/var/ossec/bin/list_agents",
+        "/var/ossec/bin/verify-agent-conf",
+        "/var/ossec/bin/syscheck_control",
+        "/var/ossec/bin/util.sh",
+        "/var/ossec/bin/ossec-analysisd",
+        "/var/ossec/bin/ossec-execd",
+        "/var/ossec/bin/ossec-authd",
+    ]
+    path = BUILD_DIRECTORY / f"ossec-server_{OSSEC_VERSION}+focal_amd64.deb"
+    contents = subprocess.check_output(["dpkg-deb", "-c", str(path)]).decode()
+    for wanted_file in wanted_files:
+        assert re.search(
+            r"^.* .{}$".format(wanted_file),
+            contents,
+            re.M,
+        )
