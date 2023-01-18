0896be7baa494fb656bcaa8ff4e68ac42fbd8a6d
Nathan Dyer <nathandyer@fastmail.com>
2023-01-18 15:05:09 -0500

    Use explicit length checks and validate the URL contains .onion

--- install_files/ansible-base/roles/tails-config/files/securedrop@securedrop.freedom.press/extension.js
+++ install_files/ansible-base/roles/tails-config/files/securedrop@securedrop.freedom.press/extension.js
@@ -48,7 +48,7 @@ class Indicator extends PanelMenu.Button {
         let source = new PopupMenu.PopupMenuItem('Launch Source Interface');
         source.connect('activate', () => {
             let [ok, out, err, exit] = GLib.spawn_command_line_sync('/bin/bash -c "cat ~/Persistent/securedrop/install_files/ansible-base/app-sourcev3-ths"');
-       	    if (out.length > 0) {
+       	    if (out.length == 62 && out.toString().endsWith(".onion")) {
        	    	Util.spawn(['tor-browser', out.toString()]);
        	    } else {
        	    	Util.trySpawnCommandLine('notify-send "Unable to find Source Interface Onion Address"');
@@ -58,7 +58,7 @@ class Indicator extends PanelMenu.Button {
         let journalist = new PopupMenu.PopupMenuItem('Launch Journalist Interface');
         journalist.connect('activate', () => {
        	    let [ok, out, err, exit] = GLib.spawn_command_line_sync('/bin/bash -c "cut -d: -f1 ~/Persistent/securedrop/install_files/ansible-base/app-journalist.auth_private"');
-       	    if (out.length > 0) {
+       	    if (out.length == 56) {
        	    	Util.spawn(['tor-browser', out.toString() + '.onion']);
        	    } else {
        	    	Util.trySpawnCommandLine('notify-send "Unable to find Journalist Interface Onion Address"');
