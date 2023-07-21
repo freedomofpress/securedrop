b66e75af7ce67c64887d30052cb6da6b55e5dd5f
Kunal Mehta <legoktm@debian.org>
2023-07-21 02:42:29 -0400

    redwood: Encrypt a stream instead of a file
    In the initial implementation, I misunderstood how the interaction
    between SecureTemporaryFile and pretty_bad_protocol worked.
    SecureTemporaryFile isn't really a file, it's more like a buffer that's
    backed by on-disk storage. SecureDrop streamed SecureTemporaryFile into
    pretty_bad_protocol, which in turn streamed it to stdin to pass to GPG.
    
    There's no native Python type that represents a stream (ignoring
    asyncio), so pyO3 doesn't have a built-in conversion. The simplest thing
    is to implement a Rust type that wraps a Python object, calls `.read()`
    on it, and implements the `io::Read` trait. The separate pyo3-file[1]
    crate (which I found after writing this) does basically the same thing.
    
    The accompanying test case is implemented in Python since we need to
    access SecureTemporaryFile.
    
    In theory we could stream files directly into Sequoia and drop the
    need for SecureTemporaryFile entirely, but that requires more work
    and would need adding compression support to our Rust code.
    
    [1] https://github.com/omerbenamram/pyo3-file

---
+++ redwood/src/stream.rs
@@ -0,0 +1,32 @@
+use pyo3::types::PyBytes;
+use pyo3::{PyAny, PyResult};
+use std::io::{self, ErrorKind, Read, Write};
+
+/// Wrapper to implement the `Read` trait around a Python
+/// object that contains a `.read()` function.
+pub(crate) struct Stream<'a> {
+    pub(crate) reader: &'a PyAny,
+}
+
+impl Stream<'_> {
+    /// Read the specified number of bytes out of the object
+    fn read_bytes(&self, len: usize) -> PyResult<&PyBytes> {
+        let func = self.reader.getattr("read")?;
+        // In Python this is effectively calling `reader.read(len)`
+        let bytes = func.call1((len,))?;
+        let bytes = bytes.downcast::<PyBytes>()?;
+        Ok(bytes)
+    }
+}
+
+impl Read for Stream<'_> {
+    fn read(&mut self, mut buf: &mut [u8]) -> std::io::Result<usize> {
+        let bytes = self.read_bytes(buf.len()).map_err(|err| {
+            // The PyErr could be a type error (e.g. no "read" method) or an
+            // actual I/O failure if the read() call failed, let's just treat
+            // all of them as "other" for simplicity.
+            io::Error::new(ErrorKind::Other, err.to_string())
+        })?;
+        buf.write(bytes.as_bytes())
+    }
+}
