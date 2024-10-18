fd9dfc14cced531e62a7d48118d9c562f210eac9
Kunal Mehta <legoktm@debian.org>
2024-10-18 16:49:07 -0400

    Fix pip and setuptools in package build process
    * setuptools now wants an explicit `package` listing in setup.py, so
      provide it an empty one.
    * babel no longer explicitly depends on setuptools, but it still needs
      it for `pybabel`, so add it into the translation-requirements.
    * Use pip 24.2, like we upgraded to everywhere else.

--- securedrop/debian/translations.sh
+++ securedrop/debian/translations.sh
@@ -6,9 +6,9 @@ set -ex
 # beyond what the system version provides, see #6317.
 python3 -m venv /tmp/securedrop-app-code-i18n-ve
 /tmp/securedrop-app-code-i18n-ve/bin/pip3 install -r \
-<(echo "pip==21.3
---hash=sha256:4a1de8f97884ecfc10b48fe61c234f7e7dcf4490a37217011ad9369d899ad5a6
---hash=sha256:741a61baab1dbce2d8ca415effa48a2b6a964564f81a9f4f1fce4c433346c034")
+<(echo "pip==24.2 \
+--hash=sha256:5b5e490b5e9cb275c879595064adce9ebd31b854e3e803740b72f9ccf34a45b8 \
+--hash=sha256:2cd581cf58ab7fcfca4ce8efa6dcacd0de5bf8d0a3eb9ec927e07405f4d9e2a2")
 
 # Install dependencies
 /tmp/securedrop-app-code-i18n-ve/bin/pip3 install --no-deps --no-binary :all: --require-hashes -r requirements/python3/translation-requirements.txt
